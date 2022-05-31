e18a952a1e8f00c007611949a944342f1a64be56
Christopher Lee <clee01@esri.com>
2022-05-31 10:25:22 -0400

    Add Display scene sample.
    Added Display scene sample and documentation files.

---
+++ Shared/Samples/Display scene/DisplaySceneView.swift
@@ -0,0 +1,51 @@
+// Copyright 2022 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import SwiftUI
+import ArcGIS
+
+struct DisplaySceneView: View {
+    /// A scene with imagery basemap style and a tiled elevation source
+    @StateObject private var scene = makeScene()
+    
+    /// The starting viewpoint with its initial camera location
+    private let viewpoint = Viewpoint(
+        latitude: 45.74,
+        longitude: 6.88,
+        scale: 10_000,
+        camera: Camera(
+            latitude: 45.74,
+            longitude: 6.88,
+            altitude: 4500,
+            heading: 10,
+            pitch: 70,
+            roll: 0
+        )
+    )
+    
+    /// Make a scene.
+    private static func makeScene() -> ArcGIS.Scene {
+        let scene = Scene(basemapStyle: .arcGISImagery)
+        let surface = Surface()
+        let worldElevationServiceURL = URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!
+        let elevationSource = ArcGISTiledElevationSource(url: worldElevationServiceURL)
+        surface.addElevationSource(elevationSource)
+        scene.baseSurface = surface
+        return scene
+    }
+    
+    var body: some View {
+        SceneView(scene: scene, viewpoint: viewpoint)
+    }
+}
---
+++ Shared/Samples/Display scene/README.md
@@ -0,0 +1,31 @@
+# Display scene
+
+Display a scene with a terrain surface and some imagery.
+
+![Image of display scene](display-scene.png)
+
+## Use case
+
+Scene views are 3D representations of real-world areas and objects. Scene views are helpful for visualizing complex datasets where 3D relationships, topography, and elevation of elements are important factors.
+
+## How to use the sample
+
+When loaded, the sample will display a scene. Pan and zoom to explore the scene.
+
+## How it works
+
+1. Create a `Scene` object with `arcGISImagery` basemap style.
+2. Create an `ArcGISTiledElevationSource` object and use add it to a `Surface` object.
+3. Set the surface to the scene's basemap surface.
+4. Create a `SceneView` view and display the scene.
+
+## Relevant API
+
+* ArcGISTiledElevationSource
+* Scene
+* SceneView
+* Surface
+
+## Tags
+
+3D, basemap, elevation, scene, surface
