d1444ccbba761d0ab9557094bd519a24627a1e41
Christopher Lee <clee01@esri.com>
2022-06-27 17:00:58 -0400

    Use view model

--- Shared/Samples/Find route/FindRouteView.swift
+++ Shared/Samples/Find route/FindRouteView.swift
@@ -12,158 +12,194 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import SwiftUI
 import ArcGIS
+import SwiftUI
 
 struct FindRouteView: View {
-    /// A Boolean value indicating whether to show an alert.
-    @State private var showAlert = false
-    
-    /// The error shown in the alert.
-    @State private var error: Error?
-    
-    /// The direction maneuvers for the calculated route.
-    @State private var directionsList: [DirectionManeuver] = []
-    
-    /// The parameters for the route.
-    @State private var routeParameters: RouteParameters?
-    
-    /// A map with a topographic basemap style and an initial viewpoint.
-    @StateObject private var map: Map = {
-        let map = Map(basemapStyle: .arcGISTopographic)
-        map.initialViewpoint = Viewpoint(
-            center: Point(
-                x: -13041154.715252,
-                y: 3858170.236806,
-                spatialReference: .webMercator
-            ),
-            scale: 1e5
-        )
-        return map
-    }()
-    
-    /// The graphics overlay for the stops.
-    @StateObject private var stopGraphicsOverlay: GraphicsOverlay = {
-        let stopOne = Stop.stopOne
-        let stopTwo = Stop.stopTwo
-        
-        let stopOneGraphic = Graphic(
-            geometry: stopOne.geometry,
-            symbol: TextSymbol(text: stopOne.name, color: .blue, size: 20)
-        )
-        let stopTwoGraphic = Graphic(
-            geometry: stopTwo.geometry,
-            symbol: TextSymbol(text: stopTwo.name, color: .red, size: 20)
-        )
-        
-        return GraphicsOverlay(graphics: [stopOneGraphic, stopTwoGraphic])
-    }()
-    
-    /// The graphics overlay for the route.
-    @StateObject private var routeGraphicsOverlay = GraphicsOverlay(graphics: [
-        Graphic(symbol: SimpleLineSymbol(style: .solid, color: .yellow, width: 5))
-    ])
-    
-    /// A route task from a URL
-    private let routeTask = RouteTask(url: .routeTaskURL)
-    
-    /// The stops for this sample.
-    private let stops: [Stop] = [.stopOne, .stopTwo]
-    
-    /// The graphic for the first stop.
-    private var stopOneGraphic: Graphic { stopGraphicsOverlay.graphics.first! }
-    
-    /// The graphic for the second stop.
-    private var stopTwoGraphic: Graphic { stopGraphicsOverlay.graphics.last! }
-    
-    /// The graphic for the route.
-    private var routeGraphic: Graphic { routeGraphicsOverlay.graphics.first! }
-    
-    /// Creates the parameters for the route.
-    private func createRouteParameters() async {
-        do {
-            // Creates the default parameters.
-            let parameters = try await routeTask.createDefaultParameters()
-            
-            // Sets return directions on the parameters to true.
-            parameters.returnDirections = true
-            
-            // Updates the parameters for the route.
-            routeParameters = parameters
-        } catch {
-            self.error = error
-            showAlert = true
-        }
-    }
-    
-    /// Finds the route from stop one to stop two.
-    private func findRoute() async {
-        // Ensures the route parameters exist.
-        guard let routeParameters = routeParameters else { return }
-        
-        // Resets the geometry, directions, and stops.
-        routeGraphic.geometry = nil
-        directionsList.removeAll()
-        routeParameters.clearStops()
-        
-        // Sets the stops for the route parameters.
-        routeParameters.setStops(stops)
-        
-        do {
-            // Solves the route based on the given route parameters.
-            let routeResult = try await routeTask.solveRoute(routeParameters: routeParameters)
-            if let firstRoute = routeResult.routes.first {
-                // Updates the route geometry and list of directions.
-                routeGraphic.geometry = firstRoute.routeGeometry
-                directionsList = firstRoute.directionManeuvers
-            }
-        } catch {
-            self.error = error
-            showAlert = true
-        }
-    }
+    /// The view model for this sample.
+    @StateObject private var model = Model()
     
     var body: some View {
-        MapView(map: map, graphicsOverlays: [routeGraphicsOverlay, stopGraphicsOverlay])
-            .task {
-                await createRouteParameters()
-            }
+        MapView(map: model.map, graphicsOverlays: model.graphicsOverlays)
+            .alert(isPresented: $model.showAlert, presentingError: model.error)
             .toolbar {
                 ToolbarItemGroup(placement: .bottomBar) {
                     Spacer()
                     Button("Route") {
-                        Task {
-                            await findRoute()
-                        }
+                        model.findRoute()
                     }
+                    .disabled(model.routeIsDisabled)
                     Spacer()
-                    NavigationLink {
-                        // Displays the list of direction maneuvers.
-                        List {
-                            ForEach(0..<directionsList.count, id: \.self) { index in
-                                Text(directionsList[index].directionText)
-                            }
-                        }
-                        .navigationTitle("Directions")
+                    Button {
+                        model.showDirections = true
                     } label: {
                         Image(systemName: "arrow.triangle.turn.up.right.diamond")
                     }
-                    .disabled(directionsList.isEmpty)
+                    .disabled(model.directions.isEmpty)
+                    .popover(isPresented: $model.showDirections) {
+                        NavigationView {
+                            List(model.directions, id: \.directionText) { directionManeuver in
+                                Text(directionManeuver.directionText)
+                            }
+                            .navigationTitle("Directions")
+                            .navigationBarTitleDisplayMode(.inline)
+                            .toolbar {
+                                ToolbarItem(placement: .navigationBarTrailing) {
+                                    Button("Done") {
+                                        model.showDirections = false
+                                    }
+                                }
+                            }
+                        }
+                        .frame(minWidth: 320, minHeight: 500)
+                    }
                 }
             }
     }
 }
 
+private extension FindRouteView {
+    /// A view model for this sample.
+    @MainActor class Model: ObservableObject {
+        /// The directions for the route.
+        @Published var directions: [DirectionManeuver] = []
+        
+        /// A Boolean value indicating whether to show the directions.
+        @Published var showDirections = false
+        
+        /// The parameters for the route.
+        @Published var routeParameters: RouteParameters!
+
+        /// A Boolean value indicating whether to show an alert.
+        @Published var showAlert = false
+
+        /// A Boolean value indicating whether to disable the route button.
+        var routeIsDisabled: Bool { routeParameters == nil }
+        
+        /// The error shown in the alert.
+        var error: Error?
+        
+        /// The route task.
+        private let routeTask = RouteTask(url: .routeTask)
+        
+        /// A map with a topographic basemap style and an initial viewpoint.
+        let map: Map
+        
+        /// The stops for the route.
+        private let stops: [Stop]
+        
+        /// The graphics overlay for the route.
+        private let routeGraphicsOverlay: GraphicsOverlay
+        
+        /// The graphics overlay for the stops.
+        private let stopGraphicsOverlay: GraphicsOverlay
+        
+        /// The graphics overlays for the route and stops.
+        let graphicsOverlays: [GraphicsOverlay]
+        
+        /// The graphic for the route.
+        private var routeGraphic: Graphic { routeGraphicsOverlay.graphics.first! }
+        /// The graphic for the first stop.
+        private var stopOneGraphic: Graphic { stopGraphicsOverlay.graphics.first! }
+        /// The graphic for the second stop.
+        private var stopTwoGraphic: Graphic { stopGraphicsOverlay.graphics.last! }
+        
+        init() {
+            // Initializes the map
+            map = Map(basemapStyle: .arcGISTopographic)
+            map.initialViewpoint = Viewpoint(
+                center: Point(
+                    x: -13041154.7153,
+                    y: 3858170.2368,
+                    spatialReference: .webMercator
+                ),
+                scale: 1e5
+            )
+            
+            // Initializes the graphics overlay for the route.
+            routeGraphicsOverlay = GraphicsOverlay(graphics: [
+                Graphic(symbol: SimpleLineSymbol(style: .solid, color: .yellow, width: 5))
+            ])
+            
+            // Initializes the stops and the graphics overlay for them.
+            stops = [.one, .two]
+            let stopOne = stops.first!
+            let stopTwo = stops.last!
+            
+            let stopOneGraphic = Graphic(
+                geometry: stopOne.geometry,
+                symbol: TextSymbol(text: stopOne.name, color: .blue, size: 20)
+            )
+            
+            let stopTwoGraphic = Graphic(
+                geometry: stopTwo.geometry,
+                symbol: TextSymbol(text: stopTwo.name, color: .red, size: 20)
+            )
+
+            stopGraphicsOverlay = GraphicsOverlay(graphics: [stopOneGraphic, stopTwoGraphic])
+            
+            // Initializes the graphics overlays.
+            graphicsOverlays = [routeGraphicsOverlay, stopGraphicsOverlay]
+            
+            // Creates the route parameters.
+            Task {
+                do {
+                    routeParameters = try await makeRouteParameters()
+                } catch {
+                    self.error = error
+                    showAlert = true
+                }
+            }
+        }
+        
+        /// Creates the parameters for the route.
+        private func makeRouteParameters() async throws -> RouteParameters {
+            // Creates the default parameters.
+            let parameters = try await routeTask.createDefaultParameters()
+            
+            // Sets the return directions on the parameters to true.
+            parameters.returnDirections = true
+            
+            // Sets the stops for the route.
+            parameters.setStops(stops)
+            
+            // Returns the parameters for the route.
+            return parameters
+        }
+        
+        /// Finds the route from stop one to stop two.
+        func findRoute() {
+            Task {
+                // Resets the route geometry and directions.
+                routeGraphic.geometry = nil
+                directions.removeAll()
+                
+                do {
+                    // Solves the route based on the route parameters.
+                    let routeResult = try await routeTask.solveRoute(routeParameters: routeParameters)
+                    if let firstRoute = routeResult.routes.first {
+                        // Updates the route geometry and directions.
+                        routeGraphic.geometry = firstRoute.routeGeometry
+                        directions = firstRoute.directionManeuvers
+                    }
+                } catch {
+                    self.error = error
+                    showAlert = true
+                }
+            }
+        }
+    }
+}
+
 private extension Stop {
     /// The stop for the origin.
-    static var stopOne: Stop {
+    static var one: Stop {
         let stop = Stop(point: Point(x: -13041171.537945, y: 3860988.271378, spatialReference: .webMercator))
         stop.name = "Origin"
         return stop
     }
-    
     /// The stop for the destination.
-    static var stopTwo: Stop {
+    static var two: Stop {
         let stop = Stop(point: Point(x: -13041693.562570, y: 3856006.859684, spatialReference: .webMercator))
         stop.name = "Destination"
         return stop
@@ -172,7 +208,7 @@ private extension Stop {
 
 private extension URL {
     /// The URL for the route task.
-    static var routeTaskURL: URL {
+    static var routeTask: URL {
         URL(string: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/NetworkAnalysis/SanDiego/NAServer/Route")!
     }
 }
