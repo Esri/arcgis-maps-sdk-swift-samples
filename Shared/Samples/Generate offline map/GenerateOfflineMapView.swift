8ed6dabbeea324b1bc913ab95d032dc1298469bd
Christopher Lee <clee01@esri.com>
2022-06-28 14:17:47 -0400

    Use view model

--- Shared/Samples/Generate offline map/GenerateOfflineMapView.swift
+++ Shared/Samples/Generate offline map/GenerateOfflineMapView.swift
@@ -17,167 +17,31 @@ import Combine
 import SwiftUI
 
 struct GenerateOfflineMapView: View {
-    /// A Boolean value indicating whether to show an alert.
-    @State private var showAlert = false
     
-    /// The error shown in the alert.
-    @State private var error: Error?
     
-    /// A Boolean value indicating whether to disable the generate offline map button.
-    @State private var generateIsDisabled = true
-    
-    /// The a red rectangle representing the extent of the area of interest.
-    @State private var areaOfInterest: Envelope?
-    
-    /// The offline map task.
-    @State private var offlineMapTask: OfflineMapTask?
-    
-    /// The generate offline map job.
-    @State private var generateOfflineMapJob: GenerateOfflineMapJob?
-    
-    /// The job's completed progress when generating the offline map. **NOTE: temporary placeholder for job progress view**.
-    @State private var jobProgress: Double?
-    
-    /// A cancellable that updates the job's completed progress. **NOTE: temporary placeholder for job progress view**.
-    @State private var cancellable: AnyCancellable?
-    
-    /// The offline map that is generated.
-    @State private var offlineMap: Map?
-    
-    /// The online map that is loaded from a portal item.
-    @StateObject private var onlineMap = Map(item: PortalItem.napervilleWaterNetwork)
-    
-    /// A graphics overlay consisting of the area of interest graphic.
-    @StateObject private var graphicsOverlay = GraphicsOverlay(graphics: [
-        Graphic(symbol: SimpleLineSymbol(style: .solid, color: .red, width: 2))
-    ])
-    
-    /// The graphic for the area of interest.
-    private var areaOfInterestGraphic: Graphic {
-        graphicsOverlay.graphics.first!
-    }
-    
-    /// Creates a temporary directory for the offline map.
-    private func createTemporaryDirectory() {
-        do {
-            try FileManager.default.createDirectory(at: .temporaryDirectory, withIntermediateDirectories: false)
-        } catch {
-            self.error = error
-            showAlert = true
-        }
-    }
-    
-    /// Removes the temporary directory.
-    private func removeTemporaryDirection() {
-        do {
-            try FileManager.default.removeItem(at: .temporaryDirectory)
-        } catch {
-            self.error = error
-            showAlert = true
-        }
-    }
-    
-    /// Updates the area of interest graphic to represent the new extent.
-    private func updateAreaOfInterest(mapView: MapViewProxy, geometry: GeometryProxy) {
-        // Creates the minimum point for an envelope, in the map
-        // view’s spatial reference, from a point that is located
-        // at 10% the width and height of the map view's frame.
-        guard let min = mapView.location(fromScreenPoint: geometry.min()) else { return }
-        
-        // Creates the maximum point for an envelope, in the map
-        // view’s spatial reference, from a point that is located
-        // at 90% the width and height of the map view's frame.
-        guard let max = mapView.location(fromScreenPoint: geometry.max()) else { return }
-        
-        // Creates an envelope from the min and max points.
-        areaOfInterest = Envelope(min: min, max: max)
-        // Updates the geometry of the graphic.
-        areaOfInterestGraphic.geometry = areaOfInterest
-    }
-    
-    /// Initializes the offline map task.
-    private func configureOfflineMapTask() async {
-        do {
-            try await onlineMap.load()
-            offlineMapTask = OfflineMapTask(portalItem: PortalItem.napervilleWaterNetwork)
-            generateIsDisabled = false
-        } catch {
-            self.error = error
-            showAlert = true
-        }
-    }
-    
-    /// Generates the offline map.
-    private func generateOfflineMap() async {
-        guard let offlineMapTask = offlineMapTask,
-              let areaOfInterest = areaOfInterest else {
-            return
-        }
-        
-        do {
-            // Creates the default parameters for the offline map task.
-            let parameters = try await offlineMapTask.createDefaultGenerateOfflineMapParameters(areaOfInterest: areaOfInterest)
-            
-            // Creates the offline map job based on the parameters.
-            let generateOfflineMapJob = offlineMapTask.generateOfflineMap(parameters: parameters, downloadDirectoryURL: .temporaryDirectory)
-            self.generateOfflineMapJob = generateOfflineMapJob
-            
-            // Starts the job.
-            generateOfflineMapJob.start()
-            generateIsDisabled = true
-            
-            cancellable = generateOfflineMapJob.progress // **NOTE: temporary placeholder for job progress view**.
-                .publisher(for: \.fractionCompleted)
-                .receive(on: RunLoop.main)
-                .sink { jobProgress = $0 }
-            
-            // Awaits the results of the job and sets the offline map to the output.
-            let output = try await generateOfflineMapJob.result.get()
-            offlineMap = output.offlineMap
-            
-            // Sets the initial viewpoint of the offline map.
-            offlineMap?.initialViewpoint = Viewpoint(targetExtent: areaOfInterest.expanded(by: 0.8))
-            
-            cancellable?.cancel() //  **NOTE: temporary placeholder for job progress view**.
-            self.generateOfflineMapJob = nil
-        } catch {
-            self.error = error
-            showAlert = true
-        }
-    }
+    /// The view model for this sample.
+    @StateObject private var model = Model()
     
     var body: some View {
         GeometryReader { geometry in
             MapViewReader { mapView in
-                MapView(map: offlineMap ?? onlineMap, graphicsOverlays: offlineMap == nil ? [graphicsOverlay] : [])
-                    .onViewpointChanged(kind: .centerAndScale) { _ in
-                        if offlineMap == nil {
-                            // Updates the area of interest when the visible area changes.
-                            updateAreaOfInterest(mapView: mapView, geometry: geometry)
-                        }
-                    }
+                MapView(map: model.offlineMap ?? model.onlineMap)
                     .interactionModes([.pan, .zoom])
                     .disabled(generateOfflineMapJob != nil)
+                    .alert(isPresented: $model.isShowingAlert, presentingError: model.error)
                     .task {
-                        await configureOfflineMapTask()
-                        updateAreaOfInterest(mapView: mapView, geometry: geometry)
-                    }
-                    .onAppear {
-                        createTemporaryDirectory()
+                        await model.initializeOfflineMapTask()
                     }
                     .onDisappear {
-                        removeTemporaryDirection()
+                        model.removeTemporaryDirectory()
                     }
                     .overlay {
                         // NOTE: Temporary placeholder for job progress view.
                         if let generateOfflineMapJob = generateOfflineMapJob {
                             VStack {
-                                if let jobProgress = jobProgress {
-                                    VStack {
-                                        Text("\(jobProgress, format: .percent) completed")
-                                        ProgressView(value: jobProgress, total: 1)
-                                    }
-                                    .frame(maxWidth: geometry.size.width * 0.5)
+                                VStack {
+                                    Text("\(model.jobProgress, format: .percent) completed")
+                                    ProgressView(value: model.jobProgress, total: 1)
                                 }
                                 
                                 Button("Cancel") {
@@ -213,6 +77,142 @@ struct GenerateOfflineMapView: View {
     }
 }
 
+private extension GenerateOfflineMapView {
+    /// The view model for this sample.
+    @MainActor
+    class Model: ObservableObject {
+        /// The offline map that is generated.
+        @Published var offlineMap: Map?
+        
+        /// The generate offline map job's progress.
+        @Published var jobProgress = 0.0
+        
+        /// A Boolean value indicating whether the generate button is disabled.
+        @Published var isGenerateDisabled = true
+        
+        /// A Boolean value indicating whether to show an alert.
+        @Published var isShowingAlert = false
+        
+        /// The error shown in the alert.
+        @Published var error: Error?
+        
+        /// The offline map task.
+        private var offlineMapTask: OfflineMapTask!
+        
+        /// The generate offline map job.
+        private var generateOfflineMapJob: GenerateOfflineMapJob!
+        
+        /// A cancellable that updates the job's progress.
+        private var cancellable: AnyCancellable?
+        
+        /// A URL referencing the temporary directory where the offline map files are stored.
+        private let temporaryDirectory = FileManager
+            .default
+            .temporaryDirectory
+            .appendingPathComponent(UUID().uuidString)
+        
+        /// The online map that is loaded from a portal item.
+        let onlineMap = Map(item: PortalItem.napervilleWaterNetwork)
+        
+        init() {
+            // Creates the temporary directory.
+            makeTemporaryDirectory()
+        }
+        
+        /// Initializes the offline map task.
+        func initializeOfflineMapTask() async {
+            do {
+                // Waits for the online map to load.
+                try await onlineMap.load()
+                offlineMapTask = OfflineMapTask(portalItem: .napervilleWaterNetwork)
+                isGenerateDisabled = false
+            } catch {
+                self.error = error
+                isShowingAlert = true
+            }
+        }
+        
+        /// Generates the offline map.
+        /// - Parameter mapView: A map view proxy used to convert the min and max screen points to
+        /// the map view's spatial reference.
+        /// - Parameter geometry: A geometry proxy used to reference the min and max screen points.
+        func generateOfflineMap(mapView: MapViewProxy, geometry: GeometryProxy) async {
+            // Creates the min and max points for the envelope.
+            guard let min = mapView.location(fromScreenPoint: geometry.min()),
+                  let max = mapView.location(fromScreenPoint: geometry.max()) else {
+                return
+            }
+            
+            // Creates the envelope representing the area of interest.
+            let extent = Envelope(min: min, max: max)
+            
+            do {
+                // Creates the default parameters for the offline map task.
+                let parameters = try await offlineMapTask.createDefaultGenerateOfflineMapParameters(areaOfInterest: extent)
+                
+                // Creates the generate offline map job based on the parameters.
+                generateOfflineMapJob = offlineMapTask.generateOfflineMap(parameters: parameters, downloadDirectoryURL: temporaryDirectory)
+                
+                // Starts the job.
+                generateOfflineMapJob.start()
+                
+                // Updates the job's progress.
+                cancellable = generateOfflineMapJob.progress
+                    .publisher(for: \.fractionCompleted)
+                    .receive(on: RunLoop.main)
+                    .sink { self.jobProgress = $0 }
+                
+                // Awaits the result of the job.
+                let result = await generateOfflineMapJob.result
+                
+                // Cancels the job progress subscriber and sets the job to nil.
+                cancellable?.cancel()
+                generateOfflineMapJob = nil
+                
+                switch result {
+                case .success(let output):
+                    // Sets the offline map to the output's offline map.
+                    offlineMap = output.offlineMap
+                    // Sets the initial viewpoint of the offline map.
+                    offlineMap?.initialViewpoint = Viewpoint(targetExtent: extent.expanded(by: 0.8))
+                case .failure(let error):
+                    // Shows an alert with the error if the job fails and the
+                    // error is not a cancellation error.
+                    guard !(error is CancellationError) else { return }
+                    self.error = error
+                    isShowingAlert = true
+                }
+            } catch {
+                self.error = error
+                isShowingAlert = true
+            }
+        }
+        
+        /// Cancels the generate offline map job.
+        func cancelJob() async {
+            // Cancels the generate offline map job.
+            await generateOfflineMapJob.cancel()
+            // Cancels the job progress subscriber and sets the job to nil.
+            cancellable?.cancel()
+            generateOfflineMapJob = nil
+        }
+        
+        /// Creates the temporary directory.
+        private func makeTemporaryDirectory() {
+            do {
+                try FileManager.default.createDirectory(at: temporaryDirectory, withIntermediateDirectories: false)
+            } catch {
+                self.error = error
+                isShowingAlert = true
+            }
+        }
+        
+        /// Removes the temporary directory.
+        func removeTemporaryDirectory() {
+            try? FileManager.default.removeItem(at: temporaryDirectory)
+        }
+    }
+}
 
 private extension PortalItem {
     /// A portal item displaying the Naperville, IL water network.
@@ -221,16 +221,6 @@ private extension PortalItem {
             portal: .arcGISOnline(isLoginRequired: false),
             id: PortalItem.ID("acc027394bc84c2fb04d1ed317aac674")!
         )
-    }
-}
-
-private extension URL {
-    /// A URL to a temporary directory.
-    static var temporaryDirectory: URL {
-        FileManager
-            .default
-            .temporaryDirectory
-            .appendingPathComponent(UUID().uuidString)
     }
 }
 
