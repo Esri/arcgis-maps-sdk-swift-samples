1119f0f102b986a11f23b01e075ae4de497be688
Ting Chen <tchen@esri.com>
2022-07-18 10:30:02 -0700

    Apply changes to test README mechanism.

--- Shared/Supporting Files/Views/SampleDetailView.swift
+++ Shared/Supporting Files/Views/SampleDetailView.swift
@@ -26,6 +26,7 @@ struct SampleDetailView: View {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
                         print("Info button was tapped")
+                        print(sample.readmeURL)
                     } label: {
                         Image(systemName: "info.circle")
                     }
