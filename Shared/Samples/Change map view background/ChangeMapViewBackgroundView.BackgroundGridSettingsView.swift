7a57b42aa274208d8df55130624d116ca6467b58
Mark Dostal <mhdostal@aol.com>
2023-03-17 15:48:38 -0500

    Add initial files

---
+++ Shared/Samples/Change map view background/ChangeMapViewBackgroundView.BackgroundGridSettingsView.swift
@@ -0,0 +1,58 @@
+// Copyright 2023 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import SwiftUI
+
+extension ChangeMapViewBackgroundView {
+    struct BackgroundGridSettingsView: View {
+        /// The view model for the sample.
+        @EnvironmentObject private var model: ChangeMapViewBackgroundView.Model
+        
+        var body: some View {
+            List {
+                Section("Background Grid Settings") {
+                    HStack {
+                        ColorPicker(selection: $model.color) {
+                            Text("Color")
+                        }
+                    }
+                    HStack {
+                        ColorPicker(selection: $model.lineColor) {
+                            Text("Line Color")
+                        }
+                    }
+                    VStack {
+                        HStack {
+                            Text("Line Width")
+                            Spacer()
+                            Text(model.lineWidth.formatted())
+                                .foregroundColor(.secondary)
+                        }
+                        Slider(value: $model.lineWidth, in: model.lineWidthRange, step: 1)
+                    }
+                    VStack {
+                        HStack {
+                            Text("Grid Size")
+                            Spacer()
+                            Text(model.size.formatted())
+                                .foregroundColor(.secondary)
+                        }
+                        Spacer()
+                        Slider(value: $model.size, in: model.sizeRange, step: 1)
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Shared/Samples/Change map view background/ChangeMapViewBackgroundView.Model.swift
@@ -0,0 +1,86 @@
+// Copyright 2023 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+import SwiftUI
+import ArcGIS
+
+extension ChangeMapViewBackgroundView {
+    /// The model used to store the geo model and other expensive objects
+    /// used in this view.
+    class Model: ObservableObject {
+        /// A map with a streets basemap style.
+        let map: Map = {
+            // Initialize tiled layer.
+            let tiledLayer = ArcGISTiledLayer(url: .worldTimeZones)
+            
+            // Initialize map with tiled layer as basemap.
+            let map = Map(basemap: Basemap(baseLayer: tiledLayer))
+            return map
+        }()
+        
+        /// The background grid for the map.
+        let backgroundGrid = BackgroundGrid(backgroundColor: .black, lineColor: .white, lineWidth: 2, size: 32)
+        
+        // The line width range.
+        // Used by Slider, which requires CGFloat values.
+        let lineWidthRange = CGFloat(0)...CGFloat(10)
+        
+        // The grid size range.
+        // Used by Slider, which requires CGFloat values.
+        let sizeRange = CGFloat(2)...CGFloat(50)
+        
+        /// The background color of the grid.
+        @Published var color: Color {
+            didSet {
+                backgroundGrid.backgroundColor = UIColor(color)
+            }
+        }
+        
+        /// The color of the grid lines.
+        @Published var lineColor: Color {
+            didSet {
+                backgroundGrid.lineColor = UIColor(lineColor)
+            }
+        }
+        
+        /// The width of the grid lines in device-independent pixels (DIP).
+        @Published var lineWidth: CGFloat {
+            didSet {
+                backgroundGrid.lineWidth = lineWidth
+            }
+        }
+        
+        /// The size of each grid square in device-independent pixels (DIP).
+        @Published var size: CGFloat {
+            didSet {
+                backgroundGrid.size = size
+            }
+        }
+        
+        init() {
+            // Initialize properties from the background grid.
+            color = Color(uiColor: backgroundGrid.backgroundColor)
+            lineColor = Color(uiColor: backgroundGrid.lineColor)
+            lineWidth = backgroundGrid.lineWidth
+            size = backgroundGrid.size
+        }
+    }
+}
+
+private extension URL {
+    static let worldTimeZones = URL(
+        string: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/WorldTimeZones/MapServer"
+    )!
+}
