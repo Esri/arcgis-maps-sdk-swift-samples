fa7b4c4a5bbf443a401bf3d0812313e343b865f9
Philip Gruenler <pgruenler@esri.com>
2023-03-30 14:24:32 -0700

    PR review comments

--- Shared/Samples/Query feature table/QueryFeatureTableView.swift
+++ Shared/Samples/Query feature table/QueryFeatureTableView.swift
@@ -27,65 +27,46 @@ struct QueryFeatureTableView: View {
         didSet { isShowingAlert = error != nil }
     }
     
-    /// The string used to query the feature table.
-    @State private var query = ""
-    
-    /// A Boolean value indicating if the search field has focus.
-    @FocusState private var searchFieldIsFocused: Bool
+    /// The text in the search bar.
+    @State private var searchBarText = ""
     
     var body: some View {
         MapViewReader { mapViewProxy in
-            MapView(map: model.map, viewpoint: .statesViewpoint)
+            MapView(map: model.map)
                 .alert(isPresented: $isShowingAlert, presentingError: error)
-                .overlay(alignment: .topTrailing) {
-                    HStack {
-                        SearchField(
-                            query: $model.currentQuery,
-                            prompt: "Search state names",
-                            isResultsButtonHidden: true,
-                            isResultListHidden: nil
-                        )
-                        .focused($searchFieldIsFocused)
-                        .onSubmit {
-                            // Set the query string when the search button is tapped.
-                            query = model.currentQuery
-                        }
-                        .submitLabel(.search)
-                        Button("Cancel") {
-                            // Hide keyboard.
-                            searchFieldIsFocused = false
-                        }
-                    }
-                    .padding()
-                }
-                .task(id: query) {
-                    // Makes sure we have a query string and a layer.
-                    guard !query.isEmpty, let featureLayer = model.featureLayer else { return }
-                    // Unselect all selected features.
-                    featureLayer.unselectFeatures(model.selectedFeatures)
-                    model.selectedFeatures.removeAll()
-                    // Make the query parameters and execute the query.
+                // Makes the search bar.
+                .searchable(text: $searchBarText, prompt: Text("Search state names"))
+                .onSubmit(of: .search, {
+                    model.currentQuery = searchBarText
+                })
+                // A task that runs when the query text changes.
+                .task(id: model.currentQuery) {
+                    // Makes sure we have a query string.
+                    guard !model.currentQuery.isEmpty else { return }
+                    // Unselects all selected features.
+                    model.featureLayer.clearSelection()
+                    // Makes the query parameters and executes the query.
                     let queryParameters = QueryParameters()
-                    queryParameters.whereClause = "upper(STATE_NAME) LIKE '%\(query.uppercased())%'"
+                    queryParameters.whereClause = "upper(STATE_NAME) LIKE '%\(model.currentQuery.uppercased())%'"
                     do {
                         let queryResult = try await model.featureTable.queryFeatures(using: queryParameters)
-                        model.selectedFeatures = Array(queryResult.features())
-                        if !model.selectedFeatures.isEmpty {
-                            // Display the selection.
-                            featureLayer.selectFeatures(model.selectedFeatures)
-                            // Zoom to selected feature.
-                            if let featureGeometry = model.selectedFeatures.first?.geometry {
-                                try await mapViewProxy.setViewpointGeometry(featureGeometry, padding: 25)
+                        let queryResultFeatures = Array(queryResult.features())
+                        if !queryResultFeatures.isEmpty {
+                            // Displays the selection.
+                            model.featureLayer.selectFeatures(queryResultFeatures)
+                            // Zooms to the selected features.
+                            if let combinedExtent = GeometryEngine.combineExtents(of: queryResultFeatures.compactMap(\.geometry)) {
+                                await mapViewProxy.setViewpointGeometry(combinedExtent, padding: 25)
                             }
                         } else {
-                            // If the query returned no features then zoom
+                            // If the query returned no features then zooms
                             // to the extent of the layer.
-                            if let layerExtent = featureLayer.fullExtent {
-                                try await mapViewProxy.setViewpointGeometry(layerExtent, padding: 50)
+                            if let layerExtent = model.featureLayer.fullExtent {
+                                await mapViewProxy.setViewpointGeometry(layerExtent, padding: 50)
                             }
                         }
                     } catch {
-                        // Display error as an alert.
+                        // Displays the error as an alert.
                         self.error = error
                     }
                 }
@@ -96,40 +77,47 @@ struct QueryFeatureTableView: View {
 private extension QueryFeatureTableView {
     class Model: ObservableObject {
         /// A map with a topographic basemap style.
-        let map = Map(basemapStyle: .arcGISTopographic)
-        
+        let map: Map = {
+            let map = Map(basemapStyle: .arcGISTopographic)
+            map.initialViewpoint = Viewpoint(
+                center: Point(x: -11e6, y: 5e6, spatialReference: .webMercator),
+                scale: 9e7
+            )
+            return map
+        }()
+
+        /// The text used in the query.
         @Published var currentQuery = ""
         
-        var selectedFeatures: [Feature] = []
-        
-        // Create a feature table using a url.
+        /// A feature table of US Daytime Population Census Tracts.
         let featureTable = ServiceFeatureTable(
-            url: URL(string: "https://services.arcgis.com/jIL9msH9OI208GCb/arcgis/rest/services/USA_Daytime_Population_2016/FeatureServer/0")!
+            item: PortalItem(
+                portal: .arcGISOnline(connection: .anonymous),
+                id: .daytimePopulation
+            )
         )
-        var featureLayer: FeatureLayer?
+
+        /// A feature layer created from the service feature table.
+        let featureLayer: FeatureLayer
         
         init() {
-            // Create a feature layer from feature table.
+            // Creates a feature layer from feature table.
             featureLayer = FeatureLayer(featureTable: featureTable)
-            if let featureLayer {
-                // Show the layer at all scales.
-                featureLayer.minScale = nil
-                featureLayer.maxScale = nil
-                
-                // Set a new renderer.
-                let lineSymbol = SimpleLineSymbol(style: .solid, color: .black, width: 1)
-                let fillSymbol = SimpleFillSymbol(style: .solid, color: UIColor.yellow.withAlphaComponent(0.5), outline: lineSymbol)
-                featureLayer.renderer = SimpleRenderer(symbol: fillSymbol)
-                // Add the feature layer to the map.
-                map.addOperationalLayer(featureLayer)
-            }
+            // Shows the layer at all scales.
+            featureLayer.minScale = nil
+            featureLayer.maxScale = nil
+            
+            // Sets a new renderer on the feature layer.
+            let lineSymbol = SimpleLineSymbol(style: .solid, color: .black, width: 1)
+            let fillSymbol = SimpleFillSymbol(style: .solid, color: .yellow.withAlphaComponent(0.5), outline: lineSymbol)
+            featureLayer.renderer = SimpleRenderer(symbol: fillSymbol)
+            // Adds the feature layer to the map.
+            map.addOperationalLayer(featureLayer)
         }
     }
 }
 
-private extension Viewpoint {
-    /// The initial viewpoint.
-    static var statesViewpoint: Viewpoint {
-        Viewpoint(center: Point(x: -11e6, y: 5e6, spatialReference: .webMercator), scale: 9e7)
-    }
+private extension PortalItem.ID {
+    /// The portal item ID of a USA 2016 Daytime Population feature layer.
+    static var daytimePopulation: Self { Self("f01f0eda766344e29f42031e7bfb7d04")! }
 }
