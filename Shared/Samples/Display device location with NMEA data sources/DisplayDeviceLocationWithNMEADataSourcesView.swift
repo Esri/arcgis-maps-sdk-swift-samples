6845dd631fd459da52747698396f649cd09b9407
Mark Dostal <mhdostal@aol.com>
2023-04-26 11:44:28 -0500

    Handle broken accessory picker.

--- Shared/Samples/Display device location with NMEA data sources/DisplayDeviceLocationWithNMEADataSourcesView.swift
+++ Shared/Samples/Display device location with NMEA data sources/DisplayDeviceLocationWithNMEADataSourcesView.swift
@@ -74,35 +74,47 @@ struct DisplayDeviceLocationWithNMEADataSourcesView: View {
             // Use the supported accessory directly if it's already connected.
             model.accessoryDidConnect(connectedAccessory: accessory, protocolString: protocolString)
         } else {
-            // Show a picker to pair the device with a Bluetooth accessory.
-            // NOTE: the Bluetooth accessory picker is currently not supported
+            error = AccessoryError(
+                detail: "There are no supported Bluetooth devices connected. Open up \"Bluetooth Settings\", connect to your supported device, and try again."
+            )
+            isShowingAlert = true
+        
+            // NOTE: The code below shows how to use the built-in Bluetooth picker
+            // to pair a device. However there are a couple of issues that
+            // prevent the built-in picker from functioning as desired.
+            // The work-around is to have the supported device connected prior
+            // to running the sample. The above message will be displayed
+            // if no devices with a supported protocol are connected.
+            //
+            // The Bluetooth accessory picker is currently not supported
             // for Apple Silicon devices - https://developer.apple.com/documentation/externalaccessory/eaaccessorymanager/1613913-showbluetoothaccessorypicker/
             // "On Apple silicon, this method displays an alert to let the user
             // know that the Bluetooth accessory picker is unavailable."
+            //
             // Also, it appears that there is currently a bug with
             // `showBluetoothAccessoryPicker` - https://developer.apple.com/forums/thread/690320
             // The work-around is to ensure your device is already connected and it's
             // protocol is in the app's list of protocol strings in the plist.info table.
-            EAAccessoryManager.shared().showBluetoothAccessoryPicker(withNameFilter: nil) { error in
-                if let error = error as? EABluetoothAccessoryPickerError,
-                   error.code != .alreadyConnected {
-                    switch error.code {
-                    case .resultNotFound:
-                        self.error = AccessoryError(detail: "The specified accessory could not be found, perhaps because it was turned off prior to connection.")
-                    case .resultCancelled:
-                        // Don't show error message when the picker is cancelled.
-                        self.error = nil
-                        return
-                    default:
-                        self.error = AccessoryError(detail: "Selecting an accessory failed for an unknown reason.")
-                    }
-                    isShowingAlert = (self.error != nil)
-                } else if let (accessory, protocolString) = model.firstSupportedAccessoryWithProtocol() {
-                    // Proceed with supported and connected accessory, and
-                    // ignore other accessories that aren't supported.
-                    model.accessoryDidConnect(connectedAccessory: accessory, protocolString: protocolString)
-                }
-            }
+//            EAAccessoryManager.shared().showBluetoothAccessoryPicker(withNameFilter: nil) { error in
+//                if let error = error as? EABluetoothAccessoryPickerError,
+//                   error.code != .alreadyConnected {
+//                    switch error.code {
+//                    case .resultNotFound:
+//                        self.error = AccessoryError(detail: "The specified accessory could not be found, perhaps because it was turned off prior to connection.")
+//                    case .resultCancelled:
+//                        // Don't show error message when the picker is cancelled.
+//                        self.error = nil
+//                        return
+//                    default:
+//                        self.error = AccessoryError(detail: "Selecting an accessory failed for an unknown reason.")
+//                    }
+//                    isShowingAlert = (self.error != nil)
+//                } else if let (accessory, protocolString) = model.firstSupportedAccessoryWithProtocol() {
+//                    // Proceed with supported and connected accessory, and
+//                    // ignore other accessories that aren't supported.
+//                    model.accessoryDidConnect(connectedAccessory: accessory, protocolString: protocolString)
+//                }
+//            }
         }
     }
 }
