b6be87372fb2ea2b2dfca80cfb3c6b011331621a
CalebRas <caleb.rasmu553n@gmail.com>
2023-06-02 08:48:45 -0700

    Update with review suggestions.

--- Shared/Samples/Create convex hull around points/CreateConvexHullAroundPointsView.swift
+++ Shared/Samples/Create convex hull around points/CreateConvexHullAroundPointsView.swift
@@ -30,19 +30,17 @@ struct CreateConvexHullAroundPointsView: View {
         MapView(map: model.map, graphicsOverlays: model.graphicsOverlays)
             .onSingleTapGesture { _, mapPoint in
                 model.inputPoints.append(mapPoint)
-                model.pointsGraphicsOverlay.addGraphic(Graphic(geometry: mapPoint, symbol: model.markerSymbol))
+                model.pointsGraphicsOverlay.addGraphic(Graphic(geometry: mapPoint))
                 createIsDisabled = false
                 resetIsDisabled = false
             }
             .toolbar {
                 ToolbarItemGroup(placement: .bottomBar) {
-                    // Create button.
                     Button("Create") {
                         model.createConvexHull()
                         createIsDisabled = true
                     }
                     .disabled(createIsDisabled)
-                    // Reset button.
                     Button("Reset") {
                         model.reset()
                         createIsDisabled = true
@@ -58,7 +56,7 @@ private extension CreateConvexHullAroundPointsView {
     /// The view model for this sample.
     private class Model: ObservableObject {
         /// A map with a topographic basemap.
-        let map = Map(basemapStyle: .arcGISTopographic)
+        let map: Map
         
         /// An array of input points to be used in creating the convexHull.
         var inputPoints: [Point] = []
@@ -69,19 +67,31 @@ private extension CreateConvexHullAroundPointsView {
         }
         
         /// The graphics overlay for the input points graphics.
-        let pointsGraphicsOverlay = GraphicsOverlay()
+        var pointsGraphicsOverlay: GraphicsOverlay
         
         /// The graphics overlay for the convex hull graphic.
-        private let convexHullGraphicsOverlay = GraphicsOverlay()
+        private let convexHullGraphicsOverlay: GraphicsOverlay
         
         /// A red simple marker symbol to display where the user tapped on the map.
-        let markerSymbol = SimpleMarkerSymbol(style: .circle, color: .red, size: 10)
+        private let markerSymbol: SimpleMarkerSymbol
         
         /// A blue simple line symbol for the outline of the convex hull graphic.
-        private let lineSymbol = SimpleLineSymbol(style: .solid, color: .blue, width: 4)
+        private let lineSymbol: SimpleLineSymbol
         
         /// A hollow polygon simple fill symbol for the convex hull graphic.
-        private lazy var fillSymbol = SimpleFillSymbol(style: .noFill, outline: lineSymbol)
+        private var fillSymbol: SimpleFillSymbol
+        
+        init() {
+            map = Map(basemapStyle: .arcGISTopographic)
+            
+            markerSymbol = SimpleMarkerSymbol(style: .circle, color: .red, size: 10)
+            lineSymbol = SimpleLineSymbol(style: .solid, color: .blue, width: 4)
+            fillSymbol = SimpleFillSymbol(style: .noFill, outline: lineSymbol)
+            
+            pointsGraphicsOverlay = GraphicsOverlay()
+            pointsGraphicsOverlay.renderer = SimpleRenderer(symbol: markerSymbol)
+            convexHullGraphicsOverlay = GraphicsOverlay()
+        }
         
         /// Reset the points and graphics.
         func reset() {
@@ -92,30 +102,28 @@ private extension CreateConvexHullAroundPointsView {
         
         /// Create the convex hull graphic using the inputPoints.
         func createConvexHull() {
-            // Normalize points.
-            if let normalizedPoints = GeometryEngine.normalizeCentralMeridian(of: Multipoint(points: inputPoints)) {
-                // Create convex hull geometry.
-                if let convexHullGeometry = GeometryEngine.convexHull(for: normalizedPoints) {
-                    // Set the symbol depending on the geometry type of the convex hull.
-                    let symbol: Symbol?
-                    switch convexHullGeometry {
-                    case is Point:
-                        symbol = markerSymbol
-                    case is Polyline:
-                        symbol = lineSymbol
-                    case is Polygon:
-                        symbol = fillSymbol
-                    default:
-                        symbol = nil
-                    }
-                    
-                    // Remove the existing graphic for convex hull.
-                    convexHullGraphicsOverlay.removeAllGraphics()
-                    
-                    // Create the convex hull graphic.
-                    let convexHullGraphic = Graphic(geometry: convexHullGeometry, symbol: symbol)
-                    convexHullGraphicsOverlay.addGraphic(convexHullGraphic)
+            // Normalize points and create convex hull geometry.
+            if let normalizedPoints = GeometryEngine.normalizeCentralMeridian(of: Multipoint(points: inputPoints)),
+               let convexHullGeometry = GeometryEngine.convexHull(for: normalizedPoints) {
+                // Set the symbol depending on the geometry type of the convex hull.
+                let symbol: Symbol?
+                switch convexHullGeometry {
+                case is Point:
+                    symbol = markerSymbol
+                case is Polyline:
+                    symbol = lineSymbol
+                case is Polygon:
+                    symbol = fillSymbol
+                default:
+                    symbol = nil
                 }
+                
+                // Remove the existing graphic for convex hull.
+                convexHullGraphicsOverlay.removeAllGraphics()
+                
+                // Create the convex hull graphic.
+                let convexHullGraphic = Graphic(geometry: convexHullGeometry, symbol: symbol)
+                convexHullGraphicsOverlay.addGraphic(convexHullGraphic)
             }
         }
     }
