d1089ed302a1649b34aaf2c16f438bf97af0df60
CalebRas <caleb.rasmu553n@gmail.com>
2023-06-09 13:43:13 -0700

    Update with review suggestions.

--- Shared/Samples/Identify layer features/IdentifyLayerFeaturesView.swift
+++ Shared/Samples/Identify layer features/IdentifyLayerFeaturesView.swift
@@ -16,37 +16,56 @@ import ArcGIS
 import SwiftUI
 
 struct IdentifyLayerFeaturesView: View {
-    /// The view model for the sample.
-    @StateObject private var model = Model()
+    /// A map with a topographic basemap centered on the United States.
+    @State var map: Map = {
+        let map = Map(basemapStyle: .arcGISTopographic)
+        map.initialViewpoint = Viewpoint(
+            center: Point(x: -10977012.785807, y: 4514257.550369, spatialReference: .webMercator),
+            scale: 68015210
+        )
+        return map
+    }()
     
     /// The tapped screen point.
     @State var tapScreenPoint: CGPoint!
     
+    /// The string text for the identify layer results overlay.
+    @State var overlayText = "Tap on the map to identify feature layers."
+    
+    /// A Boolean value indicating whether to show an error alert.
+    @State var isShowingAlert = false
+    
+    /// The error shown in the error alert.
+    @State var error: Error? {
+        didSet { isShowingAlert = error != nil }
+    }
+    
     var body: some View {
         ZStack {
             MapViewReader { proxy in
-                MapView(map: model.map)
+                MapView(map: map)
                     .onSingleTapGesture { screenPoint, _ in
                         tapScreenPoint = screenPoint
                     }
                     .task {
                         do {
-                            // Add map image layer.
+                            // Add map image layer to the map.
                             let mapImageLayer = ArcGISMapImageLayer(url: .worldCities)
                             try await mapImageLayer.load()
                             
                             // Hide continent and world layers.
                             mapImageLayer.subLayerContents[1].isVisible = false
                             mapImageLayer.subLayerContents[2].isVisible = false
-                            model.map.addOperationalLayer(mapImageLayer)
+                            map.addOperationalLayer(mapImageLayer)
                             
-                            // Add feature layer.
+                            // Add feature layer to the map.
                             let featureTable = ServiceFeatureTable(url: .damageAssessment)
                             try await featureTable.load()
                             let featureLayer = FeatureLayer(featureTable: featureTable)
-                            model.map.addOperationalLayer(featureLayer)
+                            map.addOperationalLayer(featureLayer)
                         } catch {
-                            model.error = error
+                            // Present error load the layers if any.
+                            self.error = error
                         }
                     }
                     .task(id: tapScreenPoint) {
@@ -56,116 +75,65 @@ struct IdentifyLayerFeaturesView: View {
                             screenPoint: screenPoint,
                             tolerance: 12,
                             returnPopupsOnly: false,
-                            maximumResultsPerLayer: 10) {
-                            model.handleIdentifyResults(results)
-                            model.isShowingLayerAlert = true
+                            maximumResultsPerLayer: 10
+                           ) {
+                            handleIdentifyResults(results)
                         }
                     }
-                    .alert(isPresented: $model.isShowingLayerAlert) {
-                        Alert(
-                            title: Text(model.layerAlertTitle),
-                            message: Text(model.layerAlertMessage)
-                        )
+                    .overlay(alignment: .top) {
+                        Text(overlayText)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .padding(8)
+                            .background(.thinMaterial, ignoresSafeAreaEdges: .horizontal)
                     }
-                    .alert(isPresented: $model.isShowingErrorAlert, presentingError: model.error)
+                    .alert(isPresented: $isShowingAlert, presentingError: error)
             }
         }
     }
 }
 
 private extension IdentifyLayerFeaturesView {
-    /// The view model for the sample.
-    class Model: ObservableObject {
-        /// A map with a topographic basemap centered on the United States..
-        let map: Map = {
-            let map = Map(basemapStyle: .arcGISTopographic)
+    /// Update the overlay text based on the identify layer results.
+    /// - Parameter results: An `IdentifyLayerResult Array` to handle.
+    func handleIdentifyResults(_ results: [IdentifyLayerResult]) {
+        // Create overlay message from the results.
+        var alertMessageString = ""
+        var totalGeoElementsCount = 0
+        for (i, identifyLayerResult) in results.enumerated() {
+            // Create alert message the from geoElement count and the layer name.
+            let geoElementsCount = geoElementsCountFromResult(identifyLayerResult)
+            let layerName = identifyLayerResult.layerContent.name
+            alertMessageString.append("\(layerName): \(geoElementsCount)")
             
-            // Center the map on the USA.
-            map.initialViewpoint = Viewpoint(
-                center: Point(x: -10977012.785807, y: 4514257.550369, spatialReference: .webMercator),
-                scale: 68015210
-            )
-            return map
-        }()
-        
-        /// A Boolean value indicating whether to show a layer alert.
-        @Published var isShowingLayerAlert = false
-        
-        /// The title for the identify layer result alert.
-        @Published var layerAlertTitle = ""
-        
-        /// The message for the identify layer result alert.
-        @Published var layerAlertMessage = ""
-        
-        /// A Boolean value indicating whether to show an error alert.
-        @Published var isShowingErrorAlert = false
-        
-        /// The error shown in the error alert.
-        @Published var error: Error? {
-            didSet { isShowingErrorAlert = error != nil }
-        }
-        
-        /// Update the identify layer result alert text based on the identify layer results.
-        /// - Parameter results: An `IdentifyLayerResult Array` to handle.
-        func handleIdentifyResults(_ results: [IdentifyLayerResult]) {
-            var alertMessageString = ""
-            var totalGeoElementsCount = 0
-            
-            for (iCount, identifyLayerResult) in results.enumerated() {
-                // Create alert message the from geoElement count and the layer name.
-                let geoElementsCount = geoElementsCountFromResult(identifyLayerResult)
-                let layerName = identifyLayerResult.layerContent.name
-                alertMessageString.append("\(layerName): \(geoElementsCount)")
-                
-                // Add new line character if not the final element in array.
-                if iCount != results.count - 1 {
-                    alertMessageString.append("\n")
-                }
-                
-                // Update total count.
-                totalGeoElementsCount += geoElementsCount
+            // Add new line character if not the final element in array.
+            if i < results.count - 1 {
+                alertMessageString.append("\n")
             }
             
-            if totalGeoElementsCount > 0 {
-                // Show the results if any elements were found.
-                layerAlertTitle = "Number of elements found"
-                layerAlertMessage = alertMessageString
-            } else {
-                // Notify user that no elements were found.
-                layerAlertTitle = "No element found"
-                layerAlertMessage = ""
-            }
+            // Update total count.
+            totalGeoElementsCount += geoElementsCount
         }
         
-        /// Count the geoElements from an identify layer result.
-        /// - Parameter result: The `IdentifyLayerResult` to count.
-        /// - Returns: An `Int` count of the geoElements
-        private func geoElementsCountFromResult(_ result: IdentifyLayerResult) -> Int {
-            var tempResults = [result]
-            
-            // Using Depth First Search approach to handle recursion.
-            var count = 0
-            var index = 0
-            
-            while index < tempResults.count {
-                // Get the result object from the array.
-                let identifyResult = tempResults[index]
-                
-                // Update count with geoElements from the result.
-                count += identifyResult.geoElements.count
-                
-                // Check if the result has any sublayer results. If so, add those
-                // result objects in the tempResults array after the current result.
-                if !identifyResult.sublayerResults.isEmpty {
-                    tempResults.insert(contentsOf: identifyResult.sublayerResults, at: index + 1)
-                }
-                
-                // Update the count and repeat.
-                index += 1
-            }
-            
-            return count
+        // Update overlay text with the geo-elements found if any.
+        if totalGeoElementsCount > 0 {
+            overlayText = alertMessageString
+        } else {
+            overlayText = "No element found."
         }
+    }
+    
+    /// Count the geo-elements from an identify layer result using recursion.
+    /// - Parameter result: The `IdentifyLayerResult` to count.
+    /// - Returns: An `Int` count of the geo-elements.
+    private func geoElementsCountFromResult(_ identifyResult: IdentifyLayerResult) -> Int {
+        // Get geoElements count from the result.
+        var count = identifyResult.geoElements.count
+        
+        // Get the count using recursion from the result's sublayer results if any.
+        for result in identifyResult.sublayerResults {
+            count += geoElementsCountFromResult(result)
+        }
+        return count
     }
 }
 
