a74a7b37a05172dc1efe45ac3aa2dc5b254a29b8
Caleb Rasmussen <crasmussen@esri.com>
2023-06-09 12:01:37 -0700

    Update Shared/Samples/Show labels on layer/ShowLabelsOnLayerView.swift
    Co-authored-by: Ting <tchen@esri.com>

--- Shared/Samples/Show labels on layer/ShowLabelsOnLayerView.swift
+++ Shared/Samples/Show labels on layer/ShowLabelsOnLayerView.swift
@@ -65,7 +65,7 @@ struct ShowLabelsOnLayerView: View {
 }
 
 private extension ShowLabelsOnLayerView {
-    /// Add labels to a feature layer.
+    /// Adds labels to a feature layer.
     /// - Parameter layer: The `FeatureLayer` to add the labels to.
     func addLabels(to layer: FeatureLayer) {
         // Create label definitions for the two groups.
