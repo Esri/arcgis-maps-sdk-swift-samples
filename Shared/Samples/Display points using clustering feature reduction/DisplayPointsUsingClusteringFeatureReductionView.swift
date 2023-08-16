3ccd7dfd8f8a34b7ba10090e88c231d9c85a13d5
David Feinzimer <dfeinzimer@esri.com>
2023-08-16 12:27:02 -0700

    Update DisplayPointsUsingClusteringFeatureReductionView.swift

--- Shared/Samples/Display points using clustering feature reduction/DisplayPointsUsingClusteringFeatureReductionView.swift
+++ Shared/Samples/Display points using clustering feature reduction/DisplayPointsUsingClusteringFeatureReductionView.swift
@@ -60,20 +60,14 @@ struct DisplayPointsUsingClusteringFeatureReductionView: View {
                 .task(id: identifyScreenPoint) {
                     guard let identifyScreenPoint,
                           let layer,
-                          let identifyResult = await Result(awaiting: {
-                              try await proxy.identify(
-                                on: layer,
-                                screenPoint: identifyScreenPoint,
-                                tolerance: 3
-                              )
-                          })
-                        .cancellationToNil()
-                    else {
-                        return
-                    }
-                    
+                          let identifyResult = try? await proxy.identify(
+                            on: layer,
+                            screenPoint: identifyScreenPoint,
+                            tolerance: 3
+                          )
+                    else { return }
                     self.identifyScreenPoint = nil
-                    self.popup = try? identifyResult.get().popups.first
+                    self.popup = identifyResult.popups.first
                     self.showsPopup = self.popup != nil
                 }
                 .floatingPanel(
