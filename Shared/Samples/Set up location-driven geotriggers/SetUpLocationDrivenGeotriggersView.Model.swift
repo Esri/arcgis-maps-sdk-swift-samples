340d0104156f4b51c5b4d06061ae4eaf39bb589f
Ting Chen <tchen@esri.com>
2023-08-22 16:31:44 -0700

    Only import Combine for needed types.

--- Shared/Samples/Set up location-driven geotriggers/SetUpLocationDrivenGeotriggersView.Model.swift
+++ Shared/Samples/Set up location-driven geotriggers/SetUpLocationDrivenGeotriggersView.Model.swift
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import SwiftUI
 import ArcGIS
+import Combine
+import Foundation
 
 extension SetUpLocationDrivenGeotriggersView {
     /// The view model for the sample.
--- Shared/Samples/Set visibility of subtype sublayer/SetVisibilityOfSubtypeSublayerView.Model.swift
+++ Shared/Samples/Set visibility of subtype sublayer/SetVisibilityOfSubtypeSublayerView.Model.swift
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import SwiftUI
 import ArcGIS
+import Combine
+import Foundation
 
 extension SetVisibilityOfSubtypeSublayerView {
     /// The view model for this sample.
