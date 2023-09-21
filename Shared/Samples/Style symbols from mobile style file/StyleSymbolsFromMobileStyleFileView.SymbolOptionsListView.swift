d524d360fbad7fd696862994861a3ab922104ee7
Caleb Rasmussen <crasmussen@esri.com>
2023-09-21 15:54:38 -0700

    Add source code.

---
+++ Shared/Samples/Style symbols from mobile style file/StyleSymbolsFromMobileStyleFileView.SymbolOptionsListView.swift
@@ -0,0 +1,121 @@
+// Copyright 2023 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import ArcGIS
+import SwiftUI
+
+extension StyleSymbolsFromMobileStyleFileView {
+    struct SymbolOptionsListView: View {
+        /// The display scale of the environment.
+        @Environment(\.displayScale) private var displayScale
+        
+        /// The view model for the sample.
+        @ObservedObject var model: Model
+        
+        /// The list of all the symbols and their associated data from the symbol style.
+        @State private var symbols: [SymbolDetails] = []
+        
+        var body: some View {
+            VStack {
+                Image(uiImage: model.currentSymbol?.image ?? UIImage())
+                
+                List {
+                    // Create a picker for each symbol category.
+                    ForEach(SymbolCategory.allCases, id: \.self) { category in
+                        Section(category.label) {
+                            Picker("Symbol Names", selection: $model.symbolOptionSelections.categoryKeys[category]) {
+                                ForEach(symbols.filter { $0.category == category.rawValue }, id: \.key) { symbol in
+                                    Label {
+                                        Text(symbol.label)
+                                    } icon: {
+                                        Image(uiImage: symbol.image)
+                                    }
+                                    .tag(symbol.key as String?)
+                                }
+                            }
+                            .pickerStyle(.inline)
+                            .labelsHidden()
+                        }
+                    }
+                    
+                    Section {
+                        ColorPicker("Color", selection: $model.symbolOptionSelections.color)
+                        VStack {
+                            HStack {
+                                Text("Size")
+                                Spacer()
+                                Text(model.symbolOptionSelections.size.formatted(.number.rounded()))
+                                    .foregroundColor(.secondary)
+                            }
+                            Slider(value: $model.symbolOptionSelections.size, in: 20...60, step: 1)
+                        }
+                    }
+                }
+            }
+            .task(id: model.symbolOptionSelections) {
+                // Update the current symbol when an option selection changes.
+                await model.updateCurrentSymbol(displayScale: displayScale)
+            }
+            .task(id: displayScale) {
+                // Update the symbols when the display scale changes.
+                symbols = await getSymbols(displayScale: displayScale)
+                await model.updateCurrentSymbol(displayScale: displayScale)
+            }
+        }
+        
+        /// Gets the symbols from the symbol style.
+        /// - Parameter displayScale: The display scale of the environment for creating symbol swatches.
+        /// - Returns:  An `Array` of `SymbolDetails` which contain a symbol and its associated information.
+        private func getSymbols(displayScale: Double) async -> [SymbolDetails] {
+            // Get the default symbol search parameters from the symbol style.
+            guard let searchParameters = try? await model.symbolStyle.defaultSearchParameters else { return [] }
+            
+            // Get the symbol style search results using the search parameters.
+            guard let searchResults = try? await model.symbolStyle.searchSymbols(
+                using: searchParameters
+            ) else { return [] }
+            
+            // Create a symbol for each search result.
+            let symbols = await withTaskGroup(of: SymbolDetails?.self) { group in
+                for result in searchResults {
+                    group.addTask {
+                        // Get the symbol from the symbol style using the symbol's key from the result.
+                        if let symbol = try? await self.model.symbolStyle.symbol(forKeys: [result.key]) {
+                            // Create an image swatch from the symbol using the display scale.
+                            if let swatch = try? await symbol.makeSwatch(scale: displayScale) {
+                                return SymbolDetails(
+                                    symbol: symbol,
+                                    image: swatch,
+                                    name: result.name,
+                                    key: result.key,
+                                    category: result.category
+                                )
+                            }
+                        }
+                        return nil
+                    }
+                }
+                
+                var symbols: [SymbolDetails] = []
+                for await symbol in group {
+                    if let symbol {
+                        symbols.append(symbol)
+                    }
+                }
+                return symbols.sorted { $0.name < $1.name }
+            }
+            return symbols
+        }
+    }
+}
--- Shared/Samples/Style symbols from mobile style file/StyleSymbolsFromMobileStyleFileView.swift
+++ Shared/Samples/Style symbols from mobile style file/StyleSymbolsFromMobileStyleFileView.swift
@@ -16,52 +16,65 @@ import ArcGIS
 import SwiftUI
 
 struct StyleSymbolsFromMobileStyleFileView: View {
+    /// The display scale of the environment.
+    @Environment(\.displayScale) private var displayScale
+    
     /// The view model for the sample.
     @StateObject private var model = Model()
     
-    /// A Boolean that indicates whether the symbol sheet is showing.
-    @State private var isShowingSymbolSheet = false
+    /// A Boolean value that indicates whether the symbol options sheet is showing.
+    @State private var isShowingSymbolOptionsSheet = false
     
     var body: some View {
         MapView(map: model.map, graphicsOverlays: [model.graphicsOverlay])
             .onSingleTapGesture { _, mapPoint in
-                /// Add current symbol to map on tap.
-                let graphic = Graphic(geometry: mapPoint, symbol: model.currentSymbol)
-                model.graphicsOverlay.addGraphic(graphic)
+                // Add the current symbol to map as a graphic at the tap location.
+                let symbolGraphic = Graphic(geometry: mapPoint, symbol: model.currentSymbol?.symbol)
+                model.graphicsOverlay.addGraphic(symbolGraphic)
             }
             .toolbar {
                 ToolbarItemGroup(placement: .bottomBar) {
                     Button("Symbol") {
-                        isShowingSymbolSheet = true
+                        isShowingSymbolOptionsSheet = true
                     }
-                    .sheet(isPresented: $isShowingSymbolSheet, detents: [.large]) {
-                        NavigationView {
-                            Spacer()
-                                .navigationTitle("Layers")
-                                .navigationBarTitleDisplayMode(.inline)
-                                .toolbar {
-                                    ToolbarItem(placement: .confirmationAction) {
-                                        Button("Done") {
-                                            isShowingSymbolSheet = false
-                                        }
-                                    }
-                                }
-                        }
-                        .navigationViewStyle(.stack)
-                        .frame(idealWidth: 320, idealHeight: 428)
+                    .sheet(isPresented: $isShowingSymbolOptionsSheet, detents: [.large]) {
+                        symbolOptionsList
                     }
+                    
                     Spacer()
                     Button("Clear") {
-                        // Clears all symbol graphics from the map.
+                        // Clear all graphics from the map.
                         model.graphicsOverlay.removeAllGraphics()
                     }
                 }
             }
+            .task(id: displayScale) {
+                // Update the current symbol when the display scale change.
+                await model.updateCurrentSymbol(displayScale: displayScale)
+            }
+    }
+    
+    /// The list containing the symbol options.
+    private var symbolOptionsList: some View {
+        NavigationView {
+            SymbolOptionsListView(model: model)
+                .navigationTitle("Symbol")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .confirmationAction) {
+                        Button("Done") {
+                            isShowingSymbolOptionsSheet = false
+                        }
+                    }
+                }
+        }
+        .navigationViewStyle(.stack)
     }
 }
 
-private extension StyleSymbolsFromMobileStyleFileView {
+extension StyleSymbolsFromMobileStyleFileView {
     /// The view model for the sample.
+    @MainActor
     class Model: ObservableObject {
         /// A map with a topographic basemap.
         let map = Map(basemapStyle: .arcGISTopographic)
@@ -69,15 +82,109 @@ private extension StyleSymbolsFromMobileStyleFileView {
         /// The graphics overlay for all the symbol graphics on the map.
         let graphicsOverlay = GraphicsOverlay()
         
-        /// The symbols style.
-        let symbolStyle = SymbolStyle(name: "emoji-mobile", bundle: nil)
+        /// The emoji mobile symbol style created from a local symbol style file.
+        let symbolStyle = SymbolStyle(url: .emojiMobile)
         
-        /// The current symbol selection
-        var currentSymbol: Symbol
+        /// The current symbol created from the symbol style based on the option selections.
+        @Published var currentSymbol: SymbolDetails?
         
+        /// The current symbol option selections used to create the current symbol.
+        @Published var symbolOptionSelections = SymbolOptions(
+            eyes: "Eyes-crossed",
+            hat: "Hat-cowboy",
+            mouth: "Mouth-frown",
+            color: Color.yellow,
+            size: 40
+        )
         
-        init() {
-            currentSymbol = SimpleMarkerSymbol(style: .circle, color: .red, size: 10)
+        /// Updates the current symbol with a symbol created from the symbol style using the current option selections.
+        /// - Parameter displayScale: The display scale of the environment for creating the symbol swatch.
+        func updateCurrentSymbol(displayScale: Double) async {
+            // Get the keys from the option selections.
+            var symbolKeys = ["Face1"]
+            symbolKeys.append(contentsOf: symbolOptionSelections.categoryKeys.map { $0.value })
+            
+            // Get the symbol from symbol style using the keys.
+            if let pointSymbol = try? await symbolStyle.symbol(forKeys: symbolKeys) as? MultilayerPointSymbol {
+                // Color lock all layers but the first one.
+                let layers = pointSymbol.symbolLayers
+                for (i, layer) in layers.enumerated() {
+                    layer.colorIsLocked = i != 0 ? true : false
+                }
+                
+                pointSymbol.color = UIColor(symbolOptionSelections.color)
+                pointSymbol.size = symbolOptionSelections.size
+                
+                // Create an image swatch for the symbol using the display scale.
+                if let swatch = try? await pointSymbol.makeSwatch(scale: displayScale) {
+                    // Update the current symbol with the created symbol and swatch.
+                    currentSymbol = SymbolDetails(symbol: pointSymbol, image: swatch)
+                }
+            }
         }
+    }
+    
+    /// A symbol and its associated information.
+    struct SymbolDetails {
+        /// The symbol from the symbol style.
+        let symbol: Symbol
+        /// The image swatch of the symbol.
+        let image: UIImage
+        /// The name of the symbol as found in the symbol style.
+        let name: String
+        /// The key of the symbol as found in the symbol style.
+        let key: String
+        /// The category of the symbol as found in the symbol style.
+        let category: String
+        /// The human-readable label of the symbol name.
+        var label: String {
+            let splitName = name.replacingOccurrences(of: "-", with: " ").split(separator: " ")
+            return splitName.last?.capitalized ?? name
+        }
+        
+        init(symbol: Symbol, image: UIImage, name: String = "", key: String = "", category: String = "") {
+            self.symbol = symbol
+            self.image = image
+            self.name = name
+            self.key = key
+            self.category = category
+        }
+    }
+    
+    /// The different options used in creating a symbol from the symbol style.
+    struct SymbolOptions: Equatable {
+        /// A dictionary containing the key of a symbol for each symbol category.
+        var categoryKeys: [SymbolCategory: String] = [:]
+        /// The color of the symbol.
+        var color: Color
+        /// The size of the symbol.
+        var size: Double
+        
+        init(eyes: String, hat: String, mouth: String, color: Color, size: Double) {
+            categoryKeys[.eyes] = eyes
+            categoryKeys[.hat] = hat
+            categoryKeys[.mouth] = mouth
+            self.color = color
+            self.size = size
+        }
+    }
+    
+    /// The different symbol categories as found in the symbol style.
+    enum SymbolCategory: String, CaseIterable {
+        case eyes = "Eyes"
+        case hat = "Hat"
+        case mouth = "Mouth"
+        
+        /// A human-readable label of the category name.
+        var label: String {
+            return self.rawValue.hasSuffix("s") ? self.rawValue : "\(self.rawValue)s"
+        }
+    }
+}
+
+private extension URL {
+    /// A URL to the local emoji mobile symbol style file.
+    static var emojiMobile: URL {
+        Bundle.main.url(forResource: "emoji-mobile", withExtension: "stylx")!
     }
 }
