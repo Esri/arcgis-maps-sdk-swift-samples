b48442ffaa0e70d67fbee621ed8f4c7f3849f2b3
Caleb Rasmussen <crasmussen@esri.com>
2023-09-25 13:17:49 -0700

    Add source code.

--- Shared/Samples/Play KML Tour/PlayKMLTourView.swift
+++ Shared/Samples/Play KML Tour/PlayKMLTourView.swift
@@ -16,27 +16,125 @@ import ArcGIS
 import SwiftUI
 
 struct PlayKMLTourView: View {
-    /// The view model for the sample.
-    @StateObject private var model = Model()
+    /// A scene with an imagery basemap and a world elevation service.
+    @State private var scene: ArcGIS.Scene = {
+        let scene = Scene(basemapStyle: .arcGISImagery)
+        scene.initialViewpoint = Viewpoint(latitude: .nan, longitude: .nan, scale: 114_145_911)
+        
+        // Set base surface with an elevation source.
+        let surface = Surface()
+        let elevationSource = ArcGISTiledElevationSource(url: .worldElevationService)
+        surface.addElevationSource(elevationSource)
+        scene.baseSurface = surface
+        return scene
+    }()
+    
+    /// The current viewpoint of the scene view used when reseting the tour.
+    @State private var viewpoint: Viewpoint?
+    
+    /// The tour controller used to control the KML tour.
+    @State private var tourController = KMLTourController()
+    
+    /// The current status of the KML tour.
+    @State private var tourStatus: KMLTour.Status = .notInitialized
+    
+    /// A Boolean that indicates whether to show an error alert.
+    @State private var isShowingErrorAlert = false
+    
+    /// The error shown in the error alert.
+    @State private var error: Error? {
+        didSet { isShowingErrorAlert = error != nil }
+    }
+    
+    /// A Boolean value that indicates whether the KML tour is not ready to be played.
+    var tourIsNotReady: Bool {
+        tourStatus == .notInitialized || tourStatus == .initializing
+    }
     
     var body: some View {
-        MapView(map: model.map)
-            .alert(isPresented: $model.isShowingErrorAlert, presentingError: model.error)
+        // Create a scene view with a scene and a viewpoint.
+        SceneView(scene: scene, viewpoint: viewpoint)
+            .onViewpointChanged(kind: .centerAndScale) { viewpoint = $0 }
+            .task {
+                do {
+                    // Add KML layer to the scene.
+                    let dataset = KMLDataset(url: .esriTour)
+                    try await dataset.load()
+                    let layer = KMLLayer(dataset: dataset)
+                    scene.addOperationalLayer(layer)
+                    
+                    // Set the tour controller with first tour in the dataset.
+                    if let tour = firstKMLTour(in: dataset) {
+                        tourController.tour = tour
+                        
+                        // Listen for tour status updates.
+                        for await status in tourController.tour!.$status {
+                            tourStatus = status
+                        }
+                    }
+                } catch {
+                    self.error = error
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    ZStack {
+                        HStack {
+                            Button {
+                                tourController.reset()
+                                viewpoint = scene.initialViewpoint
+                            } label: {
+                                Image(systemName: "gobackward")
+                            }
+                            .disabled(tourIsNotReady || tourStatus == .initialized)
+                            Spacer()
+                        }
+                        
+                        Button {
+                            tourStatus == .playing ? tourController.pause() : tourController.play()
+                        } label: {
+                            Image(systemName: tourStatus == .playing ? "pause.fill" : "play.fill")
+                        }
+                        .disabled(tourIsNotReady)
+                    }
+                }
+            }
+            .alert(isPresented: $isShowingErrorAlert, presentingError: error)
     }
 }
 
 private extension PlayKMLTourView {
-    /// The view model for the sample.
-    class Model: ObservableObject {
-        /// A map with a topographic basemap.
-        let map = Map(basemapStyle: .arcGISTopographic)
+    /// Finds the first KML tour in a KML dataset.
+    /// - Parameter dataset: The `KMLDataset` to search through.
+    /// - Returns: The first `KMLTour` in the dataset if any.
+    private func firstKMLTour(in dataset: KMLDataset) -> KMLTour? {
+        var nodes = dataset.rootNodes
+        var i = 0
         
-        /// A Boolean that indicates whether to show an error alert.
-        @Published var isShowingErrorAlert = false
-        
-        /// The error shown in the alert.
-        @Published var error: Error? {
-            didSet { isShowingErrorAlert = error != nil }
+        // Loop through the nodes until a tour is found.
+        while i < nodes.count {
+            if nodes[i] is KMLTour {
+                return nodes[i] as? KMLTour
+            }
+            
+            // If node is a container, add all of its children to be looped through.
+            if nodes[i] is KMLContainer {
+                nodes.append(contentsOf: (nodes[i] as! KMLContainer).childNodes)
+            }
+            i += 1
         }
+        return nil
+    }
+}
+
+private extension URL {
+    /// A URL to world elevation service from Terrain3D ArcGIS REST service.
+    static var worldElevationService: URL {
+        URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!
+    }
+    
+    /// A URL to a KMZ file of an Esri Tour on ArcGIS Online.
+    static var esriTour: URL {
+        URL(string: "https://www.arcgis.com/sharing/rest/content/items/f10b1d37fdd645c9bc9b189fb546307c/data")!
     }
 }
