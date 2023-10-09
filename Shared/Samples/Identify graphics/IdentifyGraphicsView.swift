6785b3d409804a5a54524d55dcb90bf30da29971
Caleb Rasmussen <crasmussen@esri.com>
2023-10-09 11:45:04 -0700

    Add source code.

--- Shared/Samples/Identify graphics/IdentifyGraphicsView.swift
+++ Shared/Samples/Identify graphics/IdentifyGraphicsView.swift
@@ -19,7 +19,38 @@ struct IdentifyGraphicsView: View {
     /// A map with a topographic basemap.
     @State private var map = Map(basemapStyle: .arcGISTopographic)
     
-    /// A Boolean value that indicates whether to show an error alert.
+    /// A graphics overlay containing a yellow polygon graphic.
+    @State private var graphicsOverlay = {
+        let graphicsOverlay = GraphicsOverlay()
+        
+        // Create a polygon from a list of points.
+        let polygon = Polygon(points: [
+            Point(x: -20e5, y: 20e5),
+            Point(x: 20e5, y: 20e5),
+            Point(x: 20e5, y: -20e5),
+            Point(x: -20e5, y: -20e5)
+        ])
+        
+        // Create a graphic of the polygon and add it to the overlay.
+        let polygonGraphic = Graphic(geometry: polygon)
+        graphicsOverlay.addGraphic(polygonGraphic)
+        
+        // Create a renderer using a simple fill symbol.
+        let polygonSymbol = SimpleFillSymbol(style: .solid, color: .yellow)
+        graphicsOverlay.renderer = SimpleRenderer(symbol: polygonSymbol)
+        
+        return graphicsOverlay
+    }()
+    
+    /// A Boolean value indicating whether the graphic alert is showing.
+    @State private var isShowingGraphicAlert = false
+    
+    /// The text shown in the graphic alert that shows the alert when changed.
+    @State private var graphicAlertText = "" {
+        didSet { isShowingGraphicAlert = graphicAlertText.isNotEmpty }
+    }
+    
+    /// A Boolean value indicating whether to show an error alert.
     @State private var isShowingErrorAlert = false
     
     /// The error shown in the error alert.
@@ -28,7 +59,42 @@ struct IdentifyGraphicsView: View {
     }
     
     var body: some View {
-        MapView(map: map)
-            .alert(isPresented: $isShowingErrorAlert, presentingError: error)
+        MapViewReader { mapViewProxy in
+            MapView(map: map, graphicsOverlays: [graphicsOverlay])
+                .onSingleTapGesture { screenPoint, _ in
+                    Task {
+                        do {
+                            // Identify the screen point on the graphics overlay using the proxy.
+                            // Use `identifyGraphicsOverlays` instead if you need to identify on
+                            // all the graphics overlay present in the map view.
+                            let identifyResult = try await mapViewProxy.identify(
+                                on: graphicsOverlay,
+                                screenPoint: screenPoint,
+                                tolerance: 12
+                            )
+                            
+                            // Display an alert if a graphic was found at the screen point.
+                            if identifyResult.graphics.isNotEmpty {
+                                let postfix = identifyResult.graphics.count > 1 ? "s" : ""
+                                graphicAlertText = "Tapped on \(identifyResult.graphics.count) graphic\(postfix)."
+                            }
+                        } catch {
+                            // Show an error alert for an error throw while identifying.
+                            self.error = error
+                        }
+                    }
+                }
+                .alert(graphicAlertText, isPresented: $isShowingGraphicAlert) {
+                    Button("OK") { }
+                }
+                .alert(isPresented: $isShowingErrorAlert, presentingError: error)
+        }
+    }
+}
+
+private extension Collection {
+    /// A Boolean value indicating whether the collection is not empty.
+    var isNotEmpty: Bool {
+        !self.isEmpty
     }
 }
