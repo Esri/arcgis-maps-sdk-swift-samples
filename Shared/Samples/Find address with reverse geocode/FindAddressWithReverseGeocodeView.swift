8fc5fae5dd65289bf6acf6c694e421606f699f39
Caleb Rasmussen <crasmussen@esri.com>
2023-11-13 10:36:17 -0800

    Add sample files.

---
+++ Shared/Samples/Find address with reverse geocode/FindAddressWithReverseGeocodeView.swift
@@ -0,0 +1,42 @@
+// Copyright 2023 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import ArcGIS
+import SwiftUI
+
+struct FindAddressWithReverseGeocodeView: View {
+    /// The view model for the sample.
+    @StateObject private var model = Model()
+    
+    var body: some View {
+        MapView(map: model.map)
+            .alert(isPresented: $model.errorAlertIsShowing, presentingError: model.error)
+    }
+}
+
+private extension FindAddressWithReverseGeocodeView {
+    /// The view model for the sample.
+    class Model: ObservableObject {
+        /// A map with a topographic basemap.
+        let map = Map(basemapStyle: .arcGISTopographic)
+        
+        /// A Boolean value indicating whether the error alert is showing.
+        @Published var errorAlertIsShowing = false
+        
+        /// The error shown in the error alert.
+        @Published var error: Error? {
+            didSet { errorAlertIsShowing = error != nil }
+        }
+    }
+}
