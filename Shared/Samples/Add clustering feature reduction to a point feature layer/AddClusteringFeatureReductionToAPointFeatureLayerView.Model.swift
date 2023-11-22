fa0c8f96e2319f9db2561be9246380005946a97d
Ting Chen <tchen@esri.com>
2023-11-22 15:02:26 -0800

    Add files to the project.

---
+++ Shared/Samples/Add clustering feature reduction to a point feature layer/AddClusteringFeatureReductionToAPointFeatureLayerView.Model.swift
@@ -0,0 +1,9 @@
+//
+//  AddClusteringFeatureReductionToAPointFeatureLayerView.Model.swift
+//  Samples
+//
+//  Created by Ting Chen on 11/22/23.
+//  Copyright © 2023 Esri. All rights reserved.
+//
+
+import Foundation
---
+++ Shared/Samples/Add clustering feature reduction to a point feature layer/AddClusteringFeatureReductionToAPointFeatureLayerView.swift
@@ -0,0 +1,19 @@
+//
+//  AddClusteringFeatureReductionToAPointFeatureLayerView.swift
+//  Samples
+//
+//  Created by Ting Chen on 11/22/23.
+//  Copyright © 2023 Esri. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddClusteringFeatureReductionToAPointFeatureLayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddClusteringFeatureReductionToAPointFeatureLayerView()
+}
