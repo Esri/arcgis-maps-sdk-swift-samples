21edc9dac88cac620ed0e54b113405d89f1d087f
Caleb Rasmussen <crasmussen@esri.com>
2024-01-18 12:33:16 -0800

    Add sample source code.

--- Shared/Samples/Style point with distance composite scene symbol/StylePointWithDistanceCompositeSceneSymbolView.swift
+++ Shared/Samples/Style point with distance composite scene symbol/StylePointWithDistanceCompositeSceneSymbolView.swift
@@ -19,30 +19,121 @@ struct StylePointWithDistanceCompositeSceneSymbolView: View {
     /// The view model for the sample.
     @StateObject private var model = Model()
     
-    /// The error shown in the error alert.
-    @State private var error: Error?
+    /// The distance from the target object to the camera in meters.
+    @State private var cameraDistance = Measurement(value: 0, unit: UnitLength.meters)
     
     var body: some View {
-        MapView(map: model.map)
-            .errorAlert(presentingError: $error)
+        SceneView(
+            scene: model.scene,
+            cameraController: model.cameraController,
+            graphicsOverlays: [model.graphicsOverlay]
+        )
+        .overlay(alignment: .top) {
+            VStack {
+                Text("Zoom in and out to see the symbol change.")
+                    .frame(maxWidth: .infinity)
+                    .padding(8)
+                    .background(.thinMaterial, ignoresSafeAreaEdges: .horizontal)
+                
+                HStack {
+                    Spacer()
+                    HStack {
+                        Text("Distance:")
+                        Text(cameraDistance, format: .measurement(
+                            width: .narrow,
+                            usage: .asProvided,
+                            numberFormatStyle: .number.precision(.fractionLength(0))
+                        ))
+                    }
+                    .padding()
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(10)
+                    .shadow(radius: 3)
+                }
+                .padding(.trailing, 8)
+            }
+        }
+        .task {
+            for await newDistance in model.cameraController.$cameraDistance {
+                cameraDistance.value = newDistance
+            }
+        }
     }
 }
 
 private extension StylePointWithDistanceCompositeSceneSymbolView {
     /// The view model for the sample.
     class Model: ObservableObject {
-        /// A map with a topographic basemap.
-        let map = Map(basemapStyle: .arcGISTopographic)
+        /// A scene with an imagery basemap and world elevation surface.
+        let scene: ArcGIS.Scene = {
+            let scene = Scene(basemapStyle: .arcGISImagery)
+            let elevationSource = ArcGISTiledElevationSource(url: .worldElevationService)
+            scene.baseSurface.addElevationSource(elevationSource)
+            return scene
+        }()
+        
+        /// The camera controller focused on the plane graphic.
+        private(set) var cameraController: OrbitGeoElementCameraController!
+        
+        /// The graphics overlay for the plane graphic.
+        let graphicsOverlay: GraphicsOverlay = {
+            let graphicsOverlay = GraphicsOverlay()
+            graphicsOverlay.sceneProperties.surfacePlacement = .relative
+            return graphicsOverlay
+        }()
+        
+        /// The plane graphic created from a distance composite symbol.
+        private let planeGraphic: Graphic = {
+            // Create the different symbols.
+            let planeSymbol = ModelSceneSymbol(url: .bristol, scale: 100)
+            let coneSymbol = SimpleMarkerSceneSymbol.cone(color: .red, diameter: 200, height: 600)
+            coneSymbol.pitch = -90.0
+            let circleSymbol = SimpleMarkerSymbol(style: .circle, color: .red, size: 10)
+            
+            // Create a distance composite symbol using the symbols.
+            let distanceCompositeSymbol = DistanceCompositeSceneSymbol()
+            distanceCompositeSymbol.addRange(
+                DistanceSymbolRange(symbol: planeSymbol, minDistance: 0, maxDistance: 10000)
+            )
+            distanceCompositeSymbol.addRange(
+                DistanceSymbolRange(symbol: coneSymbol, minDistance: 10001, maxDistance: 30000)
+            )
+            distanceCompositeSymbol.addRange(
+                DistanceSymbolRange(symbol: circleSymbol, minDistance: 30001, maxDistance: 0)
+            )
+            
+            // Create a graphic using the distance composite symbol.
+            let planePosition = Point(x: -2.708, y: 56.096, z: 5000, spatialReference: .wgs84)
+            let planeGraphic = Graphic(geometry: planePosition, symbol: distanceCompositeSymbol)
+            
+            return planeGraphic
+        }()
+        
+        init() {
+            graphicsOverlay.addGraphic(planeGraphic)
+            
+            // Create the camera controller targeted on the plane graphic.
+            cameraController = {
+                let cameraController = OrbitGeoElementCameraController(
+                    target: planeGraphic,
+                    distance: 4000
+                )
+                cameraController.cameraPitchOffset = 80
+                cameraController.cameraHeadingOffset = -30
+                return cameraController
+            }()
+        }
     }
 }
 
 private extension URL {
-    // A URL to the local Bristol 3D model files.
+    /// A URL to the local Bristol 3D model files.
     static var bristol: URL {
         Bundle.main.url(forResource: "Bristol", withExtension: "dae", subdirectory: "Bristol")!
     }
-}
-
-#Preview {
-    StylePointWithDistanceCompositeSceneSymbolView()
+    
+    /// A world elevation service from Terrain3D ArcGIS REST service.
+    static var worldElevationService: URL {
+        URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!
+    }
 }
