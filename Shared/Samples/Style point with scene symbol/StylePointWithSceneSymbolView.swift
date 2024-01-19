650d082a1746120d8fc7f7730b2369046f44773c
Caleb Rasmussen <crasmussen@esri.com>
2024-01-19 13:31:38 -0800

    Add source code.

--- Shared/Samples/Style point with scene symbol/StylePointWithSceneSymbolView.swift
+++ Shared/Samples/Style point with scene symbol/StylePointWithSceneSymbolView.swift
@@ -16,23 +16,100 @@ import ArcGIS
 import SwiftUI
 
 struct StylePointWithSceneSymbolView: View {
-    /// The view model for the sample.
-    @StateObject private var model = Model()
+    /// A scene with a topographic basemap and elevation surface.
+    @State private var scene: ArcGIS.Scene = {
+        // Create a scene with an initial viewpoint.
+        let scene = Scene(basemapStyle: .arcGISTopographic)
+        let camera = Camera(
+            latitude: 48.973,
+            longitude: 4.92,
+            altitude: 2082,
+            heading: 60,
+            pitch: 75,
+            roll: 0
+        )
+        scene.initialViewpoint = Viewpoint(latitude: .nan, longitude: .nan, scale: .nan, camera: camera)
+        
+        // Add an elevation source to the base surface.
+        let elevationSource = ArcGISTiledElevationSource(url: .worldElevationService)
+        scene.baseSurface.addElevationSource(elevationSource)
+        
+        return scene
+    }()
     
-    /// The error shown in the error alert.
-    @State private var error: Error?
+    /// The graphics overlay for the scene symbol graphics.
+    @State private var graphicsOverlay: GraphicsOverlay = {
+        let graphicsOverlay = GraphicsOverlay()
+        graphicsOverlay.sceneProperties.surfacePlacement = .absolute
+        return graphicsOverlay
+    }()
+    
+    init() {
+        // Add the scene symbol graphics to the graphics overlay.
+        graphicsOverlay.addGraphics(makeGraphics())
+    }
     
     var body: some View {
-        MapView(map: model.map)
-            .errorAlert(presentingError: $error)
+        // Add the scene and graphics overlay to a scene view.
+        SceneView(scene: scene, graphicsOverlays: [graphicsOverlay])
+    }
+    
+    /// Creates a graphic for each simple marker scene symbol style.
+    /// - Returns: A list of graphics.
+    private func makeGraphics() -> [Graphic] {
+        // Create a simple maker scene symbol for each style.
+        let sceneSymbols = SimpleMarkerSceneSymbol.Style.allCases.map { style in
+            SimpleMarkerSceneSymbol(
+                style: style,
+                color: .random(),
+                height: 200,
+                width: 200,
+                depth: 200,
+                anchorPosition: .center
+            )
+        }
+        
+        // Create a graphic for each scene symbol.
+        let startingX = 4.975
+        let graphics = sceneSymbols.enumerated().map { offset, symbol in
+            let point = Point(
+                x: startingX + 0.01 * Double(offset),
+                y: 49,
+                z: 500,
+                spatialReference: .wgs84
+            )
+            return Graphic(geometry: point, symbol: symbol)
+        }
+        
+        return graphics
     }
 }
 
-private extension StylePointWithSceneSymbolView {
-    /// The view model for the sample.
-    class Model: ObservableObject {
-        /// A map with a topographic basemap.
-        let map = Map(basemapStyle: .arcGISTopographic)
+private extension UIColor {
+    /// Creates a random color whose red, green, and blue values are in the
+    /// range `0...1` and whose alpha value is `1`.
+    /// - Returns: A new `UIColor` object.
+    static func random() -> UIColor {
+        let range: ClosedRange<CGFloat> = 0...1
+        return UIColor(
+            red: .random(in: range),
+            green: .random(in: range),
+            blue: .random(in: range),
+            alpha: 1
+        )
+    }
+}
+
+private extension SimpleMarkerSceneSymbol.Style {
+    static var allCases: [Self] {
+        return [.cone, .cube, .cylinder, .diamond, .sphere, .tetrahedron]
+    }
+}
+
+private extension URL {
+    /// A world elevation service from the Terrain3D ArcGIS REST service.
+    static var worldElevationService: URL {
+        URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!
     }
 }
 
