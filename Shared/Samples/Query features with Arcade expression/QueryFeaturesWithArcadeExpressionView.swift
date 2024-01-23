43df33fabf2c897461b08b6c650aa5b6cba6f1d6
Caleb Rasmussen <crasmussen@esri.com>
2024-01-23 09:54:23 -0800

    Add source code.

--- Shared/Samples/Query features with Arcade expression/QueryFeaturesWithArcadeExpressionView.swift
+++ Shared/Samples/Query features with Arcade expression/QueryFeaturesWithArcadeExpressionView.swift
@@ -19,20 +19,136 @@ struct QueryFeaturesWithArcadeExpressionView: View {
     /// The view model for the sample.
     @StateObject private var model = Model()
     
+    /// The point on the screen where the user tapped.
+    @State private var tapScreenPoint: CGPoint?
+    
+    /// The placement of the callout on the map.
+    @State private var calloutPlacement: CalloutPlacement?
+    
+    /// A Boolean value indicating whether there is a current evaluation operation.
+    @State private var isEvaluating = false
+    
     /// The error shown in the error alert.
     @State private var error: Error?
     
     var body: some View {
-        MapView(map: model.map)
-            .errorAlert(presentingError: $error)
+        MapViewReader { mapViewProxy in
+            MapView(map: model.map)
+                .callout(placement: $calloutPlacement.animation(.default.speed(2))) { placement in
+                    let crimeCount = placement.geoElement?.attributes["Crime_Count"] as? Double ?? 0
+                    Text("Crimes in the last 60 days: \(Int(crimeCount))")
+                        .font(.callout)
+                        .padding(8)
+                }
+                .onSingleTapGesture { screenPoint, _ in
+                    tapScreenPoint = screenPoint
+                }
+                .task(id: tapScreenPoint) {
+                    guard let tapScreenPoint else { return }
+                    
+                    calloutPlacement = nil
+                    isEvaluating = true
+                    defer { isEvaluating = false }
+                    
+                    do {
+                        // Identify the tapped feature using the map view proxy.
+                        let identifyResults = try await mapViewProxy.identifyLayers(
+                            screenPoint: tapScreenPoint,
+                            tolerance: 10
+                        )
+                        
+                        guard let identifiedGeoElements = identifyResults.first?.geoElements,
+                              let identifiedFeature = identifiedGeoElements.first as? ArcGISFeature,
+                              let tapMapPoint = mapViewProxy.location(fromScreenPoint: tapScreenPoint)
+                        else { return }
+                        
+                        // Evaluate the crime count for the feature.
+                        let crimeCount = try await model.crimeCount(for: identifiedFeature)
+                        isEvaluating = false
+                        
+                        // Update the callout with the evaluation results.
+                        identifiedFeature.setAttributeValue(crimeCount, forKey: "Crime_Count")
+                        calloutPlacement = .geoElement(identifiedFeature, tapLocation: tapMapPoint)
+                        
+                        await mapViewProxy.setViewpointCenter(tapMapPoint)
+                    } catch {
+                        self.error = error
+                    }
+                }
+                .overlay(alignment: .center) {
+                    if isEvaluating {
+                        VStack {
+                            Text("Evaluating")
+                            ProgressView()
+                                .progressViewStyle(.circular)
+                        }
+                        .padding()
+                        .background(.ultraThickMaterial)
+                        .cornerRadius(10)
+                        .shadow(radius: 50)
+                    }
+                }
+                .task {
+                    await mapViewProxy.setViewpointScale(2e5)
+                }
+                .errorAlert(presentingError: $error)
+        }
     }
 }
 
 private extension QueryFeaturesWithArcadeExpressionView {
     /// The view model for the sample.
     class Model: ObservableObject {
-        /// A map with a topographic basemap.
-        let map = Map(basemapStyle: .arcGISTopographic)
+        /// A map of the "Crime in Police Beats" portal item.
+        let map: Map = {
+            // Create a portal item using a portal and item ID.
+            let portalItem = PortalItem(
+                portal: .arcGISOnline(connection: .anonymous),
+                id: .crimesInPoliceBeats
+            )
+            
+            // Create a map using the portal item.
+            let map = Map(item: portalItem)
+            return map
+        }()
+        
+        /// The Arcade evaluator for evaluating the crime count of a feature.
+        private let crimeCountEvaluator: ArcadeEvaluator = {
+            // Create a string containing the Arcade expression.
+            let expressionValue = """
+            var crimes = FeatureSetByName($map, 'Crime in the last 60 days');
+            return Count(Intersects($feature, crimes));
+            """
+            
+            // Create an Arcade expression using the string.
+            let expression = ArcadeExpression(expression: expressionValue)
+            
+            // Create an Arcade evaluator with the Arcade expression and an Arcade profile.
+            let evaluator = ArcadeEvaluator(expression: expression, profile: .formCalculation)
+            return evaluator
+        }()
+        
+        /// Evaluates the crime count for a given feature.
+        /// - Parameter feature: The ArcGIS feature evaluate.
+        /// - Returns: The evaluated crime count in the last 60 days.
+        func crimeCount(for feature: ArcGISFeature) async throws -> Double? {
+            // Create the profile variables for the script with the feature and map.
+            let profileVariables: [String: Any] = ["$feature": feature, "$map": map]
+            
+            // Evaluate for the profile variables using the evaluator.
+            let result = try await crimeCountEvaluator.evaluate(withProfileVariables: profileVariables)
+            
+            // Cast the result to get it's value.
+            let crimeCount = result.result(as: .double) as? Double
+            return crimeCount
+        }
+    }
+}
+
+private extension PortalItem.ID {
+    /// The ID used in the "Crimes in Police Beats" portal item.
+    static var crimesInPoliceBeats: Self {
+        Self("539d93de54c7422f88f69bfac2aebf7d")!
     }
 }
 
