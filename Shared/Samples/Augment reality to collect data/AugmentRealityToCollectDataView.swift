512f1e817b9195cec60812286a6ae1e0f615a74e
Destiny Hochhalter <dhochhalterruiz@esri.com>
2024-03-27 14:06:08 -0700

    Add sample model

--- Shared/Samples/Augment reality to collect data/AugmentRealityToCollectDataView.swift
+++ Shared/Samples/Augment reality to collect data/AugmentRealityToCollectDataView.swift
@@ -17,58 +17,16 @@ import ArcGISToolkit
 import SwiftUI
 
 struct AugmentRealityToCollectDataView: View {
-    /// A scene with an imagery basemap.
-    @State private var scene: ArcGIS.Scene = {
-        // Creates an elevation source from Terrain3D REST service.
-        let elevationServiceURL = URL(
-            string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"
-        )!
-        let elevationSource = ArcGISTiledElevationSource(url: elevationServiceURL)
-        let surface = Surface()
-        surface.addElevationSource(elevationSource)
-        surface.backgroundGrid.isVisible = false
-        // Allow camera to go beneath the surface.
-        surface.navigationConstraint = .unconstrained
-        let scene = Scene(basemapStyle: .arcGISImagery)
-        scene.baseSurface = surface
-        scene.baseSurface.opacity = 0
-        return scene
-    }()
-    /// The AR tree survey service feature table.
-    private let featureTable = ServiceFeatureTable(
-        url: URL(string: "https://services2.arcgis.com/ZQgQTuoyBrtmoGdP/arcgis/rest/services/AR_Tree_Survey/FeatureServer/0")!
-    )
-    /// The graphics overlay which shows marker symbols.
-    @State private var graphicsOverlay: GraphicsOverlay = {
-        let graphicsOverlay = GraphicsOverlay()
-        let tappedPointSymbol = SimpleMarkerSceneSymbol(
-            style: .diamond,
-            color: .orange,
-            height: 0.5,
-            width: 0.5,
-            depth: 0.5,
-            anchorPosition: .center
-        )
-        graphicsOverlay.renderer = SimpleRenderer(symbol: tappedPointSymbol)
-        graphicsOverlay.sceneProperties.surfacePlacement = .absolute
-        return graphicsOverlay
-    }()
+    /// The view model for this sample.
+    @StateObject private var model = Model()
     /// The status text displayed to the user.
     @State private var statusText = "Tap to create a feature"
-    /// The selected tree health for the new feature.
-    @State private var treeHealth: TreeHealth?
     /// A Boolean value indicating whether a feature can be added .
     @State private var canAddFeature = false
     /// A Boolean value indicating whether the tree health action sheet is presented.
     @State private var treeHealthSheetIsPresented = false
     /// The error shown in the error alert.
     @State private var error: Error?
-    
-    init() {
-        let featureLayer = FeatureLayer(featureTable: featureTable)
-        featureLayer.sceneProperties.surfacePlacement = .absolute
-        scene.addOperationalLayer(featureLayer)
-    }
     
     var body: some View {
         if #available(iOS 16, *) {
@@ -85,23 +43,23 @@ struct AugmentRealityToCollectDataView: View {
     @MainActor @ViewBuilder var augmentRealityToCollectDataView: some View {
         VStack(spacing: 0) {
             WorldScaleSceneView { _ in
-                SceneView(scene: scene, graphicsOverlays: [graphicsOverlay])
+                SceneView(scene: model.scene, graphicsOverlays: [model.graphicsOverlay])
             }
             .calibrationButtonAlignment(.bottomLeading)
             .onCalibratingChanged { newCalibrating in
-                scene.baseSurface.opacity = newCalibrating ? 0.5 : 0
+                model.scene.baseSurface.opacity = newCalibrating ? 0.5 : 0
             }
             .onSingleTapGesture { _, scenePoint in
-                graphicsOverlay.removeAllGraphics()
+                model.graphicsOverlay.removeAllGraphics()
                 canAddFeature = true
                 
                 // Add feature graphic.
-                graphicsOverlay.addGraphic(Graphic(geometry: scenePoint))
+                model.graphicsOverlay.addGraphic(Graphic(geometry: scenePoint))
                 statusText = "Placed relative to ARKit plane"
             }
             .task {
                 do {
-                    try await featureTable.load()
+                    try await model.featureTable.load()
                 } catch {
                     self.error = error
                 }
@@ -130,7 +88,16 @@ struct AugmentRealityToCollectDataView: View {
                     actions: {
                         ForEach(TreeHealth.allCases, id: \.self) { treeHealth in
                             Button(treeHealth.label) {
-                                addTree(health: treeHealth)
+                                statusText = "Adding feature"
+                                Task {
+                                    do {
+                                        try await model.addTree(health: treeHealth)
+                                        statusText = "Tap to create a feature"
+                                        canAddFeature = false
+                                    } catch {
+                                        self.error = error
+                                    }
+                                }
                             }
                         }
                     }, message: {
@@ -140,41 +107,85 @@ struct AugmentRealityToCollectDataView: View {
         }
         .errorAlert(presentingError: $error)
     }
-    
-    /// Adds a feature to represent a tree to the tree survey service feature table.
-    /// - Parameter treeHealth: The health of the tree.
-    private func addTree(health: TreeHealth) {
-        guard let featureGraphic = graphicsOverlay.graphics.first,
-              let featurePoint = featureGraphic.geometry as? Point else { return }
+}
+
+private extension AugmentRealityToCollectDataView {
+    @MainActor
+    class Model: ObservableObject {
+        /// A scene with an imagery basemap.
+        @State var scene: ArcGIS.Scene = {
+            // Creates an elevation source from Terrain3D REST service.
+            let elevationServiceURL = URL(
+                string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"
+            )!
+            let elevationSource = ArcGISTiledElevationSource(url: elevationServiceURL)
+            let surface = Surface()
+            surface.addElevationSource(elevationSource)
+            surface.backgroundGrid.isVisible = false
+            // Allow camera to go beneath the surface.
+            surface.navigationConstraint = .unconstrained
+            let scene = Scene(basemapStyle: .arcGISImagery)
+            scene.baseSurface = surface
+            scene.baseSurface.opacity = 0
+            return scene
+        }()
+        /// The AR tree survey service feature table.
+        let featureTable = ServiceFeatureTable(
+            url: URL(string: "https://services2.arcgis.com/ZQgQTuoyBrtmoGdP/arcgis/rest/services/AR_Tree_Survey/FeatureServer/0")!
+        )
+        /// The graphics overlay which shows marker symbols.
+        @State var graphicsOverlay: GraphicsOverlay = {
+            let graphicsOverlay = GraphicsOverlay()
+            let tappedPointSymbol = SimpleMarkerSceneSymbol(
+                style: .diamond,
+                color: .orange,
+                height: 0.5,
+                width: 0.5,
+                depth: 0.5,
+                anchorPosition: .center
+            )
+            graphicsOverlay.renderer = SimpleRenderer(symbol: tappedPointSymbol)
+            graphicsOverlay.sceneProperties.surfacePlacement = .absolute
+            return graphicsOverlay
+        }()
+        /// The selected tree health for the new feature.
+        @State private var treeHealth: TreeHealth?
         
-        statusText = "Adding feature"
+        init() {
+            let featureLayer = FeatureLayer(featureTable: featureTable)
+            featureLayer.sceneProperties.surfacePlacement = .absolute
+            scene.addOperationalLayer(featureLayer)
+        }
         
-        // Create attributes for the new feature.
-        let featureAttributes: [String: Any] = [
-            "Health": health.rawValue,
-            "Height": 3.2,
-            "Diameter": 1.2
-        ]
-        
-        if let newFeature = featureTable.makeFeature(
-            attributes: featureAttributes,
-            geometry: featurePoint
-        ) as? ArcGISFeature {
-            Task {
+        /// Adds a feature to represent a tree to the tree survey service feature table.
+        /// - Parameter treeHealth: The health of the tree.
+        func addTree(health: TreeHealth) async throws {
+            guard let featureGraphic = graphicsOverlay.graphics.first,
+                  let featurePoint = featureGraphic.geometry as? Point else { return }
+            
+            // Create attributes for the new feature.
+            let featureAttributes: [String: Any] = [
+                "Health": health.rawValue,
+                "Height": 3.2,
+                "Diameter": 1.2
+            ]
+            
+            if let newFeature = featureTable.makeFeature(
+                attributes: featureAttributes,
+                geometry: featurePoint
+            ) as? ArcGISFeature {
                 do {
                     // Add the feature to the feature table.
                     try await featureTable.add(newFeature)
                     _ = try await featureTable.applyEdits()
                 } catch {
-                    self.error = error
+                    throw error
                 }
+                newFeature.refresh()
             }
-            newFeature.refresh()
+            
+            graphicsOverlay.removeAllGraphics()
         }
-        
-        statusText = "Tap to create a feature"
-        graphicsOverlay.removeAllGraphics()
-        canAddFeature = false
     }
 }
 
