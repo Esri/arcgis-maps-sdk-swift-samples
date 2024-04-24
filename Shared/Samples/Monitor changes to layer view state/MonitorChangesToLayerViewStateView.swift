519bf47d2ff8975b69eced11f98f06929f6c3ee6
Caleb Rasmussen <crasmussen@esri.com>
2024-04-24 11:14:00 -0700

    Add source code.

--- Shared/Samples/Monitor changes to layer view state/MonitorChangesToLayerViewStateView.swift
+++ Shared/Samples/Monitor changes to layer view state/MonitorChangesToLayerViewStateView.swift
@@ -16,19 +16,102 @@ import ArcGIS
 import SwiftUI
 
 struct MonitorChangesToLayerViewStateView: View {
-    /// The view model for the sample.
-    @StateObject private var model = Model()
+    /// A map with a topographic basemap.
+    @State private var map: Map = {
+        let map = Map(basemapStyle: .arcGISTopographic)
+        map.initialViewpoint = Viewpoint(
+            center: Point(x: -13e6, y: 51e5, spatialReference: .webMercator),
+            scale: 2e7
+        )
+        return map
+    }()
+    
+    /// The Satellite (MODIS) Thermal Hotspots and Fire Activity feature layer.
+    @State private var featureLayer: FeatureLayer = {
+        let portalItem = PortalItem(url: .thermalHotspotsAndFireActivity)!
+        let featureLayer = FeatureLayer(item: portalItem)
+        
+        featureLayer.minScale = 1e8
+        featureLayer.maxScale = 6e6
+        
+        return featureLayer
+    }()
+    
+    /// The text representing the view status of the feature layer.
+    @State private var statusText = ""
+    
+    /// A Boolean value indicating whether the feature layer is visible.
+    @State private var layerIsVisible = true
     
     var body: some View {
-        MapView(map: model.map)
+        MapView(map: map)
+            .onLayerViewStateChanged { layer, layerViewState in
+                // Only checks the view state of the feature layer.
+                guard layer.id == featureLayer.id else { return }
+                
+                // Gets the status from the view state for the layer.
+                statusText = layerViewState.status.label
+            }
+            .overlay(alignment: .top) {
+                Text("Layer view status:\n\(statusText)")
+                    .multilineTextAlignment(.center)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding(8)
+                    .background(.regularMaterial, ignoresSafeAreaEdges: .horizontal)
+            }
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Toggle(
+                        layerIsVisible ? "Layer Enabled" : "Layer Disabled",
+                        isOn: $layerIsVisible
+                    )
+                    .onChange(of: layerIsVisible) { newValue in
+                        featureLayer.isVisible = newValue
+                    }
+                }
+            }
+            .onAppear {
+                // Adds the feature layer to the map.
+                map.addOperationalLayer(featureLayer)
+            }
     }
 }
 
-private extension MonitorChangesToLayerViewStateView {
-    /// The view model for the sample.
-    class Model: ObservableObject {
-        /// A map with a topographic basemap.
-        let map = Map(basemapStyle: .arcGISTopographic)
+private extension LayerViewState.Status {
+    /// A human-readable label for the status.
+    var label: String {
+        var statuses: [String] = []
+        if self.contains(.active) {
+            statuses.append("Active")
+        }
+        if self.contains(.notVisible) {
+            statuses.append("Not Visible")
+        }
+        if self.contains(.outOfScale) {
+            statuses.append("Out of Scale")
+        }
+        if self.contains(.loading) {
+            statuses.append("Loading")
+        }
+        if self.contains(.error) {
+            statuses.append("Error")
+        }
+        if self.contains(.warning) {
+            statuses.append("Warning")
+        }
+        
+        if !statuses.isEmpty {
+            return statuses.joined(separator: ", ")
+        } else {
+            return "Unknown"
+        }
+    }
+}
+
+private extension URL {
+    /// The URL for the Satellite (MODIS) Thermal Hotspots and Fire Activity portal item.
+    static var thermalHotspotsAndFireActivity: URL {
+        URL(string: "https://runtime.maps.arcgis.com/home/item.html?id=b8f4033069f141729ffb298b7418b653")!
     }
 }
 
