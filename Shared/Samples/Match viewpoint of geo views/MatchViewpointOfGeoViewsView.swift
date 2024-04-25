83496ddc3d3c478f544623763de107b3f3fa1a37
Caleb Rasmussen <crasmussen@esri.com>
2024-04-25 14:34:00 -0700

    Add source code.

--- Shared/Samples/Match viewpoint of geo views/MatchViewpointOfGeoViewsView.swift
+++ Shared/Samples/Match viewpoint of geo views/MatchViewpointOfGeoViewsView.swift
@@ -16,11 +16,43 @@ import ArcGIS
 import SwiftUI
 
 struct MatchViewpointOfGeoViewsView: View {
-    /// A map with a topographic basemap.
-    @State private var map = Map(basemapStyle: .arcGISTopographic)
+    /// A map with an imagery basemap.
+    @State private var map = Map(basemapStyle: .arcGISImagery)
+    
+    /// A scene with an imagery basemap.
+    @State private var scene = Scene(basemapStyle: .arcGISImagery)
+    
+    /// The viewpoint of the map view.
+    @State private var mapViewpoint: Viewpoint?
+    
+    /// The viewpoint of the scene view.
+    @State private var sceneViewpoint: Viewpoint?
+    
+    /// A Boolean value indicating whether the map view is currently being navigated.
+    @State private var mapIsNavigating = false
+    
+    /// A Boolean value indicating whether the map view is currently being navigated.
+    @State private var sceneIsNavigating = false
     
     var body: some View {
-        MapView(map: map)
+        VStack(spacing: 0) {
+            MapView(map: map, viewpoint: mapViewpoint)
+                .onNavigatingChanged { mapIsNavigating = $0 }
+                .onViewpointChanged(kind: .centerAndScale) { newViewpoint in
+                    // Sets the scene's viewpoint to the map's when the map is navigating,
+                    // or when the scene isn't navigating, e.i., when the viewpoint is first set.
+                    guard mapIsNavigating || !sceneIsNavigating else { return }
+                    sceneViewpoint = newViewpoint
+                }
+            
+            SceneView(scene: scene, viewpoint: sceneViewpoint)
+                .onNavigatingChanged { sceneIsNavigating = $0 }
+                .onViewpointChanged(kind: .centerAndScale) { newViewpoint in
+                    // Sets the map's viewpoint to the scene's when the scene is navigating.
+                    guard sceneIsNavigating else { return }
+                    mapViewpoint = newViewpoint
+                }
+        }
     }
 }
 
