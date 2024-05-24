be7bd04d12c7e2ff4055751332d1dc0f966a9b49
Christopher Webb <chr13795@c02z65k8lvdq.esri.com>
2024-05-24 14:03:13 -0700

    Initial commit for branch - add in files.

---
+++ Shared/Samples/Select features in scene layer/SelectFeaturesInSceneLayerView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectFeaturesInSceneLayerView.swift
+//  Samples
+//
+//  Created by Christopher Webb on 5/24/24.
+//  Copyright © 2024 Esri. All rights reserved.
+//
+
+import SwiftUI
+import ArcGIS
+
+struct SelectFeaturesInSceneLayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectFeaturesInSceneLayerView()
+}
