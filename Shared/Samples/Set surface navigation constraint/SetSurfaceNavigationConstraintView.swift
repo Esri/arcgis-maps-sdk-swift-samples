25636d6f7f2e7fa680140fe61cfb676ebc8284bf
Christopher Webb <christopher.webb@columbia.edu>
2024-05-31 13:23:49 -0700

    update comments and sort the samples

--- Shared/Samples/Set surface navigation constraint/SetSurfaceNavigationConstraintView.swift
+++ Shared/Samples/Set surface navigation constraint/SetSurfaceNavigationConstraintView.swift
@@ -16,16 +16,22 @@ import ArcGIS
 import SwiftUI
 
 struct SetSurfaceNavigationConstraintView: View {
-    
+    /// The Webscene with a portal item.
     @State private var scene: ArcGIS.Scene = {
-        let portalID = PortalItem.ID("91a4fafd747a47c7bab7797066cb9272")!
-        let portalItem = PortalItem(portal: .arcGISOnline(connection: .anonymous), id: portalID)
+        // Creates the portal item using the id of the Webscene in the ArcGIS portal.
+        let portalItem = PortalItem(
+            portal: .arcGISOnline(connection: .anonymous),
+            id: PortalItem.ID("91a4fafd747a47c7bab7797066cb9272")!
+        )
+        // Creates the scene using the portal item.
         let scene = Scene(item: portalItem)
+        // Sets the navigation constraint on the scene's base surface to unconstrained which allowd the camera pass above and below the elevation surface.
         scene.baseSurface.navigationConstraint = .unconstrained
         return scene
     }()
 
     var body: some View {
+        // Displays the Webscene in the SceneView
         SceneView(scene: scene)
     }
 }
