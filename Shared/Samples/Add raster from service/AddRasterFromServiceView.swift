e58117b9fdc16c5cc7234b28339fb6567bdbf54e
Christopher Webb <christopher.webb@columbia.edu>
2024-06-03 14:14:11 -0700

    add in changed to projectfile, view file and README

---
+++ Shared/Samples/Add raster from service/AddRasterFromServiceView.swift
@@ -0,0 +1,48 @@
+//
+//  AddRasterFromServiceView.swift
+//  Samples
+//
+//  Created by Christopher Webb on 6/3/24.
+//  Copyright © 2024 Esri. All rights reserved.
+//
+
+import SwiftUI
+import ArcGIS
+
+struct AddRasterFromServiceView: View {
+    /// The error shown in the error alert.
+    @State private var error: Error?
+    
+    /// The current viewpoint of the map view.
+    @State private var viewpoint: Viewpoint?
+    
+    /// A map with a topographic basemap.
+    @State private var map: Map = {
+        let map = Map(basemapStyle: .arcGISDarkGrayBase)
+        let imageServiceURL = URL(string: "https://gis.ngdc.noaa.gov/arcgis/rest/services/bag_bathymetry/ImageServer")!
+        let imageServiceRaster = ImageServiceRaster(url: imageServiceURL)
+        let rasterLayer = RasterLayer(raster: imageServiceRaster)
+        map.addOperationalLayer(rasterLayer)
+        return map
+    }()
+    
+    var body: some View {
+        MapViewReader { mapViewProxy in
+            MapView(map: map, viewpoint: viewpoint)
+                .onViewpointChanged(kind: .centerAndScale) { viewpoint = $0 }
+                .task {
+                    do {
+                        let rasterLayer = map.operationalLayers.first! as! RasterLayer
+                        try await rasterLayer.load()
+                    } catch {
+                        // Presents an error message if the raster fails to load.
+                        self.error = error
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    AddRasterFromServiceView()
+}
