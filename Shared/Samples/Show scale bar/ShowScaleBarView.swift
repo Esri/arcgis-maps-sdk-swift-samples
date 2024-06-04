c2a842f95053ee3c7f0a2e9887cec4cf8e99e571
Christopher Webb <christopher.webb@columbia.edu>
2024-06-04 16:40:35 -0700

    add in file for project

---
+++ Shared/Samples/Show scale bar/ShowScaleBarView.swift
@@ -0,0 +1,19 @@
+//
+//  ShowScaleBarView.swift
+//  Samples
+//
+//  Created by Christopher Webb on 6/4/24.
+//  Copyright © 2024 Esri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ShowScaleBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShowScaleBarView()
+}
