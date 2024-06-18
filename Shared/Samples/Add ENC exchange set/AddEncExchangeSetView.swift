b0a56812978176a49c506c8b86ec515890da95e2
Christopher Webb <christopher.webb@columbia.edu>
2024-06-18 11:40:07 -0700

    add in files, add in reference to offline data in metadata.json

---
+++ Shared/Samples/Add ENC exchange set/AddEncExchangeSetView.swift
@@ -0,0 +1,19 @@
+//
+//  AddEncExchangeSetView.swift
+//  Samples
+//
+//  Created by Christopher Webb on 6/18/24.
+//  Copyright © 2024 Esri. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddEncExchangeSetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddEncExchangeSetView()
+}
