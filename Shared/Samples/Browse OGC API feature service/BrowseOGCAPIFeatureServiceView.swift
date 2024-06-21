867eafa95ef4f7a7818328bbd5325bc04f1e7056
Christopher Webb <christopher.webb@columbia.edu>
2024-06-21 13:28:02 -0700

    add in files

---
+++ Shared/Samples/Browse OGC API feature service/BrowseOGCAPIFeatureServiceView.swift
@@ -0,0 +1,19 @@
+//
+//  BrowseOGCAPIFeatureServiceView.swift
+//  Samples
+//
+//  Created by Christopher Webb on 6/21/24.
+//  Copyright © 2024 Esri. All rights reserved.
+//
+
+import SwiftUI
+
+struct BrowseOGCAPIFeatureServiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BrowseOGCAPIFeatureServiceView()
+}
