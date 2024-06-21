f34e527d8e073e25a096075218dea41eb65fd70c
Christopher Webb <christopher.webb@columbia.edu>
2024-06-21 14:51:28 -0700

    add in files

---
+++ Shared/Samples/Show service area/ShowServiceAreaView.swift
@@ -0,0 +1,19 @@
+//
+//  ShowServiceAreaView.swift
+//  Samples
+//
+//  Created by Christopher Webb on 6/21/24.
+//  Copyright © 2024 Esri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ShowServiceAreaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShowServiceAreaView()
+}
