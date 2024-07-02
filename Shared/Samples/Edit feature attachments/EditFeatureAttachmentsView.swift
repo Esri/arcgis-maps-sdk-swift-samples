65b57523e2d8ea7844e943e3ebf43a3d90f01dcc
Christopher Webb <christopher.webb@columbia.edu>
2024-07-02 00:52:46 -0700

    update for feature attachment logic

--- Shared/Samples/Edit feature attachments/EditFeatureAttachmentsView.swift
+++ Shared/Samples/Edit feature attachments/EditFeatureAttachmentsView.swift
@@ -21,10 +21,17 @@ struct EditFeatureAttachmentsView: View {
     
     @State private var tapPoint: CGPoint?
     
+    @State var attachments: [Attachment] = []
+    
+    /// The current draw status of the map.
+    @State private var currentDrawStatus: DrawStatus = .inProgress
+    
     /// The data model for the sample.
     @StateObject private var model = Model()
     
+    @State private var loaded = false
     
+    @State private var showingSheet = false
     
     var body: some View {
         MapViewReader { mapProxy in
@@ -32,9 +39,72 @@ struct EditFeatureAttachmentsView: View {
                 .callout(placement: $model.calloutPlacement
                     .animation(model.calloutShouldOffset ? nil : .default.speed(2))
                 ) { _ in
-                    Text(model.calloutText)
-                        .font(.callout)
+                    HStack {
+                        VStack {
+                            Text(model.calloutText)
+                                .font(.callout)
+                                .multilineTextAlignment(.leading)
+                            Text(model.calloutDetailText)
+                                .font(.footnote)
+                                .multilineTextAlignment(.leading)
+                        }
                         .padding(8)
+                        Button("Detail", systemImage: "exclamationmark.circle.fill") {
+                            showingSheet = true
+                        }
+                        .sheet(isPresented: $showingSheet) {
+                            VStack {
+                                Spacer()
+                                List {
+                                    ForEach(0...attachments.count, id: \.self) { index in
+                                        if index == attachments.count {
+                                            Button("Add Attachment") {
+                                                print("here")
+                                            }
+                                        } else {
+                                            HStack {
+                                                Text("\(attachments[index].name)")
+                                                    .font(.footnote)
+                                                Spacer()
+                                                Button("Download") {
+                                                    Task {
+                                                        do {
+                                                            let data = try await attachments[index].data
+                                                            print(data)
+                                                        } catch {
+                                                            self.error = error
+                                                        }
+                                                    }
+                                                }
+                                                .padding(8)
+                                                .border(.black, width: 1)
+                                                .font(.footnote)
+                                                Button("Delete") {
+                                                    Task {
+                                                        do {
+                                                            try await model.delete(attachment: attachments[index])
+                                                        } catch {
+                                                            self.error = error
+                                                        }
+                                                    }
+                                                }
+                                                .padding(8)
+                                                .border(.black, width: 1)
+                                                .font(.footnote)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        .padding(8)
+                    }
+                }
+                .onDrawStatusChanged { drawStatus in
+                    // Updates the state when the map's draw status changes.
+                    if drawStatus == .completed {
+                        loaded = true
+                    }
                 }
                 .onSingleTapGesture { tap, _ in
                     self.tapPoint = tap
@@ -43,20 +113,40 @@ struct EditFeatureAttachmentsView: View {
                     guard let point = tapPoint else { return }
                     model.featureLayer.clearSelection()
                     do {
-                        let result = try await mapProxy.identify(on: model.featureLayer, screenPoint: point, tolerance: 12)
-                        guard let feature = result.geoElements.first as? ArcGISFeature else { return }
-                        model.featureLayer.selectFeature(feature)
-                        let title = feature.attributes["typdamage"] as? String
-                        
-                        model.updateCalloutPlacement(to: point, using: mapProxy)
-                        model.calloutText = title ?? "Callout"
-                        try await feature.load()
-                        var attachments = try await feature.attachments
+                        let result = try await mapProxy.identify(
+                            on: model.featureLayer,
+                            screenPoint: point,
+                            tolerance: 5
+                        )
+                        guard let features = result.geoElements as? [ArcGISFeature],
+                              let feature = features.first else {
+                            return
+                        }
                         model.selectedFeature = feature
-                        print(feature.attributes)
-                        print(feature.canEditAttachments)
+                        model.selectFeature()
+                        if let selectedFeature = model.selectedFeature {
+                            let title = selectedFeature.attributes["typdamage"] as? String
+                            try await selectedFeature.load()
+                            guard let pngData = UIImage(named: "Layers-icon")?.pngData() else { return }
+                            try await model.add(name: "Attachment.png", type: "png", dataElement: pngData)
+                            let attachments = try await selectedFeature.attachments
+                            self.attachments = attachments
+                            print(attachments)
+                            model.updateCalloutPlacement(to: point, using: mapProxy)
+                            model.calloutText = title ?? "Callout"
+                            model.calloutDetailText = " \(attachments.count) attachments"
+                        }
                     } catch {
                         self.error = error
+                    }
+                }
+                .overlay(alignment: .center) {
+                    if !loaded {
+                        ProgressView("Drawing…")
+                            .padding()
+                            .background(.ultraThinMaterial)
+                            .cornerRadius(10)
+                            .shadow(radius: 50)
                     }
                 }
         }
@@ -84,12 +174,14 @@ private extension EditFeatureAttachmentsView {
         /// The text shown on the callout.
         @Published var calloutText: String = ""
         
+        /// The text shown on the callout.
+        @Published var calloutDetailText: String = ""
+        
         /// A Boolean value that indicates whether the callout placement should be offsetted for the map magnifier.
         @Published var calloutShouldOffset = false
         
         var featureLayer: FeatureLayer = {
-            let featureServiceURL = URL(string: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/DamageAssessment/FeatureServer/0")!
-            let featureTable = ServiceFeatureTable(url: featureServiceURL)
+            let featureTable = ServiceFeatureTable(url: .featureServiceURL)
             var featureLayer = FeatureLayer(featureTable: featureTable)
             return featureLayer
         }()
@@ -98,22 +190,6 @@ private extension EditFeatureAttachmentsView {
             map.addOperationalLayer(featureLayer)
         }
         
-//        /// Creates a callout displaying the data of a raster cell at a given screen point.
-//        /// - Parameters:
-//        ///   - screenPoint: The screen point of the raster cell at which to place the callout.
-//        ///   - mapViewProxy: The proxy used to handle the screen point.
-//        func callout(at screenPoint: CGPoint, using proxy: MapViewProxy) async {
-//            // Get the raster cell for the screen point using the map view proxy.
-//            if let rasterCell = await rasterCell(at: screenPoint, using: proxy) {
-//                // Update the callout text and placement.
-//                updateCalloutText(using: rasterCell)
-//                updateCalloutPlacement(to: screenPoint, using: proxy)
-//            } else {
-//                // Dismiss the callout if no raster cell was found, e.g. tap was not on layer.
-//                calloutPlacement = nil
-//            }
-//        }
-        
         /// Updates the location of the callout placement to a given screen point.
         /// - Parameters:
         ///   - screenPoint: The screen point at which to place the callout.
@@ -121,35 +197,44 @@ private extension EditFeatureAttachmentsView {
         func updateCalloutPlacement(to screenPoint: CGPoint, using proxy: MapViewProxy) {
             // Create an offset to offset the callout if needed, e.g. the magnifier is showing.
             let offset = calloutShouldOffset ? CGPoint(x: 0, y: -70) : .zero
-            
             // Get the map location of the screen point from the map view proxy.
             if let location = proxy.location(fromScreenPoint: screenPoint) {
                 calloutPlacement = .location(location, offset: offset)
             }
         }
         
-//        /// Updates the text shown in the callout using the attributes and coordinates of a given raster cell.
-//        /// - Parameter cell: The raster cell to create the text from.
-//        private func updateCalloutText(using cell: RasterCell) {
-//            // Create the attributes text using the attributes of the raster cell.
-//            let attributes = cell.attributes
-//                .map { "\($0.key): \($0.value)" }
-//                .sorted(by: >)
-//                .joined(separator: "\n")
-//            
-//            // Create the coordinate texts using the extent of the cell's geometry.
-//            guard let extent = cell.geometry?.extent else {
-//                calloutText = attributes
-//                return
-//            }
-//            let roundedStyle = FloatingPointFormatStyle<Double>.number.rounded(rule: .awayFromZero, increment: 0.001)
-//            let xCoordinate = "X: \(extent.xMin.formatted(roundedStyle))"
-//            let yCoordinate = "Y: \(extent.yMin.formatted(roundedStyle))"
-//            
-//            // Update the callout text.
-//            calloutText = "\(attributes)\n\n\(xCoordinate)\n\(yCoordinate)"
-//        }
+        func selectFeature() {
+            if let selectedFeature = selectedFeature {
+                featureLayer.selectFeature(selectedFeature)
+            }
+        }
+        
+        func add(name: String, type: String, dataElement: Data) async throws {
+            try await selectedFeature?.addAttachment(named: name, contentType: type, data: dataElement)
+        }
+        
+        func delete(attachment: Attachment) async throws {
+            try await selectedFeature?.deleteAttachment(attachment)
+        }
+        
+        func edit(attachment: Attachment, named: String, typed: String, dataElement: Data) async throws {
+            try await selectedFeature?.updateAttachment(attachment, name: named, contentType: typed, data: dataElement)
+        }
+        
+        func applyEdits() async throws {
+            if let serviceTable = featureLayer.featureTable as? ServiceFeatureTable {
+                let edits = try await serviceTable.applyEdits()
+                for edited in edits {
+                    let result = edited.attachmentResults
+                    print(result)
+                }
+            }
+        }
     }
+}
+
+private extension URL {
+    static let featureServiceURL =  URL(string: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/DamageAssessment/FeatureServer/0")!
 }
 
 #Preview {
