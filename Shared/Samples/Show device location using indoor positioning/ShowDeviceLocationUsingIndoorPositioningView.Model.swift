68793b97174d38d914e2e21e0cc1291e08c49d3d
Christopher Webb <christopher.webb@columbia.edu>
2024-07-16 15:55:19 -0700

    add in functionality

--- Shared/Samples/Show device location using indoor positioning/ShowDeviceLocationUsingIndoorPositioningView.Model.swift
+++ Shared/Samples/Show device location using indoor positioning/ShowDeviceLocationUsingIndoorPositioningView.Model.swift
@@ -13,8 +13,91 @@
 // limitations under the License.
 
 import ArcGIS
+import CoreLocation
 import SwiftUI
 
 extension ShowDeviceLocationUsingIndoorPositioningView {
-    
+    @MainActor
+    class Model: ObservableObject {
+        /// A indoors location data source based on sensor data, including but not
+        /// limited to radio, GPS, motion sensors.
+        @Published var indoorsLocationDataSource: IndoorsLocationDataSource?
+        @Published var currentFloor: Int = 0
+        
+        @Published var sensorCount: Int!
+        
+        @Published var satelliteCount: Int!
+        
+        @Published var horizontalAccuracy: Double!
+        
+        /// The map's location display.
+        @Published var locationDisplay = LocationDisplay()
+        
+        @Published var enveloper: Envelope?
+        
+        @Published var source: String = ""
+        
+        @Published var levelID: UUID?
+        
+        /// The measurement formatter for sensor accuracy.
+        let measurementFormatter: MeasurementFormatter = {
+            let formatter = MeasurementFormatter()
+            formatter.unitStyle = .short
+            formatter.unitOptions = .providedUnit
+            return formatter
+        }()
+        
+        func setIndoorDatasource(map: Map) async throws {
+            locationDisplay.autoPanMode = .navigation
+            try await map.indoorPositioningDefinition?.load()
+            indoorsLocationDataSource = IndoorsLocationDataSource(definition: map.indoorPositioningDefinition!)
+            locationDisplay.dataSource = indoorsLocationDataSource!
+            try await startLocationDisplay()
+            try await updateLocation(map: map)
+        }
+        
+        func updateLocation(map: Map) async throws {
+            for try await location in locationDisplay.dataSource.locations {
+                if let floorLevel = location.additionalSourceProperties[.floor] as? Int {
+                    currentFloor = floorLevel
+                    try await displayFeatures(map: map, onFloor: currentFloor + 1)
+                }
+                
+                if let floorLevelID = location.additionalSourceProperties[.floorLevelID] as? UUID {
+                    levelID = floorLevelID
+                }
+                source = location.additionalSourceProperties[.positionSource] as? String ?? "NA"
+                switch source {
+                case "GNSS":
+                    satelliteCount = location.additionalSourceProperties[.satelliteCount] as? Int ?? 0
+                default:
+                    sensorCount = location.additionalSourceProperties[.transmitterCount] as? Int ?? 0
+                }
+                horizontalAccuracy = location.horizontalAccuracy
+            }
+        }
+        
+        /// Starts the location display to show user's location on the map.
+        func startLocationDisplay() async throws {
+            // Request location permission if it has not yet been determined.
+            let locationManager = CLLocationManager()
+            if locationManager.authorizationStatus == .notDetermined {
+                locationManager.requestWhenInUseAuthorization()
+            }
+            // Start the location display to zoom to the user's current location.
+            try await locationDisplay.dataSource.start()
+        }
+        
+        /// Display features on a certain floor level using definition expression.
+        /// - Parameter floor: The floor level of the features to be displayed.
+        func displayFeatures(map: Map, onFloor floor: Int) async throws {
+            for layer in map.operationalLayers {
+                if layer.name == "Details" || layer.name == "Units" || layer.name == "Levels" {
+                    if let featureLayer = layer as? FeatureLayer {
+                        featureLayer.definitionExpression = "VERTICAL_ORDER = \(floor)"
+                    }
+                }
+            }
+        }
+    }
 }
--- Shared/Samples/Show device location using indoor positioning/ShowDeviceLocationUsingIndoorPositioningView.swift
+++ Shared/Samples/Show device location using indoor positioning/ShowDeviceLocationUsingIndoorPositioningView.swift
@@ -18,9 +18,74 @@
 import ArcGIS
 import SwiftUI
 
+// "DO NOT PUSH"
+
 struct ShowDeviceLocationUsingIndoorPositioningView: View {
+    @StateObject private var model = Model()
+    /// The error shown in the error alert.
+    @State private var error: Error?
+    
+    @State private var map = Map(basemapStyle: .arcGISTopographic)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        MapViewReader { mapProxy in
+            MapView(map: map)
+                .locationDisplay(model.locationDisplay)
+                .overlay(alignment: .center) {
+                    if model.currentFloor > 0 {
+                        if let accuracy = model.horizontalAccuracy {
+                            let text = model.measurementFormatter.string(from: Measurement(value: accuracy, unit: UnitLength.meters))
+                            Text("Current Floor: \(model.currentFloor) accuracy: \(text)")
+                        }
+                    } else {
+                        Text("No floor data")
+                    }
+                }
+                .overlay(alignment: .centerFirstTextBaseline) {
+                    Text(model.source)
+                }
+                .overlay(alignment: .topLeading) {
+                    if let sensorCount = model.sensorCount {
+                        Text("Sensors \(sensorCount)")
+                    } else {
+                        Text("No Sensors")
+                    }
+                }
+                .overlay(alignment: .topTrailing) {
+                    if let satelliteCount = model.satelliteCount {
+                        Text("Sattelites \(satelliteCount)")
+                    } else {
+                        Text("No Sattelites")
+                    }
+                }
+                .task {
+                    do {
+                        try await map.load()
+                        try await model.setIndoorDatasource(map: map)
+                    } catch {
+                        self.error = error
+                    }
+                }
+                .errorAlert(presentingError: $error)
+        }
+        .onAppear {
+            ArcGISEnvironment.apiKey = nil
+            ArcGISEnvironment.authenticationManager.arcGISAuthenticationChallengeHandler = ChallengeHandler()
+            map = Map(url: URL(string: "")!)!
+        }
+        .onDisappear {
+            ArcGISEnvironment.authenticationManager.arcGISAuthenticationChallengeHandler = nil
+        }
+    }
+}
+
+private struct ChallengeHandler: ArcGISAuthenticationChallengeHandler {
+    func handleArcGISAuthenticationChallenge(
+        _ challenge: ArcGISAuthenticationChallenge
+    ) async throws -> ArcGISAuthenticationChallenge.Disposition {
+        return .continueWithCredential(
+            try await TokenCredential.credential(for: challenge, username: "", password: "")
+        )
     }
 }
 
