e96b6a1a81caa358f8628a75711f0a72d2b18873
Christopher Webb <christopher.webb@columbia.edu>
2024-07-16 11:02:38 -0700

    add in files

---
+++ Shared/Samples/Show device location using indoor positioning/ShowDeviceLocationUsingIndoorPositioningView.Model.swift
@@ -0,0 +1,13 @@
+//
+//  ShowDeviceLocationUsingIndoorPositioningView.Model.swift
+//  Samples
+//
+//  Created by Christopher Webb on 7/16/24.
+//  Copyright © 2024 Esri. All rights reserved.
+//
+
+import SwiftUI
+
+extension ShowDeviceLocationUsingIndoorPositioningView {
+    
+}
---
+++ Shared/Samples/Show device location using indoor positioning/ShowDeviceLocationUsingIndoorPositioningView.swift
@@ -0,0 +1,19 @@
+//
+//  ShowDeviceLocationUsingIndoorPositioningView.swift
+//  Samples
+//
+//  Created by Christopher Webb on 7/16/24.
+//  Copyright © 2024 Esri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ShowDeviceLocationUsingIndoorPositioningView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShowDeviceLocationUsingIndoorPositioningView()
+}
