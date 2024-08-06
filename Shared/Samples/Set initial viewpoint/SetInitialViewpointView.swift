03fbcd7960cfbee979b4cb97b51d758204745ba9
Fredy Camas <fredycamasdev@gmail.com>
2024-08-05 17:39:23 -0700

    Add readme and source code.

--- Shared/Samples/Set initial viewpoint/SetInitialViewpointView.swift
+++ Shared/Samples/Set initial viewpoint/SetInitialViewpointView.swift
@@ -12,14 +12,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import ArcGIS
 import SwiftUI
 
 struct SetInitialViewpointView: View {
+    /// A map with a basemap and an initial viewpoint
+    @State private var map: Map = {
+        let map = Map(basemapStyle: .arcGISImageryStandard)
+        map.initialViewpoint = Viewpoint(boundingGeometry: .potashPonds)
+        return map
+    }()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        MapView(map: map)
     }
 }
 
+private extension Geometry {
+    /// The area around Potash Ponds, Moab, Utah
+    static var potashPonds: Envelope {
+        Envelope(
+            xRange: -12211308.778729 ... -12208257.879667,
+            yRange: 4645116.003309 ... 4650542.535773,
+            spatialReference: .webMercator
+        )
+    }
+}
 #Preview {
     SetInitialViewpointView()
 }
