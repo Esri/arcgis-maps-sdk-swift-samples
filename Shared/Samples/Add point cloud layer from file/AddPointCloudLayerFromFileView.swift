ef5aeadd65aa2f858dd0cca2104b8fc1b7f73847
Caleb Rasmussen <crasmussen@esri.com>
2024-10-11 09:32:42 -0700

    Add source code.

--- Shared/Samples/Add point cloud layer from file/AddPointCloudLayerFromFileView.swift
+++ Shared/Samples/Add point cloud layer from file/AddPointCloudLayerFromFileView.swift
@@ -16,19 +16,50 @@ import ArcGIS
 import SwiftUI
 
 struct AddPointCloudLayerFromFileView: View {
-    @State private var map = Map(basemapStyle: .arcGISTopographic)
+    /// A scene with a point cloud layer of Balboa Park, San Diego, CA.
+    @State private var scene: ArcGIS.Scene = {
+        let scene = Scene(basemapStyle: .arcGISImagery)
+        
+        // Creates the point cloud layer and adds it to the scene.
+        let pointCloudLayer = PointCloudLayer(url: .sanDiegoNorthBalboaPointCloud)
+        scene.addOperationalLayer(pointCloudLayer)
+        
+        // Adds an elevation source to display elevation on the scene's surface.
+        let elevationSource = ArcGISTiledElevationSource(url: .worldElevationService)
+        scene.baseSurface.addElevationSource(elevationSource)
+        
+        // Initially centers the scene's camera on the point cloud layer.
+        let camera = Camera(
+            latitude: 32.720195,
+            longitude: -117.155593,
+            altitude: 1050,
+            heading: 23,
+            pitch: 70,
+            roll: 0
+        )
+        let viewpoint = Viewpoint(latitude: .nan, longitude: .nan, scale: .nan, camera: camera)
+        scene.initialViewpoint = viewpoint
+        
+        return scene
+    }()
     
     var body: some View {
-        MapView(map: map)
+        // Displays the scene in a scene view.
+        SceneView(scene: scene)
     }
 }
 
 private extension URL {
-    /// The URL to the local point cloud scene layer package file.
+    /// The URL to the local scene layer package with point cloud data of Balboa Park, San Diego, CA.
     static var sanDiegoNorthBalboaPointCloud: URL {
         Bundle.main.url(
             forResource: "sandiego-north-balboa-pointcloud",
             withExtension: "slpk"
         )!
+    }
+    
+    /// A URL to the Terrain3D image server on ArcGIS REST.
+    static var worldElevationService: URL {
+        URL(string: "https://elevation3d.arcgis.com/arcagis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!
     }
 }
