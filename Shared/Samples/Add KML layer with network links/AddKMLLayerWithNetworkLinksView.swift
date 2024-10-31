6aef5691b440586443f86de6710e8b6e6fa4108f
Caleb Rasmussen <crasmussen@esri.com>
2024-10-31 10:07:58 -0700

    Add source code.

--- Shared/Samples/Add KML layer with network links/AddKMLLayerWithNetworkLinksView.swift
+++ Shared/Samples/Add KML layer with network links/AddKMLLayerWithNetworkLinksView.swift
@@ -16,10 +16,82 @@ import ArcGIS
 import SwiftUI
 
 struct AddKMLLayerWithNetworkLinksView: View {
-    @State private var map = Map(basemapStyle: .arcGISTopographic)
+    /// A scene with the current air traffic in parts of Europe.
+    @State private var scene: ArcGIS.Scene = {
+        let scene = Scene(basemapStyle: .arcGISImagery)
+        
+        // Creates a KML dataset from a remote KMZ file.
+        let kmlDataset = KMLDataset(url: .radarKMZFile)
+        
+        // Creates a KML layer from the dataset and adds it to the scene's operational layers.
+        let kmlLayer = KMLLayer(dataset: kmlDataset)
+        scene.addOperationalLayer(kmlLayer)
+        
+        // Sets the viewpoint to be initially centered on the data coverage.
+        scene.initialViewpoint = Viewpoint(latitude: 50.472421, longitude: 8.150526, scale: 1e7)
+        
+        return scene
+    }()
+    
+    /// The messages from the KML dataset's network links.
+    @State private var networkLinkMessages: [String] = []
+    
+    /// A Boolean value indicating whether network link messages popover is showing.
+    @State private var isShowingMessages = false
     
     var body: some View {
-        MapView(map: map)
+        SceneView(scene: scene)
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Button("Network Link Messages") {
+                        isShowingMessages = true
+                    }
+                    .popover(isPresented: $isShowingMessages) { [networkLinkMessages] in
+                        MessagesList(messages: networkLinkMessages)
+                            .presentationDetents([.fraction(0.5), .large])
+                            .frame(idealWidth: 320, idealHeight: 380)
+                    }
+                }
+            }
+            .task {
+                // Listens for new KML network link messages.
+                let kmlLayer = scene.operationalLayers.first as! KMLLayer
+                
+                for await (_, message) in kmlLayer.dataset.networkLinkMessages {
+                    networkLinkMessages.append(message)
+                }
+            }
+    }
+    
+    /// A list of KML network link messages.
+    private struct MessagesList: View {
+        /// The messages to show in the list.
+        let messages: [String]
+        
+        /// The action to dismiss the view.
+        @Environment(\.dismiss) private var dismiss
+        
+        var body: some View {
+            NavigationStack {
+                List(messages, id: \.self) { message in
+                    Text(message)
+                }
+                .navigationTitle("Network Link Messages")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .confirmationAction) {
+                        Button("Done") { dismiss() }
+                    }
+                }
+            }
+        }
+    }
+}
+
+private extension URL {
+    /// The web URL to the "radar" KMZ file containing network links for the current air traffic in parts of Europe.
+    static var radarKMZFile: URL {
+        URL(string: "https://www.arcgis.com/sharing/rest/content/items/600748d4464442288f6db8a4ba27dc95/data")!
     }
 }
 
