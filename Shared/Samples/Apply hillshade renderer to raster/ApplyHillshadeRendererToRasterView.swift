990a5244665f2ed9068e203d00a7cacf6a92cecb
Ryan Olson <rolson@esri.com>
2025-05-05 16:53:13 -0600

    update sample code

--- Shared/Samples/Apply hillshade renderer to raster/ApplyHillshadeRendererToRasterView.swift
+++ Shared/Samples/Apply hillshade renderer to raster/ApplyHillshadeRendererToRasterView.swift
@@ -16,65 +16,59 @@ import ArcGIS
 import SwiftUI
 
 struct ApplyHillshadeRendererToRasterView: View {
-//    /// The model used to store the geo model and other expensive objects
-//    /// used in this view.
-//    private class Model: ObservableObject {
-//        /// A map with imagery basemap.
-//        let map = Map(basemapStyle: .arcGISImagery)
-//    }
-//    
-//    /// The view model for the sample.
-//    @StateObject private var model = Model()
-//    
-//    var body: some View {
-//        // Creates a map view to display the map.
-//        MapView(map: model.map)
-//    }
+    /// The model used to store the geo model and other expensive objects
+    /// used in this view.
+    class Model: ObservableObject {
+        /// The map that will be shown.
+        let map: Map
+        /// The raster layer that will be added to the map.
+        let rasterLayer: RasterLayer
+        
+        init() {
+            // Create our map.
+            map = Map(basemapStyle: .arcGISImageryStandard)
+            
+            // Gets the raster file URL.
+            let rasterFileURL = Bundle.main.url(forResource: "SA_EVI_8Day_03May20", withExtension: "tif", subdirectory: "SA_EVI_8Day_03May20")!
+            
+            // Creates a raster with the file URL.
+            let raster = Raster(fileURL: rasterFileURL)
+            
+            // Creates a raster layer using the raster object.
+            rasterLayer = RasterLayer(raster: raster)
+            
+            // Apply the hillshade renderer to the raster layer.
+            rasterLayer.renderer = HillshadeRenderer(
+                altitude: 45,
+                azimuth: 315,
+                slopeType: nil,
+                zFactor: 0.000016,
+                pixelSizeFactor: 1,
+                pixelSizePower: 1,
+                outputBitDepth: 8
+            )
+            
+            // Add the raster layer to the map.
+            map.addOperationalLayer(rasterLayer)
+        }
+    }
     
-    /// A map with imagery basemap and a raster layer.
-    @State private var map = Map(basemapStyle: .arcGISImageryStandard)
-    
-    /// The error shown in the error alert.
-    @State private var error: Error?
-    
-    /// The current viewpoint of the map view.
-    @State private var viewpoint: Viewpoint?
+    /// The view model for the sample.
+    @StateObject private var model = Model()
     
     var body: some View {
-        // Creates a map view with a viewpoint to display the map.
-        MapView(map: map, viewpoint: viewpoint)
-            .onViewpointChanged(kind: .centerAndScale) { viewpoint = $0 }
-            .errorAlert(presentingError: $error)
-            .task {
-                guard map.operationalLayers.isEmpty else { return }
-                // Gets the raster file URL.
-                let rasterFileURL = Bundle.main.url(forResource: "SA_EVI_8Day_03May20", withExtension: "tif", subdirectory: "SA_EVI_8Day_03May20")!
-                // Creates a raster with the file URL.
-                let raster = Raster(fileURL: rasterFileURL)
-                // Creates a raster layer using the raster object.
-                let rasterLayer = RasterLayer(raster: raster)
-                
-                // Apply the hillshade renderer to the raster layer
-                rasterLayer.renderer = HillshadeRenderer(
-                    altitude: 45,
-                    azimuth: 315,
-                    slopeType: nil,
-                    zFactor: 0.000016,
-                    pixelSizeFactor: 1,
-                    pixelSizePower: 1,
-                    outputBitDepth: 8
-                )
-                
-                do {
-                    try await rasterLayer.load()
-                    // Adds the raster layer to the map's operational layer.
-                    map.addOperationalLayer(rasterLayer)
-                    viewpoint = Viewpoint(boundingGeometry: rasterLayer.fullExtent!)
-                } catch {
-                    // Presents an error message if the raster fails to load.
-                    self.error = error
+        MapViewReader { mapViewProxy in
+            // Creates a map view to display the map.
+            MapView(map: model.map)
+                .task {
+                    // When the view appears, load the raster layer so that
+                    // we can zoom to its extent.
+                    try? await model.rasterLayer.load()
+                    if let extent = model.rasterLayer.fullExtent {
+                        await mapViewProxy.setViewpointGeometry(extent)
+                    }
                 }
-            }
+        }
     }
 }
 
