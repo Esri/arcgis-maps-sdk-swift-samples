f6fba1a5f3d5473dffa43cd89dca6f8277369594
Ryan Olson <rolson@esri.com>
2025-05-05 16:21:14 -0600

    add sample

---
+++ Shared/Samples/Apply hillshade renderer to raster/ApplyHillshadeRendererToRasterView.swift
@@ -0,0 +1,71 @@
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import ArcGIS
+import SwiftUI
+
+struct ApplyHillshadeRendererToRasterView: View {
+//    /// The model used to store the geo model and other expensive objects
+//    /// used in this view.
+//    private class Model: ObservableObject {
+//        /// A map with imagery basemap.
+//        let map = Map(basemapStyle: .arcGISImagery)
+//    }
+//    
+//    /// The view model for the sample.
+//    @StateObject private var model = Model()
+//    
+//    var body: some View {
+//        // Creates a map view to display the map.
+//        MapView(map: model.map)
+//    }
+    
+    /// A map with imagery basemap and a raster layer.
+    @State private var map = Map(basemapStyle: .arcGISImageryStandard)
+    
+    /// The error shown in the error alert.
+    @State private var error: Error?
+    
+    /// The current viewpoint of the map view.
+    @State private var viewpoint: Viewpoint?
+    
+    var body: some View {
+        // Creates a map view with a viewpoint to display the map.
+        MapView(map: map, viewpoint: viewpoint)
+            .onViewpointChanged(kind: .centerAndScale) { viewpoint = $0 }
+            .errorAlert(presentingError: $error)
+            .task {
+                guard map.operationalLayers.isEmpty else { return }
+                do {
+                    // Gets the Shasta.tif file URL.
+                    let shastaURL = Bundle.main.url(forResource: "Shasta", withExtension: "tif", subdirectory: "raster-file/raster-file")!
+                    // Creates a raster with the file URL.
+                    let raster = Raster(fileURL: shastaURL)
+                    // Creates a raster layer using the raster object.
+                    let rasterLayer = RasterLayer(raster: raster)
+                    try await rasterLayer.load()
+                    // Adds the raster layer to the map's operational layer.
+                    map.addOperationalLayer(rasterLayer)
+                    viewpoint = Viewpoint(center: rasterLayer.fullExtent!.center, scale: 8e4)
+                } catch {
+                    // Presents an error message if the raster fails to load.
+                    self.error = error
+                }
+            }
+    }
+}
+
+#Preview {
+    ApplyHillshadeRendererToRasterView()
+}
---
+++ Shared/Samples/Apply hillshade renderer to raster/README.md
@@ -0,0 +1,32 @@
+# Apply hillshade renderer to raster
+
+Apply a hillshade renderer to a raster.
+
+![Image of apply hillshade renderer to raster](ApplyHillshadeRendererToRaster.png)
+
+## Use case
+
+An environmental agency may track coastal erosion by comparing images of an area taken over a a longer period of time with hillshade renderers applied.
+
+## How to use the sample
+
+Choose and adjust the settings to update the hillshade renderer on the raster layer. The sample allows you to change the Altitude, Azimuth, and Slope Type.
+
+## How it works
+
+1. Create a `Raster` from a grayscale raster file.
+2. Create a `RasterLayer` from the raster.
+3. Create a `Basemap` from the raster layer and set it to the map.
+4. Create a `HillshadeRenderer`, specifying the slope type and other properties, `new HillshadeRenderer(Altitude, Azimuth, ZFactor, SlopeType, PixelSizeFactor, PixelSizePower, OutputBitDepth)`.
+5. Set the hillshade renderer to be used on the raster layer with `rasterLayer.setRenderer(renderer)`.
+
+## Relevant API
+
+* Basemap
+* HillshadeRenderer
+* Raster
+* RasterLayer
+
+## Tags
+
+altitude, angle, azimuth, raster, slope, visualization
