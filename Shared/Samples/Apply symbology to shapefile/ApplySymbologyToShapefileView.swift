c21ad5d0a3383eb54c82de84629eb405b0aa077b
Ryan Olson <rolson@esri.com>
2025-05-07 17:18:22 -0600

    add new sample

---
+++ Shared/Samples/Apply symbology to shapefile/ApplySymbologyToShapefileView.swift
@@ -0,0 +1,19 @@
+//
+//  ApplySymbologyToShapefileView.swift
+//  Samples
+//
+//  Created by Ryan Olson on 5/7/25.
+//  Copyright © 2025 Esri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ApplySymbologyToShapefileView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ApplySymbologyToShapefileView()
+}
