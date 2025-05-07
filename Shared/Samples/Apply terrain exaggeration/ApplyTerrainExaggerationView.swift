7bab1e9342cd8fad0f8689996ebcb46cdb3d2063
Caleb Rasmussen <crasmussen@esri.com>
2025-05-06 18:02:02 -0700

    Add source code.

--- Shared/Samples/Apply terrain exaggeration/ApplyTerrainExaggerationView.swift
+++ Shared/Samples/Apply terrain exaggeration/ApplyTerrainExaggerationView.swift
@@ -16,10 +16,46 @@ import ArcGIS
 import SwiftUI
 
 struct ApplyTerrainExaggerationView: View {
-    @State private var map = Map(basemapStyle: .arcGISTopographic)
+    /// A scene with an elevated surface and a topographic basemap.
+    @State private var scene: ArcGIS.Scene = {
+        // Creates a scene using a basemap style.
+        let scene = Scene(basemapStyle: .arcGISTopographic)
+        
+        // Creates an elevation source and adds it to the scene's surface.
+        let elevationSource = ArcGISTiledElevationSource(url: .worldElevationService)
+        scene.baseSurface.addElevationSource(elevationSource)
+        
+        // Sets the scene's initial viewpoint to center the scene view on Levering, WA, USA.
+        let point = Point(latitude: 46.75792, longitude: -119.9489)
+        let camera = Camera(lookingAt: point, distance: 15000, heading: 40, pitch: 60, roll: 0)
+        scene.initialViewpoint = Viewpoint(latitude: .nan, longitude: .nan, scale: .nan, camera: camera)
+        
+        return scene
+    }()
+    
+    /// The elevation exaggeration of the scene's surface.
+    @State private var elevationExaggeration: Float = 1
     
     var body: some View {
-        MapView(map: map)
+        SceneView(scene: scene)
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Stepper(value: $elevationExaggeration, in: 1...10) {
+                        Text("\(elevationExaggeration, format: .number.rounded())x")
+                    }
+                    .onChange(of: elevationExaggeration) {
+                        // Sets the surface's elevation exaggeration when the stepper value changes.
+                        scene.baseSurface.elevationExaggeration = elevationExaggeration
+                    }
+                }
+            }
+    }
+}
+
+private extension URL {
+    /// A web URL to the Terrain3D image server on ArcGIS REST.
+    static var worldElevationService: URL {
+        URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!
     }
 }
 
