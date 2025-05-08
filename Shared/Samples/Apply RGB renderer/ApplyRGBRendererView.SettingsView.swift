2ad3ce1c883b5661b01ff61e502da76dffbdaef6
Ting Chen <tchen@esri.com>
2025-05-08 15:40:37 -0700

    Refactor settings to untangled dataflow.

--- Shared/Samples/Apply RGB renderer/ApplyRGBRendererView.SettingsView.swift
+++ Shared/Samples/Apply RGB renderer/ApplyRGBRendererView.SettingsView.swift
@@ -19,84 +19,41 @@ extension ApplyRGBRendererView {
     struct SettingsView: View {
         /// The action to dismiss the view.
         @Environment(\.dismiss) private var dismiss
-        
-        /// The raster layer to apply the RGB renderer to.
-        let rasterLayer: RasterLayer
-        
-        /// The stretch type to apply to the raster layer.
-        @Binding var stretchType: StretchType
-        /// The color to use for the minimum values in the min-max stretch.
-        @State private var minColor: Color = .black
-        /// The color to use for the maximum values in the min-max stretch.
-        @State private var maxColor: Color = .green
-        /// The minimum value for the percent clip stretch.
-        @State private var minValue: Double = 20
-        /// The maximum value for the percent clip stretch.
-        @State private var maxValue: Double = 80
-        /// The standard deviation factor for the standard deviation stretch.
-        @State private var standardDeviation = 0.5
+        /// A binding to the renderer settings.
+        @Binding var settings: RendererSettings
         
         var body: some View {
             Form {
                 Section {
-                    Picker("Type", selection: $stretchType) {
+                    Picker("Type", selection: $settings.stretchType) {
                         ForEach(StretchType.allCases, id: \.self) { stretchType in
                             Text(stretchType.label)
-                        }
-                    }
-                    .onChange(of: stretchType, initial: true) {
-                        switch stretchType {
-                        case .histogramEqualization:
-                            // Nothing to configure for histogram equalization.
-                            // It is useful when there are a lot of pixel values
-                            // that are closely grouped together
-                            setStretchParameters(HistogramEqualizationStretchParameters())
-                        default:
-                            break
                         }
                     }
                 }
                 
                 Section {
-                    switch stretchType {
+                    switch settings.stretchType {
                     case .histogramEqualization:
                         EmptyView()
                     case .minMax:
-                        ColorPicker("Min Color", selection: $minColor, supportsOpacity: false)
-                            .onChange(of: minColor, initial: true) {
-                                setStretchParameters(MinMaxStretchParameters(minValues: minColor.rgb, maxValues: maxColor.rgb))
-                            }
-                        ColorPicker("Max Color", selection: $maxColor, supportsOpacity: false)
-                            .onChange(of: maxColor) {
-                                // The pixel values which serve as endpoint for
-                                // the histogram used for the stretch.
-                                setStretchParameters(MinMaxStretchParameters(minValues: minColor.rgb, maxValues: maxColor.rgb))
-                            }
+                        ColorPicker("Min Color", selection: $settings.minColor, supportsOpacity: false)
+                        ColorPicker("Max Color", selection: $settings.maxColor, supportsOpacity: false)
                     case .percentClip:
                         HStack {
-                            Text("\(Int(minValue))")
+                            Text("\(Int(settings.minValue))")
                             Spacer()
-                            Text("\(Int(maxValue))")
+                            Text("\(Int(settings.maxValue))")
                         }
-                        RangeSlider(lowerValue: $minValue, upperValue: $maxValue, range: 0...100, step: 1)
-                            .onChange(of: [minValue, maxValue], initial: true) {
-                                // The percentile cutoff above which pixel values
-                                // in the raster dataset are to be clipped.
-                                setStretchParameters(PercentClipStretchParameters(min: minValue, max: maxValue))
-                            }
+                        RangeSlider(lowerValue: $settings.minValue, upperValue: $settings.maxValue, range: 0...100, step: 1)
                     case .standardDeviation:
-                        Text("Factor: \(standardDeviation, format: .number.precision(.fractionLength(2)))")
-                        Slider(value: $standardDeviation, in: 0.0...16.0, step: 0.01) {
+                        LabeledContent("Factor", value: settings.standardDeviation, format: .number.precision(.fractionLength(2)))
+                        Slider(value: $settings.standardDeviation, in: 0.0...16.0, step: 0.01) {
                             Text("Factor")
                         } minimumValueLabel: {
                             Text("0")
                         } maximumValueLabel: {
                             Text("16")
-                        }
-                        .onChange(of: standardDeviation, initial: true) {
-                            // It applies a linear stretch between the
-                            // values defined by the standard deviation.
-                            setStretchParameters(StandardDeviationStretchParameters(factor: standardDeviation))
                         }
                     }
                 }
@@ -109,41 +66,5 @@ extension ApplyRGBRendererView {
                 }
             }
         }
-        
-        /// Sets the stretch parameters for the raster layer.
-        /// - Parameter parameters: The stretch parameters to apply.
-        private func setStretchParameters(_ parameters: StretchParameters) {
-            rasterLayer.renderer = RGBRenderer(
-                stretchParameters: parameters,
-                bandIndexes: [],
-                gammas: [],
-                estimatesStatistics: true
-            )
-        }
-    }
-    
-    enum StretchType: CaseIterable {
-        case histogramEqualization, minMax, percentClip, standardDeviation
-        
-        /// The label for the stretch type.
-        var label: String {
-            switch self {
-            case .histogramEqualization: "Histogram Equalization"
-            case .minMax: "Min-Max"
-            case .percentClip: "Percent Clip"
-            case .standardDeviation: "Standard Deviation"
-            }
-        }
-    }
-}
-
-private extension Color {
-    /// The RGB components of the color.
-    var rgb: [Double] {
-        UIColor(self)
-            .cgColor
-            .components!
-            .prefix(3)
-            .map { Double($0) * 255 }
     }
 }
--- Shared/Samples/Apply RGB renderer/ApplyRGBRendererView.swift
+++ Shared/Samples/Apply RGB renderer/ApplyRGBRendererView.swift
@@ -27,17 +27,14 @@ struct ApplyRGBRendererView: View {
         )
         return Map(basemap: Basemap(baseLayer: RasterLayer(raster: raster)))
     }()
-    
-    /// The stretch type to apply to the raster layer.
-    @State private var stretchType: StretchType = .histogramEqualization
-    
+    /// The RGB renderer settings.
+    @State private var rendererSettings = RendererSettings()
+    /// A Boolean value indicating whether the settings view should be presented.
+    @State private var isShowingSettings = false
     /// The raster layer to apply RGB renderer.
     private var rasterLayer: RasterLayer {
         map.basemap?.baseLayers[0] as! RasterLayer
     }
-    
-    /// A Boolean value indicating whether the settings view should be presented.
-    @State private var isShowingSettings = false
     
     var body: some View {
         MapView(map: map)
@@ -48,12 +45,85 @@ struct ApplyRGBRendererView: View {
                     }
                     .popover(isPresented: $isShowingSettings) {
                         NavigationStack {
-                            SettingsView(rasterLayer: rasterLayer, stretchType: $stretchType)
+                            SettingsView(settings: $rendererSettings)
+                                .onChange(of: rendererSettings, initial: true) {
+                                    switch rendererSettings.stretchType {
+                                    case .histogramEqualization:
+                                        // Nothing to configure for histogram equalization.
+                                        // It is useful when there are a lot of pixel values
+                                        // that are closely grouped together
+                                        setStretchParameters(HistogramEqualizationStretchParameters())
+                                    case .minMax:
+                                        // The pixel values which serve as endpoints for
+                                        // the histogram used for the stretch.
+                                        setStretchParameters(MinMaxStretchParameters(minValues: rendererSettings.minColor.rgb, maxValues: rendererSettings.maxColor.rgb))
+                                    case .percentClip:
+                                        // The percentile cutoff above which pixel values
+                                        // in the raster dataset are to be clipped.
+                                        setStretchParameters(PercentClipStretchParameters(min: rendererSettings.minValue, max: rendererSettings.maxValue))
+                                    case .standardDeviation:
+                                        // It applies a linear stretch between the
+                                        // values defined by the standard deviation.
+                                        setStretchParameters(StandardDeviationStretchParameters(factor: rendererSettings.standardDeviation))
+                                    }
+                                }
                         }
                         .presentationDetents([.fraction(0.5)])
                         .frame(idealWidth: 320, idealHeight: 360)
                     }
                 }
             }
+    }
+    
+    /// Sets the stretch parameters for the raster layer.
+    /// - Parameter parameters: The stretch parameters to apply.
+    private func setStretchParameters(_ parameters: StretchParameters) {
+        rasterLayer.renderer = RGBRenderer(
+            stretchParameters: parameters,
+            bandIndexes: [],
+            gammas: [],
+            estimatesStatistics: true
+        )
+    }
+    
+    /// The settings for a RGB renderer.
+    struct RendererSettings: Equatable {
+        /// The stretch type to apply to the raster layer.
+        var stretchType: StretchType = .histogramEqualization
+        /// The color to use for the minimum values in the min-max stretch.
+        var minColor: Color = .black
+        /// The color to use for the maximum values in the min-max stretch.
+        var maxColor: Color = .green
+        /// The minimum value for the percent clip stretch.
+        var minValue: Double = 20
+        /// The maximum value for the percent clip stretch.
+        var maxValue: Double = 80
+        /// The standard deviation factor for the standard deviation stretch.
+        var standardDeviation = 0.5
+    }
+    
+    enum StretchType: CaseIterable {
+        case histogramEqualization, minMax, percentClip, standardDeviation
+        
+        /// The label for the stretch type.
+        var label: String {
+            switch self {
+            case .histogramEqualization: "Histogram Equalization"
+            case .minMax: "Min-Max"
+            case .percentClip: "Percent Clip"
+            case .standardDeviation: "Standard Deviation"
+            }
+        }
+    }
+}
+
+private extension Color {
+    /// The RGB components of the color.
+    var rgb: [Double] {
+        UIColor(self)
+            .cgColor
+            .components!
+            .prefix(3)
+            .map { Double($0) * 255 }
     }
 }
