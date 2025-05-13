755f9c0bda24465406e286ab11ad27b51d85902c
Ryan Olson <rolson@esri.com>
2025-05-13 14:14:44 -0600

    add open street map layer sample skeleton

---
+++ Shared/Samples/Add Open Street Map layer/AddOpenStreetMapLayerView.swift
@@ -0,0 +1,19 @@
+//
+//  AddOpenStreetMapLayerView.swift
+//  Samples
+//
+//  Created by Ryan Olson on 5/13/25.
+//  Copyright © 2025 Esri. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddOpenStreetMapLayerView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    AddOpenStreetMapLayerView()
+}
