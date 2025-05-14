17e61e3af451125416f47c22d633db87e20c68cc
Ryan Olson <rolson@esri.com>
2025-05-14 11:21:37 -0600

    add skeleton

---
+++ Shared/Samples/Show geodesic path between two points/ShowGeodesicPathBetweenTwoPointsView.swift
@@ -0,0 +1,19 @@
+//
+//  ShowGeodesicPathBetweenTwoPointsView.swift
+//  Samples
+//
+//  Created by Ryan Olson on 5/14/25.
+//  Copyright © 2025 Esri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ShowGeodesicPathBetweenTwoPointsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ShowGeodesicPathBetweenTwoPointsView()
+}
