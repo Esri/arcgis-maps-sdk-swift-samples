071f3861bdd382e6e29cc6cbc535d9e9fea27f7a
Ryan Olson <rolson@esri.com>
2025-05-14 11:42:29 -0600

    add tapstate

--- Shared/Samples/Show geodesic path between two points/ShowGeodesicPathBetweenTwoPointsView.swift
+++ Shared/Samples/Show geodesic path between two points/ShowGeodesicPathBetweenTwoPointsView.swift
@@ -1,16 +1,104 @@
+// Copyright 2025 Esri
 //
-//  ShowGeodesicPathBetweenTwoPointsView.swift
-//  Samples
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Ryan Olson on 5/14/25.
-//  Copyright © 2025 Esri. All rights reserved.
+//   https://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+import ArcGIS
 import SwiftUI
 
 struct ShowGeodesicPathBetweenTwoPointsView: View {
+    @State private var map = Map(basemapStyle: .arcGISImageryStandard)
+    @State private var overlay = {
+        let overlay = GraphicsOverlay()
+        return overlay
+    }()
+    
+    enum TapState {
+        case noGraphics
+        case startOnly(Graphic)
+        case both(Graphic, Graphic)
+    }
+    
+    @State private var tapState: TapState = .noGraphics {
+        didSet {
+            updateOverlay()
+            updateLength()
+        }
+    }
+    
+    private var graphic1: Graphic? {
+//        overlay.graphics.first
+        switch tapState {
+        case .noGraphics:
+            nil
+        case .startOnly(let graphic):
+            graphic
+        case .both(let graphic, let graphic2):
+            graphic
+        }
+    }
+    
+    private var graphic2: Graphic? {
+//        overlay.graphics.count > 1 ? overlay.graphics[1] : nil
+        switch tapState {
+        case .noGraphics, .startOnly:
+            nil
+        case .both(let graphic, let graphic2):
+            graphic2
+        }
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        MapView(map: map, graphicsOverlays: [overlay])
+            .onSingleTapGesture { _, mapPoint in
+                switch tapState {
+                case .noGraphics:
+                    tapState = .startOnly(Self.makeGraphic(at: mapPoint))
+                case .startOnly(let graphic):
+                    tapState = .both(graphic, Self.makeGraphic(at: mapPoint))
+                case .both(let graphic, let graphic2):
+                    tapState = .noGraphics
+                }
+            }
+    }
+    
+    private static func makeGraphic(at point: Point) -> Graphic {
+        let symbol = SimpleMarkerSymbol(style: .cross, color: .blue, size: 20)
+        return Graphic(geometry: point, symbol: symbol)
+    }
+    
+    private func updateOverlay() {
+        overlay.removeAllGraphics()
+        
+        switch tapState {
+        case .noGraphics:
+            break
+        case .startOnly(let graphic):
+            overlay.addGraphic(graphic)
+        case .both(let graphic, let graphic2):
+            overlay.addGraphic(graphic)
+            overlay.addGraphic(graphic2)
+        }
+    }
+    
+    private func updateLength() {
+        switch tapState {
+        case .noGraphics:
+            break
+        case .startOnly(let graphic):
+            break
+        case .both(let graphic, let graphic2):
+            break
+        }
     }
 }
 
