6b9d5cfccc89f6f535ab787dd0e7d3a5f5a5bef9
Ryan Olson <rolson@esri.com>
2025-05-14 13:39:32 -0600

    add doc

--- Shared/Samples/Show geodesic path between two points/ShowGeodesicPathBetweenTwoPointsView.swift
+++ Shared/Samples/Show geodesic path between two points/ShowGeodesicPathBetweenTwoPointsView.swift
@@ -16,22 +16,30 @@ import ArcGIS
 import SwiftUI
 
 struct ShowGeodesicPathBetweenTwoPointsView: View {
+    /// The map that will be displayed in the map view.
     @State private var map = Map(basemapStyle: .arcGISImageryStandard)
+    
+    /// The graphics overlay that will be displayed on the map view.
+    /// This will hold the graphics that show the start point, end point,
+    /// and geodesic path.
     @State private var overlay = {
         let overlay = GraphicsOverlay()
         return overlay
     }()
     
+    /// The current measurement state.
     @State private var state: MeasurementState = .empty {
         didSet {
             updateGraphicsOverlay()
         }
     }
     
+    /// The symbology for point graphics.
     private let pointSymbol: Symbol = {
         SimpleMarkerSymbol(style: .cross, color: .blue, size: 20)
     }()
     
+    /// The symbology for the line graphic.
     private let lineSymbol: Symbol = {
         SimpleLineSymbol(style: .dash, color: .yellow, width: 2)
     }()
@@ -41,8 +49,12 @@ struct ShowGeodesicPathBetweenTwoPointsView: View {
             .onSingleTapGesture { _, mapPoint in
                 switch state {
                 case .empty, .complete:
+                    // If the state is empty or complete, then start a new
+                    // path, adding the tap point as the first graphic.
                     state = .startOnly(start: mapPoint)
                 case .startOnly(let start):
+                    // If the state was started, then add the end point
+                    // to complete it.
                     state = .complete(start: start, end: mapPoint)
                 }
             }
@@ -62,6 +74,7 @@ struct ShowGeodesicPathBetweenTwoPointsView: View {
             .animation(.default, value: state)
     }
     
+    /// Update the graphics overlay for the current state.
     private func updateGraphicsOverlay() {
         overlay.removeAllGraphics()
         
@@ -79,11 +92,17 @@ struct ShowGeodesicPathBetweenTwoPointsView: View {
 }
 
 extension ShowGeodesicPathBetweenTwoPointsView {
+    /// A value that represents the measurement state of the view.
     enum MeasurementState: Equatable {
+        /// No measurement started.
         case empty
+        /// Only have a starting point.
         case startOnly(start: Point)
+        /// Completed measurement.
         case complete(start: Point, end: Point, line: Polyline, length: Measurement<UnitLength>)
         
+        /// Creates a `complete` measurement state with a start and end point,
+        /// calculating the line and length.
         static func complete(start: Point, end: Point) -> Self {
             let line = Polyline(points: [start, end])
             let geodesicLine = GeometryEngine.geodeticDensify(
