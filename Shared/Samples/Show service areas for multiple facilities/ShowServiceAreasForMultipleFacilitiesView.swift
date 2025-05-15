f299169ada96d335d15c02fc479075049611505d
Ryan Olson <rolson@esri.com>
2025-05-15 16:30:27 -0600

    add doc

--- Shared/Samples/Show service areas for multiple facilities/ShowServiceAreasForMultipleFacilitiesView.swift
+++ Shared/Samples/Show service areas for multiple facilities/ShowServiceAreasForMultipleFacilitiesView.swift
@@ -16,16 +16,23 @@ import ArcGIS
 import SwiftUI
 
 struct ShowServiceAreasForMultipleFacilitiesView: View {
+    /// The map that we will display in the map view.
     @State private var map = Map(basemapStyle: .arcGISLightGray)
     
+    /// The service area task used to calculate the service area.
     @State private var serviceAreaTask = ServiceAreaTask(url: URL(string: "https://sampleserver7.arcgisonline.com/server/rest/services/NetworkAnalysis/SanDiego/NAServer/ServiceArea")!)
     
+    /// The feature table that contains the facilities.
     @State private var facilitiesFeatureTable = ServiceFeatureTable(url: URL(string: "https://services2.arcgis.com/ZQgQTuoyBrtmoGdP/ArcGIS/rest/services/San_Diego_Facilities/FeatureServer/0")!)
     
+    /// A graphics overlay which will hold the results of the service area
+    /// calculation.
     @State private var graphicsOverlay = GraphicsOverlay()
     
+    /// The error that occurred during calculating the service area.
     @State private var error: Error?
     
+    /// A Boolean value indicating if the service area is being calculated.
     @State private var isCalculatingServiceArea = false
     
     var body: some View {
@@ -37,27 +44,46 @@ struct ShowServiceAreasForMultipleFacilitiesView: View {
                             .padding()
                             .background(.ultraThickMaterial)
                             .clipShape(.rect(cornerRadius: 10))
-                            .shadow(radius: 50)
+                            .shadow(radius: 15)
                     }
                 }
                 .task {
                     isCalculatingServiceArea = true
                     defer { isCalculatingServiceArea = false }
+                    
                     do {
+                        // Create a feature layer to display the facilities.
+                        // And add the facilities feature layer to the map.
                         let featureLayer = FeatureLayer(featureTable: facilitiesFeatureTable)
                         map.addOperationalLayer(featureLayer)
+                        
+                        // Load the facilities feature layer so that we can zoom
+                        // to the full extent of the features.
                         try await featureLayer.load()
                         if let fullExtent = featureLayer.fullExtent {
                             await mapViewProxy.setViewpointGeometry(fullExtent, padding: 50)
                         }
                         
+                        // Create default parameters for the service area task.
                         let serviceAreaParameters = try await serviceAreaTask.makeDefaultParameters()
+                        // Set the facilities for which to calculate service area for.
                         serviceAreaParameters.setFacilities(fromFeaturesIn: facilitiesFeatureTable, queryParameters: .all)
+                        // Specify that we want polygons returned, with a high
+                        // level of detail.
                         serviceAreaParameters.returnsPolygons = true
                         serviceAreaParameters.polygonDetail = .high
+                        // Set our impedence cutoffs to 1 minute and 3 minutes
+                        // accordingly.
+                        serviceAreaParameters.removeAllDefaultImpedanceCutoffs()
                         serviceAreaParameters.addDefaultImpedanceCutoffs([1, 3])
                         
-                        let serviceAreaResult = try await serviceAreaTask.solveServiceArea(using: serviceAreaParameters)
+                        // Solve the service area.
+                        let serviceAreaResult = try await serviceAreaTask.solveServiceArea(
+                            using: serviceAreaParameters
+                        )
+                        
+                        // Loop through the service area facilities and add the
+                        // results to the graphics overlay.
                         for index in serviceAreaResult.facilities.indices {
                             let polygons = serviceAreaResult.resultPolygons(forFacilityAtIndex: index)
                             for polygon in polygons {
