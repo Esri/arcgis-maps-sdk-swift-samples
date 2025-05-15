7babcecc5d8488db0ec38b94a6b9a70d9ae000d3
Ryan Olson <rolson@esri.com>
2025-05-15 14:36:58 -0600

    add sample skeleton

---
+++ Shared/Samples/Show service areas for multiple facilities/ShowServiceAreasForMultipleFacilitiesView.swift
@@ -0,0 +1,19 @@
+//
+//  ShowServiceAreasForMultipleFacilitiesView.swift
+//  Samples
+//
+//  Created by Ryan Olson on 5/15/25.
+//  Copyright © 2025 Esri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ShowServiceAreasForMultipleFacilitiesView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ShowServiceAreasForMultipleFacilitiesView()
+}
