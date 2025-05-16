d9d1b2ed7889b220ee87a36faf1c3ac849b04e69
Ryan Olson <rolson@esri.com>
2025-05-16 15:53:07 -0600

    sample skeleton

---
+++ Shared/Samples/Browse WMS layers/BrowseWMSLayersView.swift
@@ -0,0 +1,19 @@
+//
+//  BrowseWMSLayersView.swift
+//  Samples
+//
+//  Created by Ryan Olson on 5/16/25.
+//  Copyright © 2025 Esri. All rights reserved.
+//
+
+import SwiftUI
+
+struct BrowseWMSLayersView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    BrowseWMSLayersView()
+}
