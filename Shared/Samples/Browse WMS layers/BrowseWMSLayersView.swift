4276a8c76db7072c1d813e4e1d2a87e91ee53b66
Ryan Olson <rolson@esri.com>
2025-05-16 17:05:51 -0600

    progress

--- Shared/Samples/Browse WMS layers/BrowseWMSLayersView.swift
+++ Shared/Samples/Browse WMS layers/BrowseWMSLayersView.swift
@@ -1,16 +1,106 @@
+// Copyright 2025 Esri
 //
-//  BrowseWMSLayersView.swift
-//  Samples
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Ryan Olson on 5/16/25.
-//  Copyright © 2025 Esri. All rights reserved.
+//   https://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+import ArcGIS
 import SwiftUI
 
 struct BrowseWMSLayersView: View {
+    @State private var map = Map(basemapStyle: .arcGISDarkGray)
+    @State private var wmsService = WMSService(url: URL(string: "https://nowcoast.noaa.gov/geoserver/observations/weather_radar/wms?SERVICE=WMS&REQUEST=GetCapabilities")!)
+    
+    @State private var error: Error?
+    
+    @State private var selection: WMSLayerInfo?
+    
+    @State private var layerInfos = [WMSLayerInfo]()
+    
+    @State private var isListPresented = false
+    
     var body: some View {
-        Text("Hello, World!")
+        MapViewReader { mapViewProxy in
+            MapView(map: map)
+                .task {
+                    do {
+                        print("-- loading")
+                        try await wmsService.load()
+                        if let info = wmsService.serviceInfo {
+//                            let layer = WMSLayer(layerInfos: info.layerInfos)
+//                            print("-- adding layer, count: \(info.layerInfos.count)")
+//                            map.addOperationalLayer(layer)
+                            
+//                            try await layer.load()
+                            
+//                            if let layerInfo = info.layerInfos.first {
+//                                let layer = WMSLayer(layerInfos: layerInfo.sublayerInfos)
+//                                print("-- adding layer, count: \(layerInfo.sublayerInfos.count)")
+//                                map.addOperationalLayer(layer)
+////                                if let extent = layerInfo.extent {
+////                                    print("-- zooming: \(extent)")
+////                                    await mapViewProxy.setViewpointGeometry(extent, padding: 50)
+////                                }
+//                            }
+                            
+                            layerInfos = info.layerInfos
+                            
+                            
+//                            if let extent = layer.fullExtent {
+//                                print("-- zooming: \(extent)")
+//                                await mapViewProxy.setViewpointGeometry(extent, padding: 50)
+//                            }
+                        }
+                        //                    for info in wmsService.serviceInfo?.layerInfos {
+                        //
+                        //                    }
+                    } catch {
+                        print("-- error: \(error)")
+                        self.error = error
+                    }
+                }
+        }
+        .toolbar {
+            ToolbarItem(placement: .bottomBar) {
+                Button("Layer List") {
+                    isListPresented = true
+                }
+            }
+        }
+        .popover(isPresented: $isListPresented) {
+            NavigationStack {
+                WMSLayerListView(layerInfos: layerInfos, selection: $selection)
+                    .presentationDetents([.medium])
+                    .frame(idealWidth: 320, idealHeight: 380)
+                    .toolbar {
+                        ToolbarItem(placement: .topBarTrailing) {
+                            Button("Done") {
+                                isListPresented = false
+                            }
+                        }
+                    }
+            }
+        }
+        .errorAlert(presentingError: $error)
+    }
+}
+
+extension BrowseWMSLayersView {
+    struct WMSLayerListView: View {
+        let layerInfos: [WMSLayerInfo]
+        @Binding var selection: WMSLayerInfo?
+        
+        var body: some View {
+            Text("")
+        }
     }
 }
 
