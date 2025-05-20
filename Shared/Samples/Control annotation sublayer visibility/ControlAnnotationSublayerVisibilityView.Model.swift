f5852e5a4bc0c9f2a798a5cf0063575633773d7e
Destiny Hochhalter <dhochhalterruiz@esri.com>
2025-05-20 12:46:24 -0700

    Add new sample

---
+++ Shared/Samples/Control annotation sublayer visibility/ControlAnnotationSublayerVisibilityView.Model.swift
@@ -0,0 +1,126 @@
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import ArcGIS
+import SwiftUI
+
+extension ControlAnnotationSublayerVisibilityView {
+    /// The view model for this sample.
+    @MainActor
+    class Model: ObservableObject {
+        /// A map with no specified style.
+        @Published var map = Map()
+        
+        /// The mobile map package.
+        @Published private var mobileMapPackage: MobileMapPackage!
+        
+        /// The annotation layer in the map.
+        private var annotationLayer: AnnotationLayer {
+            map.operationalLayers.first(where: { $0 is AnnotationLayer }) as! AnnotationLayer
+        }
+        
+        /// The closed annotation sublayer.
+        private var closedSublayer: AnnotationSublayer {
+            annotationLayer.subLayerContents[0] as! AnnotationSublayer
+        }
+        
+        /// The open annotation sublayer.
+        private var openSublayer: AnnotationSublayer {
+            annotationLayer.subLayerContents[1] as! AnnotationSublayer
+        }
+        
+        /// A Boolean value indicating whether to show the closed annotation sublayer.
+        @Published var showsClosedSublayer: Bool = true
+        
+        /// A Boolean value indicating whether to show the open annotation sublayer.
+        @Published var showsOpenSublayer: Bool = true
+        
+        /// A Boolean value indicating whether the open annotation sublayer is visible at the current extent.
+        @Published var visibleAtCurrentExtent = false
+        
+        /// The min scale of the open annotation sublayer.
+        private var minScale: Double?
+        
+        /// The max scale of the open annotation sublayer.
+        private var maxScale: Double?
+        
+        /// The current scale of the map.
+        var currentScale: Double = .zero {
+            didSet {
+                formatCurrentScaleText()
+                guard let minScale, let maxScale else { return }
+                visibleAtCurrentExtent = openSublayer.isVisible(atScale: currentScale)
+            }
+        }
+        
+        /// The map's current scale value in text.
+        @Published var currentScaleText: String = ""
+        
+        /// The min and max scale of the open annotation layer in text.
+        @Published var minMaxScaleText: String = ""
+        
+        /// Loads a local mobile map package.
+        func loadMobileMapPackage() async throws {
+            mobileMapPackage = MobileMapPackage(fileURL: .gasDevicePackage)
+            try await mobileMapPackage.load()
+            guard let map = mobileMapPackage.maps.first else { return }
+            self.map = map
+        }
+        
+        /// Sets the visibility of the closed annotation sublayer.
+        /// - Parameter visibility: The visibility of the sublayer.
+        func setClosedSublayerVisibility(_ visibility: Bool) {
+            closedSublayer.isVisible = visibility
+        }
+        
+        /// Sets the visibility of the open annotation sublayer.
+        /// - Parameter visibility: The visibility of the sublayer.
+        func setOpenSublayerVisibility(_ visibility: Bool) {
+            openSublayer.isVisible = visibility
+        }
+        
+        /// Sets the min and max scale and formats the min-max scale text.
+        func setScaleText() {
+            minScale = openSublayer.minScale
+            maxScale = openSublayer.maxScale
+            
+            formatMinMaxScaleText()
+        }
+        
+        /// Formats the current scale text.
+        private func formatCurrentScaleText() {
+            currentScaleText = "1:\(currentScale.formatted(.decimal))"
+        }
+        
+        /// Formats the min-max scale text.
+        private func formatMinMaxScaleText() {
+            guard let minScale, let maxScale else { return }
+            minMaxScaleText = "Open (1:\(minScale.formatted(.decimal)) - 1:\(maxScale.formatted(.decimal)))"
+        }
+    }
+}
+
+private extension FormatStyle where Self == FloatingPointFormatStyle<Double> {
+    /// Formats the double with zero decimals places of precision.
+    static var decimal: Self {
+        Self.number.precision(.fractionLength(0))
+    }
+}
+
+private extension URL {
+    /// The URL to the local Gas Device Annotation mobile map package file.
+    static var gasDevicePackage: URL {
+        Bundle.main.url(forResource: "GasDeviceAnno", withExtension: "mmpk")!
+    }
+}
---
+++ Shared/Samples/Control annotation sublayer visibility/ControlAnnotationSublayerVisibilityView.swift
@@ -0,0 +1,71 @@
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import ArcGIS
+import SwiftUI
+
+struct ControlAnnotationSublayerVisibilityView: View {
+    /// The view model for the sample.
+    @StateObject private var model = Model()
+    
+    /// The error shown in the error alert.
+    @State private var error: Error?
+    
+    var body: some View {
+        MapView(map: model.map)
+            .onScaleChanged { scale in
+                model.currentScale = scale
+            }
+            .onLayerViewStateChanged { layer, layerViewState in
+                guard layer is AnnotationLayer else { return }
+                if layerViewState.status == .active {
+                    model.setScaleText()
+                }
+            }
+            .overlay(alignment: .top) {
+                Text("Current scale: \(model.currentScaleText)")
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding(8)
+                    .background(.ultraThinMaterial, ignoresSafeAreaEdges: .horizontal)
+                    .multilineTextAlignment(.center)
+            }
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Menu("Sublayers") {
+                        Toggle("Closed", isOn: $model.showsClosedSublayer)
+                            .onChange(of: model.showsClosedSublayer) {
+                                model.setClosedSublayerVisibility(model.showsClosedSublayer)
+                            }
+                        Toggle(model.minMaxScaleText, isOn: $model.showsOpenSublayer)
+                            .onChange(of: model.showsOpenSublayer) {
+                                model.setOpenSublayerVisibility(model.showsOpenSublayer)
+                            }
+                            .disabled(!model.visibleAtCurrentExtent)
+                    }
+                }
+            }
+            .task {
+                do {
+                    try await model.loadMobileMapPackage()
+                } catch {
+                    self.error = error
+                }
+            }
+            .errorAlert(presentingError: $error)
+    }
+}
+
+#Preview {
+    ControlAnnotationSublayerVisibilityView()
+}
