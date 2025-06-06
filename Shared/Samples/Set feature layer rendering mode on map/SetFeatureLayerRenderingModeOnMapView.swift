e3d70d10910cd58fa261b6aa1e58e12f0d960f32
Destiny Hochhalter <dhochhalterruiz@esri.com>
2025-06-06 15:09:14 -0700

    Add new sample

---
+++ Shared/Samples/Set feature layer rendering mode on map/SetFeatureLayerRenderingModeOnMapView.swift
@@ -0,0 +1,138 @@
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import ArcGIS
+import SwiftUI
+
+struct SetFeatureLayerRenderingModeOnMapView: View {
+    /// A map used to show dynamic rendering.
+    @State private var dynamicMap = Map()
+    
+    /// A map used to show static rendering.
+    @State private var staticMap = Map()
+    
+    /// A Boolean value indicating whether the map views are zoomed in.
+    @State private var isZoomedIn = true
+    
+    /// A Boolean value indicating whether the map views are currently zooming.
+    @State private var isZooming = false
+    
+    /// The current viewpoint of the map views.
+    @State private var currentViewpoint: Viewpoint?
+    
+    init() {
+        // Creates service feature tables using point,polygon, and polyline services
+        let pointTable = ServiceFeatureTable(url: URL(string: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Energy/Geology/FeatureServer/0")!)
+        let polylineTable = ServiceFeatureTable(url: URL(string: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Energy/Geology/FeatureServer/8")!)
+        let polygonTable = ServiceFeatureTable(url: URL(string: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Energy/Geology/FeatureServer/9")!)
+        
+        let featureLayers: [FeatureLayer] = [
+            FeatureLayer(featureTable: pointTable),
+            FeatureLayer(featureTable: polylineTable),
+            FeatureLayer(featureTable: polygonTable)
+        ]
+        
+        // Add layers with dynamic and static rendering
+        // to the respective maps.
+        for layer in featureLayers {
+            // Set dynamic rendering.
+            layer.renderingMode = .dynamic
+            dynamicMap.addOperationalLayer(layer)
+            
+            // Set static rendering.
+            let staticLayer = layer.clone()
+            staticLayer.renderingMode = .static
+            staticMap.addOperationalLayer(staticLayer)
+        }
+        
+        staticMap.initialViewpoint = .zoomedOut
+        dynamicMap.initialViewpoint = .zoomedOut
+    }
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            MapViewReader { mapViewProxy in
+                MapView(map: dynamicMap)
+                    .overlay(alignment: .top) {
+                        Text("Dynamic")
+                            .multilineTextAlignment(.center)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .padding(8)
+                            .background(.regularMaterial, ignoresSafeAreaEdges: .horizontal)
+                    }
+                    .task(id: currentViewpoint) {
+                        guard let currentViewpoint else { return }
+                        await mapViewProxy.setViewpoint(currentViewpoint, duration: 5)
+                        isZooming = false
+                    }
+            }
+            MapViewReader { mapViewProxy in
+                MapView(map: staticMap)
+                    .overlay(alignment: .top) {
+                        Text("Static")
+                            .multilineTextAlignment(.center)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .padding(8)
+                            .background(.regularMaterial, ignoresSafeAreaEdges: .horizontal)
+                    }
+                    .task(id: currentViewpoint) {
+                        guard let currentViewpoint else { return }
+                        await mapViewProxy.setViewpoint(currentViewpoint, duration: 5)
+                        isZooming = false
+                    }
+            }
+        }
+        .onChange(of: isZooming) {
+            if isZooming {
+                // Zooming began.
+                currentViewpoint = isZoomedIn ? .zoomedIn : .zoomedOut
+            } else {
+                // Zooming ended.
+                isZoomedIn.toggle()
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .bottomBar) {
+                Button {
+                    isZooming = true
+                } label: {
+                    isZoomedIn ? Text("Zoom Out") : Text("Zoom In")
+                }
+                .disabled(isZooming)
+            }
+        }
+    }
+}
+
+private extension Viewpoint {
+    static var zoomedIn: Viewpoint {
+        Viewpoint(
+            center: Point(x: -118.37, y: 34.46, spatialReference: .wgs84),
+            scale: 650000,
+            rotation: 0
+        )
+    }
+    
+    static var zoomedOut: Viewpoint {
+        Viewpoint(
+            center: Point(x: -118.45, y: 34.395, spatialReference: .wgs84),
+            scale: 50000,
+            rotation: 90
+        )
+    }
+}
+
+#Preview {
+    SetFeatureLayerRenderingModeOnMapView()
+}
