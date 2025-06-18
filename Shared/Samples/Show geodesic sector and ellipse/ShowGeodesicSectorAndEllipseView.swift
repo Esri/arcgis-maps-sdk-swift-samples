e673464575751e92c23de6e4a40d6a7fe00719e6
Christopher Webb <chr13795@esri.com>
2025-06-18 09:49:07 -0700

    add in sector graphics

--- Shared/Samples/Show geodesic sector and ellipse/ShowGeodesicSectorAndEllipseView.swift
+++ Shared/Samples/Show geodesic sector and ellipse/ShowGeodesicSectorAndEllipseView.swift
@@ -18,6 +18,15 @@ import SwiftUI
 struct ShowGeodesicSectorAndEllipseView: View {
     @StateObject private var model = Model()
     @State private var tapPoint: Point?
+    @State private var semiAxis1Length: Double = 10
+    @State private var semiAxis2Length: Double = 10
+    @State private var axisDirection: Double = 10
+    @State private var maxSegmentLength: Double = 10
+    @State private var sectorAngle: Double = 10
+    @State private var startDirection: Double = 10
+    @State private var maxPointCount: Double = 10
+    
+    //geometryType
     
     var body: some View {
         MapViewReader { proxy in
@@ -31,30 +40,47 @@ struct ShowGeodesicSectorAndEllipseView: View {
                         model.updateSector(tapPoint: tapPoint)
                     }
                 }
+                .toolbar {
+                    ToolbarItemGroup(placement: .bottomBar) {
+                        Button("Angle") { }
+                        Spacer()
+                        Button("Axis") { }
+                    }
+                }
         }
     }
 }
 
+
 private extension ShowGeodesicSectorAndEllipseView {
     @MainActor
     class Model: ObservableObject {
-        var graphicOverlay = GraphicsOverlay()
         var map = Map(basemapStyle: .arcGISTopographic)
+        var graphicOverlay = GraphicsOverlay()
+        
         var sectorFillSymbol = SimpleFillSymbol(style: .solid, color: .green)
         var sectorLineSymbol = SimpleLineSymbol(style: .solid, color: .green, width: 3)
         var sectorMarkerSymbol = SimpleMarkerSymbol(style: .circle, color: .green, size: 3)
         
+        var ellipseGraphic: Graphic!
         
-        var ellipseGraphic: Graphic = {
-            var ellipseGraphic = Graphic()
-            var ellipseLineSymbol = SimpleLineSymbol(style: .dot, color: .red, width: 2)
-            ellipseGraphic.symbol = ellipseLineSymbol
-            return ellipseGraphic
-        }()
-        
-        
+        var sectorGraphic: Graphic!
         
         func updateSector(tapPoint: Point) {
+            var sectorParams = GeodesicSectorParameters<ArcGIS.Polygon>()
+            sectorParams.center = tapPoint
+            sectorParams.axisDirection = 45
+            sectorParams.maxPointCount = 100
+            sectorParams.maxSegmentLength = 20
+            sectorParams.sectorAngle = 90
+            sectorParams.semiAxis1Length = 200
+            sectorParams.semiAxis2Length = 400
+            sectorParams.startDirection = 0
+            
+            var sectorGeometry = GeometryEngine.geodesicSector(parameters: sectorParams)
+            sectorGraphic = Graphic(geometry: sectorGeometry, symbol: sectorLineSymbol)
+            graphicOverlay.addGraphic(sectorGraphic)
+            
             let symbol = SimpleFillSymbol(style: .solid, color: .green)
             // Creates the parameters for the ellipse.
             let parameters = GeodesicEllipseParameters<ArcGIS.Polygon>(
@@ -67,9 +93,9 @@ private extension ShowGeodesicSectorAndEllipseView {
                 semiAxis2Length: 400
             )
             // Creates the geometry for the ellipse from the parameters.
-            let geometry = GeometryEngine.geodesicEllipse(parameters: parameters)
+            let ellipseGeometry = GeometryEngine.geodesicEllipse(parameters: parameters)
             // Creates a graphics overlay containing a graphic with the ellipse geometry.
-            graphicOverlay = GraphicsOverlay(graphics: [Graphic(geometry: geometry)])
+            graphicOverlay = GraphicsOverlay(graphics: [Graphic(geometry: ellipseGeometry)])
             // Creates and assigns a simple renderer to the graphics overlay.
             graphicOverlay.renderer = SimpleRenderer(symbol: symbol)
         }
