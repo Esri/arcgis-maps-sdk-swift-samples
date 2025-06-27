69610c9841a31596a901f6a1ddee827f69c0f0f7
Ryan Olson <rolson@esri.com>
2025-06-27 10:16:24 -0600

    cleanup

--- Shared/Samples/Manage features/ManageFeaturesView.swift
+++ Shared/Samples/Manage features/ManageFeaturesView.swift
@@ -20,6 +20,7 @@ struct ManageFeaturesView: View {
     @State private var model = Model()
     
     @State private var tapLocation: CGPoint?
+    @State private var tapMapPoint: Point?
     
     @State private var calloutPlacement: CalloutPlacement?
     
@@ -34,7 +35,6 @@ struct ManageFeaturesView: View {
                 ProgressView()
             }
         }
-        .animation(.default, value: model.action)
         .animation(.default, value: calloutPlacement)
         .task { await model.loadData() }
     }
@@ -43,57 +43,157 @@ struct ManageFeaturesView: View {
     func mapView(_ data: Data) -> some View {
         MapViewReader { mapView in
             MapView(map: data.map)
-                .onSingleTapGesture { tapLocation, _ in
+                .onSingleTapGesture { tapLocation, tapMapPoint in
                     self.tapLocation = tapLocation
-                    data.featureLayer.clearSelection()
-                    calloutPlacement = nil
+                    self.tapMapPoint = tapMapPoint
+                    clearSelection()
                 }
                 .callout(placement: $calloutPlacement) { placement in
-                    Group {
-                        if let geoElement = placement.geoElement {
-                            Text("element!")
-                        } else {
-                            HStack {
-                                Text("Add New Feature")
-                                Button {
-                                    createFeature()
-                                } label: {
-                                    Image(systemName: "plus.circle")
-                                }
-                            }
-                        }
+                    if let feature = placement.geoElement as? Feature {
+                        featureCalloutContent(feature: feature, table: data.featureTable)
+                    } else if let tapMapPoint {
+                        newFeatureCalloutContent(table: data.featureTable, point: tapMapPoint)
                     }
-                    .padding()
                 }
                 .overlay(alignment: .top) {
                     overlayContent
                 }
                 .task(id: tapLocation) {
-                    guard let tapLocation else { return }
+                    guard let tapLocation, let tapMapPoint else { return }
                     if let identifyResult = try? await mapView.identify(on: data.featureLayer, screenPoint: tapLocation, tolerance: 12),
                        let geoElement = identifyResult.geoElements.first,
                        let feature = geoElement as? Feature {
-                        print("-- result: \(geoElement)")
                         calloutPlacement = .geoElement(geoElement)
                         data.featureLayer.selectFeature(feature)
-                    } else if let mapPoint = mapView.location(fromScreenPoint: tapLocation) {
-                        calloutPlacement = .location(mapPoint)
+                    } else {
+                        calloutPlacement = .location(tapMapPoint)
                     }
                 }
         }
     }
     
-    @ViewBuilder var overlayContent: some View {
-        VStack {
-            Text("Tap the map to create a new feature, or an existing feature for more options.")
-                .multilineTextAlignment(.center)
+    @ViewBuilder
+    func newFeatureCalloutContent(table: ServiceFeatureTable, point: Point) -> some View {
+        HStack {
+            Text("Add New Feature")
+            Button {
+                clearSelection()
+                Task {
+                    try await createFeature(table: table, point: point)
+                }
+            } label: {
+                Image(systemName: "plus.circle")
+            }
         }
         .padding()
-        .frame(maxWidth: .infinity)
-        .background(.ultraThinMaterial)
     }
     
-    func createFeature() {
+    @ViewBuilder
+    func featureCalloutContent(feature: Feature, table: ServiceFeatureTable) -> some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("ID: \(feature.id)")
+                Text("Damage: \(feature.damageKind?.value ?? "Unknown")")
+                    .font(.footnote)
+                    .foregroundStyle(.secondary)
+            }
+            Menu {
+                Button {
+                    Task {
+                        clearSelection()
+                        try await updateAttribute(for: feature, table: table)
+                    }
+                } label: {
+                    Text("Update Attribute")
+                }
+                Button {
+                    Task {
+                        clearSelection()
+                        try await updateGeometry(for: feature, table: table)
+                    }
+                } label: {
+                    Text("Update Geometry")
+                }
+                Button {
+                    Task {
+                        clearSelection()
+                        try await delete(feature: feature, table: table)
+                    }
+                } label: {
+                    Text("Delete Feature")
+                }
+            } label: {
+                Image(systemName: "ellipsis")
+                    .padding(.leading)
+            }
+        }
+        .padding()
+    }
+    
+    @ViewBuilder var overlayContent: some View {
+        Text("Tap the map to create a new feature, or an existing feature for more options.")
+            .multilineTextAlignment(.center)
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(.ultraThinMaterial)
+    }
+    
+    func clearSelection() {
+        if case .success(let data) = model.data {
+            data.featureLayer.clearSelection()
+        }
+        calloutPlacement = nil
+    }
+    
+    func createFeature(table: ServiceFeatureTable, point: Point) async throws {
+        let feature = table.makeFeature(
+            attributes: [
+                Feature.damageTypeFieldName: DamageKind.inaccessible.value
+            ],
+            geometry: point
+        )
+        try await table.add(feature)
+        _ = try await table.serviceGeodatabase?.applyEdits()
+    }
+    
+    func updateAttribute(for feature: Feature, table: ServiceFeatureTable) async throws {
+        feature.damageKind = feature.damageKind?.next ?? .inaccessible
+        try await table.update(feature)
+        _ = try await table.serviceGeodatabase?.applyEdits()
+    }
+    
+    func updateGeometry(for feature: Feature, table: ServiceFeatureTable) async throws {
+        // TODO: ...
+        try await table.update(feature)
+        _ = try await table.serviceGeodatabase?.applyEdits()
+    }
+    
+    func delete(feature: Feature, table: ServiceFeatureTable) async throws {
+        try await table.delete(feature)
+        _ = try await table.serviceGeodatabase?.applyEdits()
+    }
+}
+
+extension Feature {
+    static let damageTypeFieldName = "typdamage"
+    static let objectIDFieldName = "objectid"
+    
+    var id: String {
+        if let objectID = attributeValue(forKey: "objectid") {
+            return "\(objectID)"
+        } else {
+            return "Unknown"
+        }
+    }
+    
+    var damageKind: ManageFeaturesView.DamageKind? {
+        get {
+            ManageFeaturesView.DamageKind(
+                attributeValue(forKey: Self.damageTypeFieldName) as? String ?? ""
+            )
+        } set {
+            setAttributeValue(newValue?.value, forKey: Self.damageTypeFieldName)
+        }
     }
 }
 
@@ -105,35 +205,50 @@ extension ManageFeaturesView {
         let featureLayer: FeatureLayer
     }
     
-    enum Action: CaseIterable {
-        case create
-        case delete
-        case updateAttribute
-        case updateGeometry
+    enum DamageKind: CaseIterable {
+        case inaccessible
+        case affected
+        case minor
+        case major
+        case destroyed
         
-        var label: String {
+        init?(_ value: String) {
+            for `case` in Self.allCases {
+                if value == `case`.value {
+                    self = `case`
+                    return
+                }
+            }
+            return nil
+        }
+        
+        var value: String {
             switch self {
-            case .create:
-                "Create Feature"
-            case .delete:
-                "Delete Feature"
-            case .updateAttribute:
-                "Update Attribute"
-            case .updateGeometry:
-                "Update Geometry"
+            case .inaccessible:
+                "Inaccessible"
+            case .affected:
+                "Affected"
+            case .minor:
+                "Minor"
+            case .major:
+                "Major"
+            case .destroyed:
+                "Destroyed"
             }
         }
         
-        var instructions: String {
+        var next: ManageFeaturesView.DamageKind {
             switch self {
-            case .create:
-                "Tap the map to create a new feature."
-            case .delete:
-                "Tap on a feature to delete it."
-            case .updateAttribute:
-                "Tap something to update..."
-            case .updateGeometry:
-                "Tap something to update geometry..."
+            case .inaccessible:
+                .affected
+            case .affected:
+                .minor
+            case .minor:
+                .major
+            case .major:
+                .destroyed
+            case .destroyed:
+                .inaccessible
             }
         }
     }
@@ -144,7 +259,6 @@ extension ManageFeaturesView {
     @MainActor
     final class Model {
         var data: Result<Data, Error>?
-        var action: Action = .create
         
         func loadData() async {
             let map = Map(basemapStyle: .arcGISStreets)
