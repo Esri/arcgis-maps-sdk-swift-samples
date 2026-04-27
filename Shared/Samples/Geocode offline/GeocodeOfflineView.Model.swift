157d67ba58877ca6b7d2de52529625d00c836470
Christopher Webb <chr13795@esri.com>
2026-04-27 14:08:32 -0700

    update

--- Shared/Samples/Geocode offline/GeocodeOfflineView.Model.swift
+++ Shared/Samples/Geocode offline/GeocodeOfflineView.Model.swift
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 import ArcGIS
+import Combine
 import UIKit
 
 extension GeocodeOfflineView {
--- Shared/Samples/Geocode offline/GeocodeOfflineView.swift
+++ Shared/Samples/Geocode offline/GeocodeOfflineView.swift
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 import ArcGIS
-import Combine
 import SwiftUI
 
 struct GeocodeOfflineView: View {
