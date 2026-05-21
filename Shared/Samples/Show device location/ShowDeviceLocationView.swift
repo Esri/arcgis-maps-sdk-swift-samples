9a51d8217fcd02f4b08bc86a7789a4943c3a02ba
Christopher Webb <chr13795@esri.com>
2026-05-21 10:31:53 -0700

    update indentation

--- Shared/Samples/Show device location/ShowDeviceLocationView.swift
+++ Shared/Samples/Show device location/ShowDeviceLocationView.swift
@@ -133,7 +133,7 @@ private extension ShowDeviceLocationView {
     /// An error that occurs when location display fails due to a permission denied error.
     struct LocationPermissionDeniedError: LocalizedError {
         var errorDescription: String {
-           "Location permission denied. Please authorize location access for this application in Settings."
+            "Location permission denied. Please authorize location access for this application in Settings."
         }
     }
 }
