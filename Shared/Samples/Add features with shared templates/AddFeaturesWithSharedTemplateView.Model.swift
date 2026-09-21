fc336f0e844a1b4cdec95986aa7854aa7cfc305f
Christopher Webb <chr13795@esri.com>
2026-09-20 17:22:21 -0700

    remove unneeded change

--- Shared/Samples/Add features with shared templates/AddFeaturesWithSharedTemplateView.Model.swift
+++ Shared/Samples/Add features with shared templates/AddFeaturesWithSharedTemplateView.Model.swift
@@ -165,7 +165,7 @@ extension AddFeaturesWithSharedTemplateView {
             case .point:
                 status = "Place a point, then tap Complete or Cancel."
                 geometryEditor.start(withType: Point.self)
-            case .polyline:
+            case .line:
                 status = "Sketch a line, then tap Complete or Cancel."
                 geometryEditor.start(withType: Polyline.self)
             default:
