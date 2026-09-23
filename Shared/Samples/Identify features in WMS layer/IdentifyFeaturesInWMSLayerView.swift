be22b1e9fdd6ebcc0df13dd25a3c40b98f1c03d7
Christopher Webb <chr13795@esri.com>
2026-09-23 10:23:50 -0700

    remove check

--- Shared/Samples/Identify features in WMS layer/IdentifyFeaturesInWMSLayerView.swift
+++ Shared/Samples/Identify features in WMS layer/IdentifyFeaturesInWMSLayerView.swift
@@ -41,10 +41,7 @@ struct IdentifyFeaturesInWMSLayerView: View {
     
     /// The error shown in the error alert.
     @State private var error: (any Error)?
-    
-    /// A Boolean value indicating whether the map has been set up.
-    @State private var didSetupMap = false
-    
+
     var body: some View {
         MapViewReader { mapViewProxy in
             MapView(map: map)
@@ -105,8 +102,6 @@ struct IdentifyFeaturesInWMSLayerView: View {
     
     /// Adds the WMS layer to the map once.
     private func setupMapIfNecessary() {
-        guard !didSetupMap else { return }
-        
         map.addOperationalLayer(waterInfoLayer)
         didSetupMap = true
     }
--- Shared/Samples/Show extruded graphics/ShowExtrudedGraphicsView.swift
+++ Shared/Samples/Show extruded graphics/ShowExtrudedGraphicsView.swift
@@ -53,9 +53,6 @@ struct ShowExtrudedGraphicsView: View {
         return graphicsOverlay
     }()
     
-    /// A Boolean value indicating whether the graphics have been added.
-    @State private var didAddGraphics = false
-    
     var body: some View {
         SceneView(scene: scene, graphicsOverlays: [graphicsOverlay])
             .onAppear {
@@ -65,8 +62,6 @@ struct ShowExtrudedGraphicsView: View {
     
     /// Adds extruded polygon graphics to the graphics overlay once.
     private func addGraphicsIfNecessary() {
-        guard !didAddGraphics else { return }
-        
         let x = Point.initialLocation.x - 0.01
         let y = Point.initialLocation.y + 0.25
         
@@ -82,7 +77,6 @@ struct ShowExtrudedGraphicsView: View {
                 graphicsOverlay.addGraphic(graphic)
             }
         }
-        didAddGraphics = true
     }
     
     /// An extruded graphic created from a given polygon with a random height.
